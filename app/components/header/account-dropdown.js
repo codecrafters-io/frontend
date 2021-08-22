c7447a7b822874af15b68ea2ad6cea5cf76d3c67
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-08-22 03:16:23 +0100

    Add log out button

---
+++ app/components/header/account-dropdown.js
@@ -0,0 +1,25 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class HeaderAccountDropdownComponent extends Component {
+  @service serverVariables;
+
+  @action
+  handleGetHelpClick(dropdownActions) {
+    window.Beacon('open');
+    dropdownActions.close();
+  }
+
+  @action
+  handleLogoutClick() {
+    // https://stackoverflow.com/a/24766685
+    let f = document.createElement('form');
+    f.action = `${this.serverVariables.get('serverUrl')}/logout`;
+    f.method = 'POST';
+    // f.target='_blank';
+
+    document.body.appendChild(f);
+    f.submit();
+  }
+}
