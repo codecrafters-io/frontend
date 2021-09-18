dd48978443de011aea112535fa4ab2686bcdad80
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-09-18 22:55:11 +0100

    fix lint

--- tests/acceptance/course-page/view-course-stages-test.js
+++ tests/acceptance/course-page/view-course-stages-test.js
@@ -1,11 +1,9 @@
 import { animationsSettled, setupAnimationTest } from 'ember-animated/test-support';
-import { currentURL } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import coursesPage from 'codecrafters-frontend/tests/pages/courses-page';
 import coursePage from 'codecrafters-frontend/tests/pages/course-page';
-import finishRender from 'codecrafters-frontend/tests/support/finish-render';
 import setupClock from 'codecrafters-frontend/tests/support/setup-clock';
 import signIn from 'codecrafters-frontend/tests/support/sign-in';
 import testScenario from 'codecrafters-frontend/mirage/scenarios/test';
