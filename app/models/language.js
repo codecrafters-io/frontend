0f802cc51349afcf504f81ce00562a7c4bb80be8
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-09-19 21:34:21 +0100

    fix lang logos, pushedAt date

--- app/models/language.js
+++ app/models/language.js
@@ -7,15 +7,39 @@ export default class LanguageModel extends Model {
 
   get grayLogoUrl() {
     return {
-      Python: '/assets/images/language-logos/python-gray-500.svg',
-      Go: '/assets/images/language-logos/go-gray-500.svg',
-    }[this.name];
+      c: '/assets/images/language-logos/c-gray-500.svg',
+      crystal: '/assets/images/language-logos/crystal-gray-500.svg',
+      elixir: '/assets/images/language-logos/elixir-gray-500.svg',
+      go: '/assets/images/language-logos/go-gray-500.svg',
+      haskell: '/assets/images/language-logos/haskell-gray-500.svg',
+      java: '/assets/images/language-logos/java-gray-500.svg',
+      javascript: '/assets/images/language-logos/javascript-gray-500.svg',
+      kotlin: '/assets/images/language-logos/kotlin-gray-500.svg',
+      nim: '/assets/images/language-logos/nim-gray-500.svg',
+      php: '/assets/images/language-logos/php-gray-500.svg',
+      python: '/assets/images/language-logos/python-gray-500.svg',
+      ruby: '/assets/images/language-logos/ruby-gray-500.svg',
+      rust: '/assets/images/language-logos/rust-gray-500.svg',
+      swift: '/assets/images/language-logos/swift-gray-500.svg',
+    }[this.slug];
   }
 
   get tealLogoUrl() {
     return {
-      Python: '/assets/images/language-logos/python-teal-500.svg',
-      Go: '/assets/images/language-logos/go-teal-500.svg',
-    }[this.name];
+      c: '/assets/images/language-logos/c-teal-500.svg',
+      crystal: '/assets/images/language-logos/crystal-teal-500.svg',
+      elixir: '/assets/images/language-logos/elixir-teal-500.svg',
+      go: '/assets/images/language-logos/go-teal-500.svg',
+      haskell: '/assets/images/language-logos/haskell-teal-500.svg',
+      java: '/assets/images/language-logos/java-teal-500.svg',
+      javascript: '/assets/images/language-logos/javascript-teal-500.svg',
+      kotlin: '/assets/images/language-logos/kotlin-teal-500.svg',
+      nim: '/assets/images/language-logos/nim-teal-500.svg',
+      php: '/assets/images/language-logos/php-teal-500.svg',
+      python: '/assets/images/language-logos/python-teal-500.svg',
+      ruby: '/assets/images/language-logos/ruby-teal-500.svg',
+      rust: '/assets/images/language-logos/rust-teal-500.svg',
+      swift: '/assets/images/language-logos/swift-teal-500.svg',
+    }[this.slug];
   }
 }
--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,9 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'codecrafters-frontend',
     environment,
+    moment: {
+      outputFormat: 'L',
+    },
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {
