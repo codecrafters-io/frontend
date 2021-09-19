86cc8e52582f161bbf0e3e3cd6b04f30848fc4fd
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-09-20 00:23:26 +0100

    move fullstory setup to header didInsert

--- app/components/header.js
+++ app/components/header.js
@@ -1,9 +1,20 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class HeaderComponent extends Component {
+  @service currentUser;
   @tracked mobileMenuIsExpanded = false;
+
+  @action
+  handleDidInsert() {
+    let username = this.currentUser.currentUserUsername;
+
+    if (username && window.FS) {
+      window.FS.identify(username, { displayName: username });
+    }
+  }
 
   @action
   toggleMobileMenu() {
--- app/instance-initializers/setup-fullstory.js
+++
@@ -1,8 +0,0 @@
-export function initialize(appInstance) {
-  let username = appInstance.lookup('service:current-user').currentUserUsername;
-  window.FS.identify(username, { displayName: username });
-}
-
-export default {
-  initialize,
-};
