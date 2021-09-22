7b95b3a0634c6e1e70bd660594b54a43f5c6e9a3
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-09-22 21:37:55 +0100

    fix user identification

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -18,7 +18,7 @@ export default class CurrentUserService extends Service {
   }
 
   get currentUserUsername() {
-    return this.currentUserPayload.username;
+    return this.currentUserPayload.attributes.username;
   }
 
   get record() {
