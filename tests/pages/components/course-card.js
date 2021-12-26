a864e631e1b99dfed3223c03e8e69aea89167b7d
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-12-26 23:59:15 +0000

    remove hasFreeLabel from tests

--- tests/pages/components/course-card.js
+++ tests/pages/components/course-card.js
@@ -4,5 +4,4 @@ export default {
   name: text('[data-test-course-name]'),
   description: text('[data-test-course-description]'),
   hasBetaLabel: isPresent('[data-test-course-beta-label]'),
-  hasFreeLabel: isPresent('[data-test-course-free-label]'),
 };
