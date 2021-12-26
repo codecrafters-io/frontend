fe63670bc843525af022e0260f3c709338e00b99
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-12-26 23:37:19 +0000

    remove course card changes

--- app/components/course-card.js
+++ app/components/course-card.js
@@ -3,15 +3,10 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class CourseCardComponent extends Component {
-  @service currentUser;
   @service router;
 
   @action
   async navigateToCourse() {
     await this.router.transitionTo('course', this.args.course.slug, { queryParams: { fresh: null } });
-  }
-
-  get shouldShowFreeLabel() {
-    return this.args.course.isFree && !this.currentUser.hasActiveSubscription;
   }
 }
