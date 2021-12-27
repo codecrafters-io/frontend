a29a9b831524f1aaa29d4d037c959db7c0ce03ec
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-12-27 10:49:02 +0000

    remove unused import

--- app/components/checkout-session-successful-modal.js
+++ app/components/checkout-session-successful-modal.js
@@ -1,6 +1,5 @@
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 import * as Sentry from '@sentry/ember';
 import Component from '@glimmer/component';
 import window from 'ember-window-mock';
