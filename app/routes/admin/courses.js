5b622217c362c185703f5a120c1869f62ccdcc15
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-12-28 23:51:19 +0000

    add admin/courses page

---
+++ app/routes/admin/courses.js
@@ -0,0 +1,14 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class AdminCoursesRoute extends Route {
+  @service currentUser;
+
+  async model() {
+    await this.currentUser.authenticate();
+
+    return {
+      courses: await this.store.findAll('course'),
+    };
+  }
+}
