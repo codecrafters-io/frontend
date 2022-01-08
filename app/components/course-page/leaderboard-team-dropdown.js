9c18e2cf75fcd9ec1f6f156b7b0f038e8f8e5126
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-01-08 14:43:51 +0000

    run percy [percy]

--- app/components/course-page/leaderboard-team-dropdown.js
+++ app/components/course-page/leaderboard-team-dropdown.js
@@ -1,15 +1,7 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
-import { later } from '@ember/runloop';
-import fade from 'ember-animated/transitions/fade';
 
-export default class CoursePageRepositoryDropdownComponent extends Component {
-  transition = fade;
-  @service router;
-  @tracked gitRepositoryURLWasCopiedRecently;
-
+export default class LeaderboardTeamDropdownComponent extends Component {
   @action
   async handleTeamLinkClick(team, dropdownActions) {
     dropdownActions.close();
