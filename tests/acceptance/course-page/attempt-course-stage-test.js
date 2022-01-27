78a4b65dd66b370306129c7a4ce9540924f1abc9
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-01-27 22:36:30 +0000

    fix computation of submission.isRecent

--- tests/acceptance/course-page/attempt-course-stage-test.js
+++ tests/acceptance/course-page/attempt-course-stage-test.js
@@ -1,4 +1,4 @@
-import { setupAnimationTest } from 'ember-animated/test-support';
+import { animationsSettled, setupAnimationTest } from 'ember-animated/test-support';
 import { currentURL } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
@@ -23,7 +23,7 @@ module('Acceptance | course-page | attempt-course-stage', function (hooks) {
     let python = this.server.schema.languages.findBy({ name: 'Python' });
     let redis = this.server.schema.courses.findBy({ slug: 'redis' });
 
-    this.server.create('repository', 'withFirstStageCompleted', {
+    let repository = this.server.create('repository', 'withFirstStageCompleted', {
       course: redis,
       language: python,
       user: currentUser,
@@ -35,8 +35,31 @@ module('Acceptance | course-page | attempt-course-stage', function (hooks) {
     assert.equal(currentURL(), '/courses/redis', 'current URL is course page URL');
     assert.equal(this.server.pretender.handledRequests.length, 4); // Fetch course (courses page + course page) + fetch repositories + leaderboard
 
-    assert.equal(coursePage.activeCourseStageItem.title, 'Respond to PING');
-    assert.equal(coursePage.activeCourseStageItem.footerText, 'Listening for a git push...');
+    assert.equal(coursePage.activeCourseStageItem.title, 'Respond to PING', 'first stage is active');
+    assert.equal(coursePage.activeCourseStageItem.footerText, 'Listening for a git push...', 'footer text is waiting for git push');
+
+    this.server.create('submission', 'withFailureStatus', {
+      repository: repository,
+      courseStage: redis.stages.models.sortBy('position')[1],
+    });
+
+    await this.clock.tick(2001); // Wait for poll
+
+    // force re-computation
+    await coursesPage.visit(); // This interacts with start-course-stage, not sure why
+    await coursesPage.clickOnCourse('Build your own Redis');
+    await animationsSettled();
+
+    assert.equal(coursePage.activeCourseStageItem.footerText, 'Tests failed. Check your git push output for logs.', 'footer text is tests failed');
+
+    await this.clock.tick(1000 * 301); // Wait for poll + 5 minutes to pass
+
+    // force re-computation
+    await coursesPage.visit(); // This interacts with start-course-stage, not sure why
+    await coursesPage.clickOnCourse('Build your own Redis');
+    await animationsSettled();
+
+    assert.equal(coursePage.activeCourseStageItem.footerText, 'Listening for a git push...', 'footer text is waiting for git push');
 
     await coursesPage.visit(); // This interacts with start-course-stage, not sure why
   });
