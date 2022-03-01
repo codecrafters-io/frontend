86ccdba1478fcfb4ee0da894630f86642532c546
m-nikhil <m_nikhil@outlook.com>
2022-03-01 03:01:16 -0800

    add course page use cache

--- app/routes/course.js
+++ app/routes/course.js
@@ -1,24 +1,30 @@
 import { A } from '@ember/array';
 import { inject as service } from '@ember/service';
 import ApplicationRoute from 'codecrafters-frontend/lib/application-route';
+import RSVP from 'rsvp';
 
 export default class CourseRoute extends ApplicationRoute {
   @service currentUser;
   @service store;
 
   async model(params) {
+    let modelPromises = {};
     let courses = await this.store.findAll('course', { include: 'supported-languages,stages' });
-    let course = courses.findBy('slug', params.course_slug);
+    modelPromises.course = courses.findBy('slug', params.course_slug);
 
-    let repositories = await this.store.query('repository', {
-      course_id: course.id,
-      include: 'language,course,user.free-usage-restrictions,course-stage-completions.course-stage,last-submission.course-stage',
-    });
+    modelPromises.repositories = this.store
+      .findAll('repository', {
+        include: 'language,course,user.free-usage-restrictions,course-stage-completions.course-stage,last-submission.course-stage',
+      })
+      .then((results) => {
+        results = results.filter((result) => {
+          return result.course.id === modelPromises.course.id;
+        });
 
-    return {
-      course: course,
-      repositories: A(repositories.toArray()),
-    };
+        return A(results.toArray());
+      });
+
+    return RSVP.hash(modelPromises);
   }
 
   setupController(controller, model) {
