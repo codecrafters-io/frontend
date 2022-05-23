57b8725a7d7a5318286ed2a8b1c7f45a53d9fb8c
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-05-23 18:01:54 +0100

    Filter by usernames if present

--- tests/acceptance/admin/view-course-submissions-test.js
+++ tests/acceptance/admin/view-course-submissions-test.js
@@ -2,7 +2,7 @@ import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { signIn } from 'codecrafters-frontend/tests/support/authentication-helpers';
-import { currentURL, visit } from '@ember/test-helpers';
+import { visit } from '@ember/test-helpers';
 import adminCoursesPage from 'codecrafters-frontend/tests/pages/admin/courses-page';
 import adminCourseSubmissionsPage from 'codecrafters-frontend/tests/pages/admin/course-submissions-page';
 import testScenario from 'codecrafters-frontend/mirage/scenarios/test';
@@ -60,19 +60,12 @@ module('Acceptance | admin | view-course-submissions', function (hooks) {
 
     let python = this.server.schema.languages.findBy({ name: 'Python' });
     let redis = this.server.schema.courses.findBy({ slug: 'redis' });
-    let stage1 = redis.stages.models.sortBy('position')[2];
 
-    let repository1 = this.server.create('repository', 'withFirstStageInProgress', { course: redis, language: python, user: user1 });
-    let repository2 = this.server.create('repository', 'withFirstStageInProgress', { course: redis, language: python, user: user2 });
-    let repository3 = this.server.create('repository', 'withFirstStageInProgress', { course: redis, language: python, user: user3 });
+    this.server.create('repository', 'withFirstStageInProgress', { course: redis, language: python, user: user1 });
+    this.server.create('repository', 'withFirstStageInProgress', { course: redis, language: python, user: user2 });
+    this.server.create('repository', 'withFirstStageInProgress', { course: redis, language: python, user: user3 });
 
     await visit('/admin/courses/redis/submissions?usernames=user1,user2');
     assert.equal(adminCourseSubmissionsPage.timelineContainer.entries.length, 4); // 2 users, 2 submissions each
-
-    await visit('/admin/courses/redis/submissions?usernames=user1');
-    assert.equal(adminCourseSubmissionsPage.timelineContainer.entries.length, 2); // 1 users, 2 submissions
-
-    await visit('/admin/courses/redis/submissions?usernames=user1,user2,user3');
-    assert.equal(adminCourseSubmissionsPage.timelineContainer.entries.length, 6); // 3 users, 2 submissions each
   });
 });
