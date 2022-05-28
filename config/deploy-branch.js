235d4367cf303861225f8c26268fe4b8cdbde564
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-05-28 15:36:27 +0100

    enable branch deploys to staging

--- config/deploy-branch.js
+++ config/deploy-branch.js
@@ -19,8 +19,8 @@ module.exports = function (deployTarget) {
 
     s3: {
       prefix: 'codecrafters-frontend',
-      accessKeyId: process.env.AWS_KEY,
-      secretAccessKey: process.env.AWS_SECRET,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       bucket: process.env.S3_BUCKET,
       region: process.env.S3_BUCKET_REGION,
     },
