2148975a223f6a0416c407201f73871b9bf50c86
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-05-30 14:17:50 +0100

    add course-stage-solution-model

---
+++ app/models/course-stage-solution.js
@@ -0,0 +1,10 @@
+import { attr, belongsTo } from '@ember-data/model';
+import Model from '@ember-data/model';
+
+export default class CourseStageSolutionModel extends Model {
+  @belongsTo('course-stage', { async: false }) courseStage;
+  @belongsTo('language', { async: false }) language;
+  @attr('string') explanationMarkdown;
+  @attr('') changedFiles; // free-form JSON
+  @attr('string') githubUrl;
+}
