a6ee06786f46330378bd43f8ff1047c960e79e5a
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-05-31 16:05:45 +0100

    add signup to preview module button

--- app/components/track-page/course-card.js
+++ app/components/track-page/course-card.js
@@ -4,9 +4,13 @@ import { inject as service } from '@ember/service';
 import showdown from 'showdown';
 
 export default class TrackPageCourseCardComponent extends Component {
-  @service store;
+  @service currentUser;
 
   get introductionHtml() {
     return htmlSafe(new showdown.Converter().makeHtml(this.args.course.trackIntroductionMarkdownFor(this.args.language)));
+  }
+
+  get shouldMaskContents() {
+    return this.currentUser.isAnonymous;
   }
 }
