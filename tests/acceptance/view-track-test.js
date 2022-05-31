bb3750c83584a2562a30ab212ca162984986ee43
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-05-31 18:37:43 +0100

    fix lint errors for basic language overview page

---
+++ tests/acceptance/view-track-test.js
@@ -0,0 +1,19 @@
+import coursesPage from 'codecrafters-frontend/tests/pages/courses-page';
+import percySnapshot from '@percy/ember';
+import testScenario from 'codecrafters-frontend/mirage/scenarios/test';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import { visit } from '@ember/test-helpers';
+
+module('Acceptance | view-track', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('it renders for anonymous user', async function (assert) {
+    testScenario(this.server);
+
+    await visit('/tracks/go');
+    assert.equal(1, 1); // dummy assertion
+  });
+});
