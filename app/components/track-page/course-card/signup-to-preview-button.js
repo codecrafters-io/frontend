95263374fa2c4aaef8aeac08df092769eb73df1a
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-01 22:51:42 +0100

    redirect users to track page after module preview

---
+++ app/components/track-page/course-card/signup-to-preview-button.js
@@ -0,0 +1,13 @@
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
+import window from 'ember-window-mock';
+
+export default class SignupToPreviewButton extends Component {
+  @service router;
+
+  @action
+  handleClicked() {
+    window.location.href = '/login?next=' + this.router.currentURL;
+  }
+}
