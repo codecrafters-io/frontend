6c3aaf1f74734f6ece98e2ea1e406d4f872df3e6
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-02 23:06:50 +0100

    fix lint errors

--- tests/acceptance/pay-test.js
+++ tests/acceptance/pay-test.js
@@ -1,4 +1,4 @@
-import { module, test } from 'qunit';
+import { module, skip, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { setupWindowMock } from 'ember-window-mock/test-support';
@@ -25,58 +25,58 @@ module('Acceptance | pay-test', function (hooks) {
     assert.equal(1, 1); // Dummy test
   });
 
-  // test('new user can wait for subscriptions to sync after successful checkout session', async function (assert) {
-  //   signIn(this.owner);
-  //   testScenario(this.server);
-  //
-  //   await coursesPage.visit({ action: 'checkout_session_successful' });
-  //
-  //   const baseRequestsCount = [
-  //     'fetch courses (courses listing page)',
-  //     'fetch repositories (courses listing page)',
-  //     'notify page view (courses listing page)',
-  //     'fetch subscriptions (modal)',
-  //   ].length;
-  //
-  //   assert.ok(coursesPage.checkoutSessionSuccessfulModal.isVisible, 'Checkout session successful modal should be visible');
-  //   assert.equal(this.server.pretender.handledRequests.length, baseRequestsCount);
-  //
-  //   await this.clock.tick(1001);
-  //   await finishRender();
-  //
-  //   assert.equal(this.server.pretender.handledRequests.length, baseRequestsCount + 1);
-  //
-  //   this.server.create('subscription', {
-  //     user: this.server.schema.users.first(),
-  //     endedAt: null,
-  //     startDate: new Date(2021, 1, 1),
-  //   });
-  //
-  //   await this.clock.tick(1001);
-  //   await finishRender();
-  //
-  //   assert.equal(this.server.pretender.handledRequests.length, baseRequestsCount + 2); // One more subscriptions request
-  //   assert.notOk(coursesPage.checkoutSessionSuccessfulModal.isVisible, 'Checkout session successful modal should not be visible');
-  //
-  //   await this.clock.tick(1001);
-  //   await finishRender();
-  //
-  //   assert.equal(this.server.pretender.handledRequests.length, baseRequestsCount + 2); // No more requests after polling is complete
-  //   assert.notOk(coursesPage.checkoutSessionSuccessfulModal.isVisible, 'Checkout session successful modal should not be visible');
-  // });
-  //
-  // test('subscriber can manage subscription', async function (assert) {
-  //   signInAsSubscriber(this.owner);
-  //   testScenario(this.server);
-  //
-  //   await coursesPage.visit();
-  //   await coursesPage.accountDropdown.toggle();
-  //   await coursesPage.accountDropdown.clickOnLink('Manage Subscription');
-  //
-  //   assert.equal(
-  //     window.location.href,
-  //     'https://test.com/billing_session',
-  //     'Clicking manage subscription button should redirect to billing session URL'
-  //   );
-  // });
+  skip('new user can wait for subscriptions to sync after successful checkout session', async function () {
+    signIn(this.owner);
+    testScenario(this.server);
+
+    // await coursesPage.visit({ action: 'checkout_session_successful' });
+    //
+    // const baseRequestsCount = [
+    //   'fetch courses (courses listing page)',
+    //   'fetch repositories (courses listing page)',
+    //   'notify page view (courses listing page)',
+    //   'fetch subscriptions (modal)',
+    // ].length;
+    //
+    // assert.ok(coursesPage.checkoutSessionSuccessfulModal.isVisible, 'Checkout session successful modal should be visible');
+    // assert.equal(this.server.pretender.handledRequests.length, baseRequestsCount);
+    //
+    // await this.clock.tick(1001);
+    // await finishRender();
+    //
+    // assert.equal(this.server.pretender.handledRequests.length, baseRequestsCount + 1);
+    //
+    // this.server.create('subscription', {
+    //   user: this.server.schema.users.first(),
+    //   endedAt: null,
+    //   startDate: new Date(2021, 1, 1),
+    // });
+    //
+    // await this.clock.tick(1001);
+    // await finishRender();
+    //
+    // assert.equal(this.server.pretender.handledRequests.length, baseRequestsCount + 2); // One more subscriptions request
+    // assert.notOk(coursesPage.checkoutSessionSuccessfulModal.isVisible, 'Checkout session successful modal should not be visible');
+    //
+    // await this.clock.tick(1001);
+    // await finishRender();
+    //
+    // assert.equal(this.server.pretender.handledRequests.length, baseRequestsCount + 2); // No more requests after polling is complete
+    // assert.notOk(coursesPage.checkoutSessionSuccessfulModal.isVisible, 'Checkout session successful modal should not be visible');
+  });
+
+  skip('subscriber can manage subscription', async function () {
+    // signInAsSubscriber(this.owner);
+    // testScenario(this.server);
+    //
+    // await coursesPage.visit();
+    // await coursesPage.accountDropdown.toggle();
+    // await coursesPage.accountDropdown.clickOnLink('Manage Subscription');
+    //
+    // assert.equal(
+    //   window.location.href,
+    //   'https://test.com/billing_session',
+    //   'Clicking manage subscription button should redirect to billing session URL'
+    // );
+  });
 });
--- tests/pages/pay-page.js
+++ tests/pages/pay-page.js
@@ -1,6 +1,5 @@
 import { clickable, create, visitable } from 'ember-cli-page-object';
 import AccountDropdown from 'codecrafters-frontend/tests/pages/components/account-dropdown';
-import CheckoutSessionSuccessfulModal from 'codecrafters-frontend/tests/pages/components/checkout-session-successful-modal';
 import Header from 'codecrafters-frontend/tests/pages/components/header';
 
 export default create({
