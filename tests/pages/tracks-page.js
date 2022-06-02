64222aaa7dde5293f0947742289753b216cad846
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-02 19:23:54 +0100

    fix lint errors

--- tests/pages/tracks-page.js
+++ tests/pages/tracks-page.js
@@ -1,9 +1,7 @@
 import { collection, create, visitable } from 'ember-cli-page-object';
 import AccountDropdown from 'codecrafters-frontend/tests/pages/components/account-dropdown';
-import CheckoutSessionSuccessfulModal from 'codecrafters-frontend/tests/pages/components/checkout-session-successful-modal';
 import TrackCard from 'codecrafters-frontend/tests/pages/components/tracks-page/track-card';
 import Header from 'codecrafters-frontend/tests/pages/components/header';
-import SubscribeModal from 'codecrafters-frontend/tests/pages/components/subscribe-modal';
 
 import finishRender from 'codecrafters-frontend/tests/support/finish-render';
 
