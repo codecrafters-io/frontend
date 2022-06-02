85bc29111b91b48d343d0a38bbb0272fa41542eb
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-02 23:02:24 +0100

    Wireup pay button

--- app/controllers/pay.js
+++ app/controllers/pay.js
@@ -1,10 +1,22 @@
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
+import window from 'ember-window-mock';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class PayController extends Controller {
   @service currentUser;
+  @tracked isCreatingCheckoutSession = false;
 
   get testimonials() {
     return this.model.courses.findBy('slug', 'docker').testimonials;
+  }
+
+  @action
+  async handleSubscribeButtonClick() {
+    this.isCreatingCheckoutSession = true;
+    let checkoutSession = this.store.createRecord('checkout-session');
+    await checkoutSession.save();
+    window.location.href = checkoutSession.url;
   }
 }
