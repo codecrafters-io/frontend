981198e38262e1db2cdb9a49bb781191ac0b315b
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-04 01:49:51 +0100

    disable elements in footer too

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,68 +1,84 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
 export default class FooterComponent extends Component {
   get footerLinkGroups() {
-    return [
-      {
-        heading: 'Challenges',
-        links: [
-          {
-            text: 'Git',
-            url: 'https://app.codecrafters.io/courses/git/overview',
-          },
-          {
-            text: 'Redis',
-            url: 'https://app.codecrafters.io/courses/redis/overview',
-          },
-          {
-            text: 'Docker',
-            url: 'https://app.codecrafters.io/courses/docker/overview',
-          },
-          {
-            text: 'SQLite',
-            url: 'https://app.codecrafters.io/courses/sqlite/overview',
-          },
-        ],
-      },
-      {
-        heading: 'Support',
-        links: [
-          {
-            text: 'Docs',
-            url: 'https://docs.codecrafters.io/',
-          },
-          {
-            text: 'Status',
-            url: 'https://status.codecrafters.io/',
-          },
-        ],
-      },
-      {
-        heading: 'Company',
-        links: [
-          {
-            text: 'About',
-            url: 'https://codecrafters.io/about',
-          },
-          {
-            text: 'Changelog',
-            url: 'https://codecrafters.canny.io/changelog',
-          },
-        ],
-      },
-      {
-        heading: 'Legal',
-        links: [
-          {
-            text: 'Terms',
-            url: 'https://codecrafters.io/terms',
-          },
-          {
-            text: 'Privacy',
-            url: 'https://codecrafters.io/privacy',
-          },
-        ],
-      },
-    ];
+    if (this.args.shouldShowChallengeLinkGroup) {
+      return [this.challengeLinkGroup, this.supportLinkGroup, this.companyLinkGroup, this.legalLinkGroup];
+    } else {
+      return [this.supportLinkGroup, this.companyLinkGroup, this.legalLinkGroup];
+    }
+  }
+
+  get challengeLinkGroup() {
+    return {
+      heading: 'Challenges',
+      links: [
+        {
+          text: 'Git',
+          url: 'https://app.codecrafters.io/courses/git/overview',
+        },
+        {
+          text: 'Redis',
+          url: 'https://app.codecrafters.io/courses/redis/overview',
+        },
+        {
+          text: 'Docker',
+          url: 'https://app.codecrafters.io/courses/docker/overview',
+        },
+        {
+          text: 'SQLite',
+          url: 'https://app.codecrafters.io/courses/sqlite/overview',
+        },
+      ],
+    };
+  }
+
+  get supportLinkGroup() {
+    return {
+      heading: 'Support',
+      links: [
+        {
+          text: 'Docs',
+          url: 'https://docs.codecrafters.io/',
+        },
+        {
+          text: 'Status',
+          url: 'https://status.codecrafters.io/',
+        },
+      ],
+    };
+  }
+
+  get companyLinkGroup() {
+    return {
+      heading: 'Company',
+      links: [
+        {
+          text: 'About',
+          url: 'https://codecrafters.io/about',
+        },
+        {
+          text: 'Changelog',
+          url: 'https://codecrafters.canny.io/changelog',
+        },
+      ],
+    };
+  }
+
+  get legalLinkGroup() {
+    return {
+      heading: 'Legal',
+      links: [
+        {
+          text: 'Terms',
+          url: 'https://codecrafters.io/terms',
+        },
+        {
+          text: 'Privacy',
+          url: 'https://codecrafters.io/privacy',
+        },
+      ],
+    };
   }
 }
