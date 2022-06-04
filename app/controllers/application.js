8f42854561c39e093b4d5987cc5e9592f3d2a2a9
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-04 01:39:03 +0100

    use minimal header for the /pay page

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -6,7 +6,12 @@ import { tracked } from '@glimmer/tracking';
 export default class ApplicationController extends Controller {
   queryParams = ['action'];
   @service('globalModals') globalModalsService;
+  @service router;
   @tracked action = null;
+
+  get currentRouteIsPay() {
+    return this.router.currentRouteName === 'pay';
+  }
 
   get isAnyModalOpen() {
     return this.globalModalsService.isAnyModalOpen;
