3cb07ccf8d15571b5070275c1ed4bff7a004845f
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-04 10:09:09 +0100

    strike out original price in pricing card

--- app/models/individual-checkout-session.js
+++ app/models/individual-checkout-session.js
@@ -2,6 +2,7 @@ import Model from '@ember-data/model';
 import { attr } from '@ember-data/model';
 
 export default class IndividualCheckoutSessionModel extends Model {
+  @attr('string') promotionCode;
   @attr('string') successUrl;
   @attr('string') cancelUrl;
   @attr('string') url;
