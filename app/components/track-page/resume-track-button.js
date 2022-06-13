5207de0fedc729423e29418db6cb274cb7e879d7
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-13 13:42:17 +0100

    wireup resume track button

--- app/components/track-page/resume-track-button.js
+++ app/components/track-page/resume-track-button.js
@@ -1,14 +1,23 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import window from 'ember-window-mock';
 
 export default class CourseOverviewResumeTrackButtonComponent extends Component {
+  @service currentUser;
   @service router;
+
+  get activeCourse() {
+    return this.args.courses.find((course) => {
+      return !this.currentUser.record.repositories
+        .filterBy('course', course)
+        .filterBy('language', this.args.language)
+        .some((repository) => repository.allStagesAreComplete);
+    });
+  }
 
   @action
   handleClicked() {
     // TODO: Handle "resume-track functionality?"
-    this.router.transitionTo('course', this.args.courses.firstObject.slug);
+    this.router.transitionTo('course', (this.activeCourse || this.args.courses.lastObject).slug);
   }
 }
