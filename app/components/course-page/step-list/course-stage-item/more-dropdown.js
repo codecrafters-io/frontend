555ea00a676162925533e816ca16c1959963d6f3
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-20 18:47:15 +0100

    allow team members to access solutions too

--- app/components/course-page/step-list/course-stage-item/more-dropdown.js
+++ app/components/course-page/step-list/course-stage-item/more-dropdown.js
@@ -6,15 +6,23 @@ export default class CoursePageStepListCourseStageItemMoreDropdownComponent exte
   @service currentUser;
   @service router;
 
+  get currentUserCanAccessSolution() {
+    if (this.solutionIsOnlyAccessibleToSubscribers) {
+      return this.currentUser.record.hasActiveSubscription || this.currentUser.record.isTeamMember;
+    } else {
+      return true;
+    }
+  }
+
   @action
   handleViewSolutionLinkClicked() {
     if (this.viewSolutionLinkIsEnabled) {
-      if (this.solutionIsOnlyAccessibleToSubscribers && !this.currentUser.isSubscriber) {
-        this.router.transitionTo('pay');
-      } else {
+      if (this.currentUserCanAccessSolution) {
         this.router.transitionTo('course-stage-solution.index', this.args.courseStage.course.slug, this.args.courseStage.slug, {
           queryParams: { language: this.args.repository.get('language.slug') },
         });
+      } else {
+        this.router.transitionTo('pay');
       }
     }
   }
