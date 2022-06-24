414d153f7cbcec3b5520ff38e7e2616c06669462
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-24 18:35:48 +0100

    add team subscription model

---
+++ app/models/team-subscription.js
@@ -0,0 +1,13 @@
+import Model from '@ember-data/model';
+import { attr, belongsTo } from '@ember-data/model';
+
+export default class TeamSubscriptionModel extends Model {
+  @belongsTo('team', { async: false }) team;
+  @attr('date') endedAt;
+  @attr('bool') isLegacy;
+  @attr('date') startDate;
+
+  get isActive() {
+    return !this.endedAt;
+  }
+}
