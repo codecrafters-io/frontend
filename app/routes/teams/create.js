942c6d2d7c2f5dc6fa069e220a11c0c7da50e366
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-06 22:21:46 +0100

    scroll to top on team create click

--- app/routes/teams/create.js
+++ app/routes/teams/create.js
@@ -4,6 +4,10 @@ import ApplicationRoute from 'codecrafters-frontend/lib/application-route';
 export default class TeamsCreateRoute extends ApplicationRoute {
   @service store;
 
+  activate() {
+    window.scrollTo({ top: 0 });
+  }
+
   async model() {
     const teams = await this.store.findAll('team', { include: 'memberships.user,subscriptions' });
 
