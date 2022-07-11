dce1087338c6eb80cd19ac27ad65c614ccb7db75
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-11 21:21:48 +0100

    fix sentry trackking

--- app/app.js
+++ app/app.js
@@ -2,9 +2,18 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'codecrafters-frontend/config/environment';
-import { InitSentryForEmber } from '@sentry/ember';
+import * as Sentry from '@sentry/ember';
 
-InitSentryForEmber();
+const sentryDSN = 'https://478cca7283ca40209deae5160b54ee4f@o294739.ingest.sentry.io/5922961';
+
+if (config.environment === 'development' || config.environment === 'production') {
+  Sentry.init({
+    autoSessionTracking: true,
+    dsn: config.environment === 'development' || config.environment === 'production' ? sentryDSN : '',
+    release: config.environment,
+    tracesSampleRate: 1.0,
+  });
+}
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
