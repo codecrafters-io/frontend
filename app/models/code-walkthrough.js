42d537875f9c34d96444906660a737bc73d971f8
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-15 21:43:48 +0100

    Add slug + introductionMarkdown to source walkthroughs

--- app/models/code-walkthrough.js
+++ app/models/code-walkthrough.js
@@ -2,5 +2,7 @@ import { attr } from '@ember-data/model';
 import Model from '@ember-data/model';
 
 export default class CodeWalkthrough extends Model {
+  @attr('string') introductionMarkdown;
   @attr('') sections; // free-form JSON
+  @attr('string') slug;
 }
