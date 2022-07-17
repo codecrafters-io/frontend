37c91350d61350e95ecea4c2c10805757923bf4f
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-17 23:03:50 +0100

    separate code walkthrough intro/conclusion

--- app/models/code-walkthrough.js
+++ app/models/code-walkthrough.js
@@ -1,5 +1,7 @@
 import { attr } from '@ember-data/model';
+import { htmlSafe } from '@ember/template';
 import Model from '@ember-data/model';
+import showdown from 'showdown';
 
 export default class CodeWalkthrough extends Model {
   @attr('string') conclusionMarkdown;
@@ -10,4 +12,20 @@ export default class CodeWalkthrough extends Model {
   @attr('string') slug;
   @attr('string') title;
   @attr('date') updatedAt;
+
+  get introductionHTML() {
+    if (this.introductionMarkdown) {
+      return htmlSafe(new showdown.Converter({ openLinksInNewWindow: true }).makeHtml(this.introductionMarkdown));
+    } else {
+      return null;
+    }
+  }
+
+  get conclusionHTML() {
+    if (this.conclusionMarkdown) {
+      return htmlSafe(new showdown.Converter({ openLinksInNewWindow: true }).makeHtml(this.conclusionMarkdown));
+    } else {
+      return null;
+    }
+  }
 }
