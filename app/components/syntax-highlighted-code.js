f34d8ed7f5046feaecc1e111762811dba46fb369
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-17 02:33:42 +0100

    update styles for code walkthrough

--- app/components/syntax-highlighted-code.js
+++ app/components/syntax-highlighted-code.js
@@ -11,12 +11,25 @@ export default class SyntaxHighlightedCodeComponent extends Component {
 
     shiki.setCDN('https://unpkg.com/shiki/');
 
-    shiki.getHighlighter({ theme: 'github-light', langs: ['c'] }).then((highlighter) => {
-      this.asyncHighlightedHTML = htmlSafe(highlighter.codeToHtml(this.args.code, { lang: 'c' }));
+    const lineOptions = (this.args.highlightedLines || '')
+      .split(',')
+      .flatMap((lineOrBlock) => [{ line: parseInt(lineOrBlock), classes: ['highlighted'] }]);
+
+    shiki.getHighlighter({ theme: 'one-dark-pro', langs: ['c'] }).then((highlighter) => {
+      this.asyncHighlightedHTML = htmlSafe(highlighter.codeToHtml(this.args.code, { lang: 'c', lineOptions: lineOptions }));
     });
   }
 
+  get temporaryHTML() {
+    const linesHTML = this.args.code
+      .split('\n')
+      .map((line) => `<div class="line">${line}</div>`)
+      .join('');
+
+    return htmlSafe(`<pre><code>${linesHTML}</code></pre>`);
+  }
+
   get highlightedHtml() {
-    return this.asyncHighlightedHTML || this.args.code;
+    return this.asyncHighlightedHTML || this.temporaryHTML;
   }
 }
