cfc0cfd033b5952841e53c9c5e9b6da25f2490a8
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-17 03:41:30 +0100

    cache shiki highlighters

--- app/components/syntax-highlighted-code.js
+++ app/components/syntax-highlighted-code.js
@@ -3,6 +3,13 @@ import Component from '@glimmer/component';
 import * as shiki from 'shiki';
 import { tracked } from '@glimmer/tracking';
 
+/**
+ * getHighlighter() is the most expensive step of Shiki. Instead of calling it on every page,
+ * cache it here as much as possible. Make sure that your highlighters can be cached, state-free.
+ * We make this async, so that multiple calls to parse markdown still share the same highlighter.
+ */
+const highlighterCacheAsync = new Map();
+
 export default class SyntaxHighlightedCodeComponent extends Component {
   @tracked asyncHighlightedHTML;
 
@@ -11,11 +18,20 @@ export default class SyntaxHighlightedCodeComponent extends Component {
 
     shiki.setCDN('https://unpkg.com/shiki/');
 
+    const cacheID = 'only-one-entry-for-now';
+
+    let highlighterAsync = highlighterCacheAsync.get(cacheID);
+
+    if (!highlighterAsync) {
+      highlighterAsync = shiki.getHighlighter({ theme: 'one-dark-pro', langs: ['c'] });
+      highlighterCacheAsync.set(cacheID, highlighterAsync);
+    }
+
     const lineOptions = (this.args.highlightedLines || '')
       .split(',')
       .flatMap((lineOrBlock) => [{ line: parseInt(lineOrBlock), classes: ['highlighted'] }]);
 
-    shiki.getHighlighter({ theme: 'one-dark-pro', langs: ['c'] }).then((highlighter) => {
+    highlighterAsync.then((highlighter) => {
       this.asyncHighlightedHTML = htmlSafe(highlighter.codeToHtml(this.args.code, { lang: 'c', lineOptions: lineOptions }));
     });
   }
