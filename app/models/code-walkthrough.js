913e9eba85c3f5c5bea3b3bdc54ee4da1c0546f2
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-17 22:59:00 +0100

    wireup description Markdown

--- app/models/code-walkthrough.js
+++ app/models/code-walkthrough.js
@@ -2,10 +2,12 @@ import { attr } from '@ember-data/model';
 import Model from '@ember-data/model';
 
 export default class CodeWalkthrough extends Model {
+  @attr('string') conclusionMarkdown;
+  @attr('string') descriptionMarkdown;
+  @attr('string') hackerNewsUrl;
   @attr('string') introductionMarkdown;
   @attr('') sections; // free-form JSON
   @attr('string') slug;
   @attr('string') title;
   @attr('date') updatedAt;
-  @attr('string') hackerNewsUrl;
 }
