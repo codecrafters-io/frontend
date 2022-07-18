95877cd6b8025f1da6aff175ad0d3620eddc1d86
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-18 19:07:20 +0100

    Add authorDetails & reviewersDetails fields

--- app/models/course-stage-solution.js
+++ app/models/course-stage-solution.js
@@ -6,8 +6,15 @@ export default class CourseStageSolutionModel extends Model {
   @belongsTo('language', { async: false }) language;
   @attr('string') explanationMarkdown;
   @attr('') changedFiles; // free-form JSON
+  @attr('') authorDetails; // free-form JSON
+  @attr('') reviewersDetails; // free-form JSON
 
   get hasExplanation() {
     return !!this.explanationMarkdown;
+  }
+
+  // For now we only support one reviewer, might have more in the future
+  get reviewerDetails() {
+    return this.reviewersDetails ? this.reviewersDetails.firstObject : null;
   }
 }
