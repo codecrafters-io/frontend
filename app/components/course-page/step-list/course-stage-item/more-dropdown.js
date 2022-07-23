3c29ca543298d608155e6625ca1ca2a9fc0767f7
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-23 10:31:14 +0530

    fix canAccessSubOnlyContent

--- app/components/course-page/step-list/course-stage-item/more-dropdown.js
+++ app/components/course-page/step-list/course-stage-item/more-dropdown.js
@@ -8,7 +8,7 @@ export default class CoursePageStepListCourseStageItemMoreDropdownComponent exte
 
   get currentUserCanAccessSolutionAndWalkthrough() {
     if (this.solutionIsOnlyAccessibleToSubscribers) {
-      return this.currentUser.canAccessSubscriberOnlyContent;
+      return this.currentUser.record.canAccessSubscriberOnlyContent;
     } else {
       return true;
     }
