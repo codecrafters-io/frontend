b10d8edd07e5700818813a8f9769ca47793a161c
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-28 15:35:09 +0800

    remove unused var

--- app/routes/membership.js
+++ app/routes/membership.js
@@ -1,5 +1,4 @@
 import ApplicationRoute from 'codecrafters-frontend/lib/application-route';
-import window from 'ember-window-mock';
 import { inject as service } from '@ember/service';
 
 export default class MembershipRoute extends ApplicationRoute {
