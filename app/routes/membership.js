3bedef3d077ec9cef5dc5cc3b6457b82bd05bf00
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-28 15:33:04 +0800

    redirect to /pay if user does not have subscriptions

--- app/routes/membership.js
+++ app/routes/membership.js
@@ -1,3 +1,16 @@
 import ApplicationRoute from 'codecrafters-frontend/lib/application-route';
+import window from 'ember-window-mock';
+import { inject as service } from '@ember/service';
 
-export default class MembershipRoute extends ApplicationRoute {}
+export default class MembershipRoute extends ApplicationRoute {
+  @service router;
+  @service('current-user') currentUserService;
+
+  beforeModel() {
+    super.beforeModel(...arguments);
+
+    if (this.currentUserService.record.subscriptions.length === 0) {
+      this.router.transitionTo('pay');
+    }
+  }
+}
