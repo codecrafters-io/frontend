bcd3f03473917611411e79ddef37523d30f4ca5f
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-29 22:00:29 +0800

    remove action query param

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,13 +1,9 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 
 export default class ApplicationController extends Controller {
-  queryParams = ['action'];
   @service('globalModals') globalModalsService;
   @service router;
-  @tracked action = null;
 
   get currentRouteIsPay() {
     return this.router.currentRouteName === 'pay';
