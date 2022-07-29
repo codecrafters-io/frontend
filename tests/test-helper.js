ce976cffaddd85365bb9715443c1a8cae72b9aa5
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-29 13:43:04 +0800

    Add ember exam

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,7 +3,7 @@ import config from 'codecrafters-frontend/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
-import { start } from 'ember-qunit';
+import start from 'ember-exam/test-support/start';
 
 import 'qunit-assertions-extra';
 
