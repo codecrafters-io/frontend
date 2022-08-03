105df26de507bfb75757895ba5fa134bf9de6b85
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-08-03 11:40:09 +0800

    force subscription sync after membership page is loaded

--- app/routes/membership.js
+++ app/routes/membership.js
@@ -2,6 +2,7 @@ import ApplicationRoute from 'codecrafters-frontend/lib/application-route';
 import { inject as service } from '@ember/service';
 
 export default class MembershipRoute extends ApplicationRoute {
+  @service store;
   @service router;
   @service('current-user') currentUserService;
 
@@ -11,5 +12,10 @@ export default class MembershipRoute extends ApplicationRoute {
     if (this.currentUserService.record.subscriptions.length === 0) {
       this.router.transitionTo('pay');
     }
+  }
+
+  afterModel() {
+    // Force a sync of subscriptions
+    this.store.findAll('subscription');
   }
 }
