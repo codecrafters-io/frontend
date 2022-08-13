7cba685043bacb80e34a5f5763cd91b9c4405edf
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-08-13 23:40:54 +0100

    Handle ended pilots

--- tests/acceptance/team-page/manage-team-billing-test.js
+++ tests/acceptance/team-page/manage-team-billing-test.js
@@ -53,4 +53,29 @@ module('Acceptance | team-page | manage-team-billing-test', function (hooks) {
     assert.ok(teamPage.pilotDetailsContainer.isPresent, 'pilot details are visible');
     assert.equal(teamPage.pilotDetailsContainer.detailsText, "Your team's pilot is valid until January 1, 2099 12:00 AM.");
   });
+
+  test('team with expired pilot sees pilot details with payment method prompt', async function (assert) {
+    testScenario(this.server);
+
+    const user = this.server.schema.users.find('63c51e91-e448-4ea9-821b-a80415f266d3');
+    const team = this.server.create('team', { id: 'dummy-team-id', name: 'Dummy Team' });
+    this.server.schema.teamPilots.create({ team: team, endDate: new Date(1999, 0, 1) });
+
+    this.server.create('team-membership', {
+      createdAt: new Date(),
+      id: 'dummy-team-membership-id',
+      user: user,
+      team: team,
+      isAdmin: true,
+    });
+
+    signIn(this.owner, this.server, user);
+
+    await coursesPage.visit();
+    await coursesPage.accountDropdown.toggle();
+    await coursesPage.accountDropdown.clickOnLink('Manage Team');
+
+    assert.ok(teamPage.pilotDetailsContainer.isPresent, 'pilot details are visible');
+    assert.equal(teamPage.pilotDetailsContainer.detailsText, "Your team's pilot ended on January 1, 1999.");
+  });
 });
