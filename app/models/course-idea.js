5b9633ea26eb41c909452bcbe341fbef1cbcc8ba
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-09-16 22:23:45 +0100

    wireup course idea cards

--- app/models/course-idea.js
+++ app/models/course-idea.js
@@ -1,9 +1,15 @@
 import Model from '@ember-data/model';
+import showdown from 'showdown';
 import { attr } from '@ember-data/model';
+import { htmlSafe } from '@ember/template';
 
 export default class CourseIdeaModel extends Model {
-  @attr('string') name;
-  @attr('string') descriptionMarkdown;
   @attr('date') createdAt;
+  @attr('string') descriptionMarkdown;
   @attr('boolean') isArchived;
+  @attr('string') name;
+
+  get descriptionHtml() {
+    return htmlSafe(new showdown.Converter({ openLinksInNewWindow: true }).makeHtml(this.descriptionMarkdown));
+  }
 }
