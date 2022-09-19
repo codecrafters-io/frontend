aeb3241d3a2dcad7cec84a0bf902cd2bb1d2ac7c
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-09-19 06:35:04 +0100

    remove unused props

--- app/components/course-ideas-page/course-idea-card/supervote-button.js
+++ app/components/course-ideas-page/course-idea-card/supervote-button.js
@@ -1,12 +1,6 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class SupervoteButtonComponent extends Component {
   @service('current-user') currentUserService;
-  @service router;
-  @service store;
-
-  @tracked isSupervoting = false;
 }
