19a9d052a26a1d744160a910e3f7cc0b96ddc3d1
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-09-19 06:40:31 +0100

    fix failed tests

--- mirage/utils/create-course-ideas.js
+++ mirage/utils/create-course-ideas.js
@@ -3,6 +3,7 @@ import courseIdeasData from '../data/course-ideas';
 export default function createCourseIdeas(server) {
   for (const courseIdeaData of courseIdeasData) {
     server.create('course-idea', {
+      createdAt: new Date(),
       name: courseIdeaData.name,
       descriptionMarkdown: courseIdeaData.description_md,
     });
