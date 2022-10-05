0d299f64693aeb9129cc1ab76a4a3cf14972a701
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-10-05 12:00:42 +0100

    handle status field

--- app/models/charge.js
+++ app/models/charge.js
@@ -1,4 +1,5 @@
 import { attr, belongsTo } from '@ember-data/model';
+import { equal } from '@ember/object/computed'; // eslint-disable-line ember/no-computed-properties-in-native-classes
 import Model from '@ember-data/model';
 
 export default class Charge extends Model {
@@ -6,7 +7,12 @@ export default class Charge extends Model {
   @attr('number') amount;
   @attr('string') currency;
   @attr('string') invoiceId;
+  @attr('string') status;
   @belongsTo('user') user;
+
+  @equal('status', 'failed') statusIsFailed;
+  @equal('status', 'pending') statusIsPending;
+  @equal('status', 'succeeded') statusIsSucceeded;
 
   get amountInDollars() {
     return this.amount / 100;
