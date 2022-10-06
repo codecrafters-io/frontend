241a30ac0053bb75f687c613f3f908e53f5839d8
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-10-06 20:56:50 +0100

    wireup submit course idea card

--- app/controllers/vote/course-ideas.js
+++ app/controllers/vote/course-ideas.js
@@ -1,6 +1,8 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default class CourseIdeasController extends Controller {
+  @service('current-user') currentUserService;
   get orderedCourseIdeas() {
     return this.model.courseIdeas.rejectBy('isArchived').sortBy('reverseSortPositionForCourseIdeasPage').reverse();
   }
