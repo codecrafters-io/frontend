d421e523a910e1f8e557fffa3996b40c47d53d13
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-11-15 00:13:34 +0000

    lint fix

--- app/components/course-page/course-stage-solution-modal/community-solution-card.js
+++ app/components/course-page/course-stage-solution-modal/community-solution-card.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 
 export default class CommunitySolutionCardComponent extends Component {
   @tracked isExpanded = false;
