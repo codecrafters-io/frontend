4db67a0d2ba63af9d5ccdbe5f1c9d779c1c1ef1d
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-11-23 17:55:20 +0000

    persist language too

--- app/models/course-stage-comment.js
+++ app/models/course-stage-comment.js
@@ -4,7 +4,6 @@ import Model from '@ember-data/model';
 export default class CourseStageCommentModel extends Model {
   @belongsTo('course-stage', { async: false }) courseStage;
   @belongsTo('language', { async: false }) language;
-  @belongsTo('repository', { async: false }) repository;
   @belongsTo('user', { async: false }) user;
 
   @attr('string') bodyMarkdown;
