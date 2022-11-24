099591b9c3ea8f43c2c2c5d73092333f31de8c02
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-11-24 18:56:31 +0000

    lint fixes

--- app/components/course-page/course-stage-solution-modal/comment-card/upvote-button.js
+++ app/components/course-page/course-stage-solution-modal/comment-card/upvote-button.js
@@ -1,6 +1,5 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { htmlSafe } from '@ember/template';
 import { inject as service } from '@ember/service';
 
 export default class UpvoteButtonComponent extends Component {
