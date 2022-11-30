94dcabf65469e02dac27bad600029e398e00fc89
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-11-30 21:50:17 +0000

    use jpg files

--- app/lib/testimonials-data.js
+++ app/lib/testimonials-data.js
@@ -8,13 +8,13 @@ const testimonials = {
   'raghav-dua': {
     authorName: 'Raghav Dua',
     authorDesignation: 'SRE at Coinbase',
-    authorAvatarImage: '/assets/images/user-avatars/raghav-dua.jpeg',
+    authorAvatarImage: '/assets/images/user-avatars/raghav-dua.jpg',
     text: `I spent a full day on your Docker building course and ended up building the whole thing myself. As a SRE (and mostly a user of docker), digging into the internals blew me away.`,
   },
   'djordje-lukic': {
     authorName: 'Djordje Lukic (@rumpl)',
     authorDesignation: 'Full-time Docker contributor',
-    authorAvatarImage: '/assets/images/user-avatars/djorde-lukic.jpeg',
+    authorAvatarImage: '/assets/images/user-avatars/djorde-lukic.jpg',
     text: `These guys ruined my weekend`,
   },
 
@@ -28,56 +28,56 @@ const testimonials = {
   'rahul-tarak': {
     authorName: 'Rahul Tarak',
     authorDesignation: 'Pioneer.app & ODX1 Fellow',
-    authorAvatarImage: '/assets/images/user-avatars/rahul-tarak.jpeg',
+    authorAvatarImage: '/assets/images/user-avatars/rahul-tarak.jpg',
     text: "The Redis challenge was a great way to procrastinate sleeping for a week! A good change of pace from my regular work, and allowed me to explore some cool tech. I'll be back for more.",
   },
 
   'pranjal-paliwal': {
     authorName: 'Pranjal Paliwal',
     authorDesignation: '$35k winner of HackAtom',
-    authorAvatarImage: '/assets/images/user-avatars/pranjal-paliwal.jpeg',
+    authorAvatarImage: '/assets/images/user-avatars/pranjal-paliwal.jpg',
     text: 'My favorite way to master a language.',
   },
 
   'kang-ming-tay': {
     authorName: 'Kang Ming Tay',
     authorDesignation: 'Software Engineer at Supabase',
-    authorAvatarImage: '/assets/images/user-avatars/kangming.jpeg',
+    authorAvatarImage: '/assets/images/user-avatars/kangming.jpg',
     text: 'The Docker challenge helped me dive into its internals, through *actual* practice. Super fun.',
   },
 
   'ananthalakshmi-sankar': {
     authorName: 'Ananthalakshmi Sankar',
     authorDesignation: 'Automation Engineer at Apple',
-    authorAvatarImage: '/assets/images/user-avatars/ananthalakshmi-sankar.jpeg',
+    authorAvatarImage: '/assets/images/user-avatars/ananthalakshmi-sankar.jpg',
     text: "There are few sites I like as much that have a step by step guide. The real-time feedback is so good, it's creepy!",
   },
 
   'beyang-liu': {
     authorName: 'Beyang Liu',
     authorDesignation: 'CTO at SourceGraph',
-    authorAvatarImage: '/assets/images/user-avatars/beyang-liu.jpeg',
+    authorAvatarImage: '/assets/images/user-avatars/beyang-liu.jpg',
     text: 'Found out from a colleague. It has you build your own version of things like Git and Docker from scratch. A cool way to build a stronger mental model of how those tools work.',
   },
 
   'akshata-mohan': {
     authorName: 'Akshata Mohan',
     authorDesignation: 'Senior Data Scientist at Cloudflare',
-    authorAvatarImage: '/assets/images/user-avatars/akshata-mohan.jpeg',
+    authorAvatarImage: '/assets/images/user-avatars/akshata-mohan.jpg',
     text: "I'm learning about how Redis works under the hood, system calls, socket programming in Python — something I've never done before",
   },
 
   'jonathan-lorimer': {
     authorName: 'Jonathan Lorimer',
     authorDesignation: 'Lead SWE at Mercury Bank',
-    authorAvatarImage: '/assets/images/user-avatars/jonathan.jpeg',
+    authorAvatarImage: '/assets/images/user-avatars/jonathan.jpg',
     text: `I was really impressed that they support Haskell, and will probably usethis to learn Rust! The git-based workflow is :chefkiss:`,
   },
 
   'patrick-burris': {
     authorName: 'Patrick Burris',
     authorDesignation: 'Senior Software Developer at CenturyLink',
-    authorAvatarImage: '/assets/images/user-avatars/patrick-burris.jpeg',
+    authorAvatarImage: '/assets/images/user-avatars/patrick-burris.jpg',
     text: "The instant feedback right there in the git push is really cool. Didn't even know that was possible!",
   },
 };
