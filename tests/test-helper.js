6d9daa5623da5ca37a36e043046a0a9a03bab4fd
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-05-07 16:51:35 +0100

    changes

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,7 @@
 import Application from 'codecrafters-frontend/app';
 import config from 'codecrafters-frontend/config/environment';
 import * as QUnit from 'qunit';
+import { forceModulesToBeLoaded, sendCoverage } from 'ember-cli-code-coverage/test-support';
 import { setApplication } from '@ember/test-helpers';
 import { setup as setupQunitDom } from 'qunit-dom';
 import { setup as setupQunitAssertionsExtra } from 'qunit-assertions-extra';
@@ -13,3 +14,8 @@ setupQunitDom(QUnit.assert);
 setupQunitAssertionsExtra(QUnit.assert);
 
 start();
+
+QUnit.done(async function () {
+  forceModulesToBeLoaded();
+  await sendCoverage();
+});
