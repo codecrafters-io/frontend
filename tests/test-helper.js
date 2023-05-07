4a5e223b9d430fdadff1faa8ab51e46ff2bfe4cc
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-05-07 16:35:13 +0100

    changes

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,13 +2,14 @@ import Application from 'codecrafters-frontend/app';
 import config from 'codecrafters-frontend/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
-import { setup } from 'qunit-dom';
+import { setup as setupQunitDom } from 'qunit-dom';
+import { setup as setupQunitAssertionsExtra } from 'qunit-assertions-extra';
 
 import start from 'ember-exam/test-support/start';
-import 'qunit-assertions-extra';
 
 setApplication(Application.create(config.APP));
 
-setup(QUnit.assert);
+setupQunitDom(QUnit.assert);
+setupQunitAssertionsExtra(QUnit.assert);
 
 start();
