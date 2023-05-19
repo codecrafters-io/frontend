f854a552c710940078009531c76c45fde53e16fe
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-05-19 19:42:39 +0100

    Remove unused imports in BadgeCardComponent

--- app/components/badges-page/badge-card.js
+++ app/components/badges-page/badge-card.js
@@ -1,7 +1,5 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 
 export default class BadgeCardComponent extends Component {
   @service store;
