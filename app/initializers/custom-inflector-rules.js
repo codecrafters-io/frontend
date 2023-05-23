44b60441a035eda7c19ead1cbd2cd53b691a007f
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-05-23 01:40:10 +0100

    Update custom inflector rules for Ember app.

--- app/initializers/custom-inflector-rules.js
+++ app/initializers/custom-inflector-rules.js
@@ -1,9 +1,8 @@
-// import Inflector from 'ember-inflector';
+import Inflector from 'ember-inflector';
 
 export function initialize(/* application */) {
-  // We aren't using free-usage-quota anymore, keeping this here for easy reference
-  // const inflector = Inflector.inflector;
-  // inflector.irregular('quota', 'quotas'); // By default, the singular form of quota is quotom.
+  const inflector = Inflector.inflector;
+  inflector.irregular('quota', 'quotas'); // By default, the singular form of quota is quotom.
 }
 
 export default {
