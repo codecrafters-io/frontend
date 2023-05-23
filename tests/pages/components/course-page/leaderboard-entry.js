47bfcb9d994ab89a3f1b6f5580b76887bb97fbe0
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-05-23 01:24:00 +0100

    install date-fns

--- tests/pages/components/course-page/leaderboard-entry.js
+++ tests/pages/components/course-page/leaderboard-entry.js
@@ -1,10 +1,9 @@
 import { hasClass, text } from 'ember-cli-page-object';
-import LeaderboardEntry from 'codecrafters-frontend/tests/pages/components/course-page/leaderboard-entry';
 
 export default {
   progressText: text('[data-test-progress-text]'),
   statusIsActive: hasClass('border-yellow-500', '[data-test-progress-bar-container]'),
   statusIsIdle: hasClass('border-teal-500', '[data-test-progress-bar-container]'),
-  username: text('[data-test-username]', LeaderboardEntry),
+  username: text('[data-test-username]'),
   scope: '[data-test-leaderboard-entry]',
 };
