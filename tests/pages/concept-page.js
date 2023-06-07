394bc4f56a59609e0b5e024cb6c47a19f62f12ed
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-06-07 14:28:18 +0100

    update

--- tests/pages/concept-page.js
+++ tests/pages/concept-page.js
@@ -1,8 +1,13 @@
-import { clickable, visitable } from 'ember-cli-page-object';
+import { collection, clickable, clickOnText, visitable } from 'ember-cli-page-object';
 import createPage from 'codecrafters-frontend/tests/support/create-page';
 
 export default createPage({
   clickOnContinueButton: clickable('[data-test-continue-button]'),
+  questionCards: collection('[data-test-question-card]', {
+    selectOption: clickOnText('[data-test-question-card-option]'),
+    clickOnSubmitButton: clickable('[data-test-question-card-submit-button]'),
+    clickOnContinueButton: clickable('[data-test-question-card-continue-button]'),
+  }),
 
   visit: visitable('/concept/:slug'),
 });
