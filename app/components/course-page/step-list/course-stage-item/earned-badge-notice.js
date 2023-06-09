cd5be08a1b72676df1761ee0bb3013c048902151
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-06-09 17:15:26 +0100

    add component file

---
+++ app/components/course-page/step-list/course-stage-item/earned-badge-notice.js
@@ -0,0 +1,21 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class EarnedBadgeNotice extends Component {
+  @tracked selectedBadge;
+
+  @action
+  handleBadgeClicked(badge) {
+    this.selectedBadge = badge;
+  }
+
+  @action
+  handleViewButtonClicked() {
+    this.selectedBadge = this.args.badgeAwards[0];
+  }
+
+  handleBadgeModalClose() {
+    this.selectedBadge = null;
+  }
+}
