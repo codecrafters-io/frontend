48f08f46453eb03c11134307deef960865e7b180
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-06-09 16:44:20 +0100

    Fix badge display and add new gold badge icon.

--- app/services/feature-flags.js
+++ app/services/feature-flags.js
@@ -27,7 +27,6 @@ export default class FeatureFlagsService extends Service {
   }
 
   get canSeeStageCompletionVideos() {
-    return false;
     return this.currentUser && this.currentUser.isStaff;
   }
 
