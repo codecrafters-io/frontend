de7a3fc5401d61e53fb4cf851f8b9af19da170d7
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-06-09 15:04:21 +0100

    Update badge earned modal styling and add descriptionMarkdown to badges in acceptance test.

--- tests/acceptance/view-badges-test.js
+++ tests/acceptance/view-badges-test.js
@@ -6,11 +6,28 @@ import { setupMirage } from 'ember-cli-mirage/test-support';
 import { signInAsStaff } from 'codecrafters-frontend/tests/support/authentication-helpers';
 
 function createBadges(server) {
-  server.create('badge', { name: 'The Turing Badge', shortDescription: 'Complete 3 stages within a day', slug: 'three-in-a-day' });
+  server.create('badge', {
+    name: 'The Turing Badge',
+    shortDescription: 'Complete 3 stages within a day',
+    slug: 'three-in-a-day',
+    descriptionMarkdown: '',
+  });
   server.create('badge', { name: 'The Hamilton Badge', shortDescription: 'Complete 5 stages in a week', slug: 'five-in-a-week' });
   server.create('badge', { name: 'The Curie Badge', shortDescription: 'Complete a stage despite a failed attempt', slug: 'persistent' });
   server.create('badge', { name: 'The Tesla Badge', shortDescription: 'Complete a stage on first attempt', slug: 'right-the-first-time' });
   server.create('badge', { name: 'The Hopper Badge', shortDescription: 'Complete a stage in under a minute', slug: 'quick-to-start' });
+
+  server.schema.badges.all().models.forEach((badge) => {
+    badge.update({
+      descriptionMarkdown: `
+**Granted in honor of the Nobel laureate Marie Curie.**
+
+When World War I led to the destruction of her laboratory, Curie didn’t falter. Instead, she set up
+mobile radiography units to aid the war effort, continuing her research under challenging circumstances.
+      `,
+    });
+    badge.save();
+  });
 }
 
 module('Acceptance | view-badges', function (hooks) {
@@ -42,5 +59,6 @@ module('Acceptance | view-badges', function (hooks) {
     assert.strictEqual(1, 1);
 
     await badgesPage.clickOnBadge('The Turing Badge');
+    await this.pauseTest();
   });
 });
