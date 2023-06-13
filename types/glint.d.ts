be4d391225a2ca9cdefd3ca1bc4d2c078275f4a4
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-06-13 22:19:06 +0100

    Add concept animation block style and fix formatting in concept component and typescript definition file.

--- types/glint.d.ts
+++ types/glint.d.ts
@@ -1,9 +1,11 @@
 import type RenderModifiersRegistry from '@ember/render-modifiers/template-registry';
 import type { ModifierLike } from '@glint/template';
+import type { HelperLike } from '@glint/template';
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry extends RenderModifiersRegistry {
     'in-viewport': ModifierLike<{ Args: { Named: { onEnter: () => void } } }>;
+    'html-safe': HelperLike<{ Return: string; Args: { Positional: [string | undefined] } }>;
     // ...
   }
 }
