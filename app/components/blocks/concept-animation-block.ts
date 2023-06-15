afe80c256d8206c9f310e1b23d840e2075d403bf
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-06-15 13:20:25 +0100

    Add TCP overview animations to ConceptAnimationBlock.

--- app/components/blocks/concept-animation-block.ts
+++ app/components/blocks/concept-animation-block.ts
@@ -20,6 +20,23 @@ import networkingProtocolsLayer3 from '/assets/concept-animations/network-protoc
 // @ts-ignore
 import networkingProtocolsLayer4 from '/assets/concept-animations/network-protocols/layer-4.lottie.json';
 
+// @ts-ignore
+import tcpOverviewConnectionIdentifiers from '/assets/concept-animations/tcp-overview/connection-identifiers.lottie.json';
+// @ts-ignore
+import tcpOverviewHandshake1 from '/assets/concept-animations/tcp-overview/handshake-1.lottie.json';
+// @ts-ignore
+import tcpOverviewHandshake2 from '/assets/concept-animations/tcp-overview/handshake-2.lottie.json';
+// @ts-ignore
+import tcpOverviewHandshake3 from '/assets/concept-animations/tcp-overview/handshake-3.lottie.json';
+// @ts-ignore
+import tcpOverviewOrderedDelivery from '/assets/concept-animations/tcp-overview/ordered-delivery.lottie.json';
+// @ts-ignore
+import tcpOverviewPacket from '/assets/concept-animations/tcp-overview/packet.lottie.json';
+// @ts-ignore
+import tcpOverviewReliableDelivery from '/assets/concept-animations/tcp-overview/reliable-delivery.lottie.json';
+// @ts-ignore
+import tcpOverviewServerClient from '/assets/concept-animations/tcp-overview/server-client.lottie.json';
+
 interface Signature {
   Element: HTMLDivElement;
 
@@ -44,6 +61,14 @@ export default class ConceptAnimationBlockComponent extends Component<Signature>
       'network-protocols/layer-2': networkingProtocolsLayer2,
       'network-protocols/layer-3': networkingProtocolsLayer3,
       'network-protocols/layer-4': networkingProtocolsLayer4,
+      'tcp-overview/connection-identifiers': tcpOverviewConnectionIdentifiers,
+      'tcp-overview/handshake-1': tcpOverviewHandshake1,
+      'tcp-overview/handshake-2': tcpOverviewHandshake2,
+      'tcp-overview/handshake-3': tcpOverviewHandshake3,
+      'tcp-overview/ordered-delivery': tcpOverviewOrderedDelivery,
+      'tcp-overview/packet': tcpOverviewPacket,
+      'tcp-overview/reliable-delivery': tcpOverviewReliableDelivery,
+      'tcp-overview/server-client': tcpOverviewServerClient,
     }[this.args.model.conceptAnimationSlug];
   }
 
