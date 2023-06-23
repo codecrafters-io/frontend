a055db526f69a08ea51f783e4e6e0a486eb6346e
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-06-23 13:28:54 +0100

    Add GitHub app installation button functionality

--- app/components/course-page/configure-github-integration-modal/install-github-app-step.ts
+++ app/components/course-page/configure-github-integration-modal/install-github-app-step.ts
@@ -14,6 +14,7 @@ interface Signature {
 export default class InstallGitHubAppStepComponent extends Component<Signature> {
   @action
   handleInstallGitHubAppButtonClick() {
+    // @ts-ignore
     window.location.href = `${config.x.backendUrl}/github_app_installations/start?repository_id=${this.args.repository.id}`;
   }
 }
