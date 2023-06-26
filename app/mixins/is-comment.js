61ce487cde8a2b522a55371bddf70b9b968fc98f
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-06-26 22:02:19 +0100

    Refactor comment card component and mixins for better approval status handling.

--- app/mixins/is-comment.js
+++ app/mixins/is-comment.js
@@ -1,30 +1,41 @@
 /* eslint-disable ember/no-new-mixins */
 import Mixin from '@ember/object/mixin';
 import { attr } from '@ember-data/model';
+import { underscore } from '@ember/string';
+import { pluralize } from 'ember-inflector';
+import config from '../config/environment';
 
 export default Mixin.create({
-  isApprovedByModerator: attr('boolean'),
+  approvalStatus: attr('string'),
   downvotesCount: attr('number'),
   upvotesCount: attr('number'),
   bodyMarkdown: attr('string'),
+
+  get adminPanelUrl() {
+    return `${config.x.backendUrl}/admin/${pluralize(underscore(this.constructor.modelName))}/${this.id}`;
+  },
+
+  get adminPanelEditUrl() {
+    return `${config.x.backendUrl}/admin/${pluralize(underscore(this.constructor.modelName))}/${this.id}/edit`;
+  },
 
   get childComments() {
     return this.target.comments.filter((comment) => comment.parentComment && comment.parentComment.id === this.id);
   },
 
+  get isApproved() {
+    return this.approvalStatus === 'approved';
+  },
+
+  get isAwaitingApproval() {
+    return this.approvalStatus === 'awaiting_approval';
+  },
+
+  get isRejected() {
+    return this.approvalStatus === 'rejected';
+  },
+
   get isTopLevelComment() {
     return !this.parentComment;
   },
-
-  // get isApproved() {
-  //   return this.approvalStatus === 'approved';
-  // },
-
-  // get isRejected() {
-  //   return this.approvalStatus === 'rejected';
-  // },
-
-  // get isAwaitingApproval() {
-  //   return this.approvalStatus === 'awaiting_approval';
-  // },
 });
