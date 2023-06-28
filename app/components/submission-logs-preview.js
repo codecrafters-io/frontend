cff1f42bfa9977609562a04999626aad2261322e
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-06-29 00:38:28 +0100

    Fix scroll issue in submission logs preview component

--- app/components/submission-logs-preview.js
+++ app/components/submission-logs-preview.js
@@ -1,10 +1,16 @@
 import Component from '@glimmer/component';
 import { default as AnsiUp } from 'ansi_up';
 import { htmlSafe } from '@ember/template';
+import { action } from '@ember/object';
 
 export default class SubmissionLogsPreviewComponent extends Component {
   get evaluation() {
     return this.args.submission.evaluations.firstObject;
+  }
+
+  @action
+  handleDidInsert(element) {
+    element.scrollTop = element.scrollHeight;
   }
 
   get logLines() {
