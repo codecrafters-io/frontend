95d2e8bb693c1389b6e79a473d3c0cf41f215ed9
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-06-28 23:11:02 +0100

    Update course-stage-item.hbs and footer.hbs in the app/components/course-page/step-list directory.

--- app/components/course-page/step-list/course-stage-item/footer.js
+++ app/components/course-page/step-list/course-stage-item/footer.js
@@ -1,7 +1,10 @@
 import { isToday, isYesterday } from 'date-fns';
+import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
 
 export default class CourseStageItemFooterComponent extends Component {
+  @tracked logsAreExpanded = false;
+
   get completedAt() {
     return this.args.repository.stageCompletedAt(this.args.courseStage);
   }
@@ -14,13 +17,21 @@ export default class CourseStageItemFooterComponent extends Component {
     return this.completedAt && isYesterday(this.completedAt);
   }
 
+  get debuggingArticleLink() {
+    return 'https://docs.codecrafters.io/challenges/debug-test-failures';
+  }
+
   get lastFailedSubmissionWasWithinLast10Minutes() {
     return this.lastFailedSubmissionCreatedAt && new Date() - this.lastFailedSubmissionCreatedAt <= 600 * 1000; // in last 10 minutes
   }
 
   get lastFailedSubmissionCreatedAt() {
+    return this.lastFailedSubmission ? this.lastFailedSubmission.createdAt : null;
+  }
+
+  get lastFailedSubmission() {
     if (this.args.repository.lastSubmissionHasFailureStatus) {
-      return this.args.repository.lastSubmission.createdAt;
+      return this.args.repository.lastSubmission;
     } else {
       return null;
     }
