22afdce0c9ba1ae18f895967e9043936aa5d7f38
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-06-29 16:42:42 +0100

    update test

--- tests/pages/components/course-page/step-list/course-completed-item.js
+++ tests/pages/components/course-page/step-list/course-completed-item.js
@@ -1,6 +1,8 @@
 import { text } from 'ember-cli-page-object';
+import { clickable } from 'ember-cli-page-object';
 
 export default {
+  clickOnPublishToGithubLink: clickable('span:contains("Click here")'),
   instructionsText: text('[data-test-instructions-text]'),
   scope: '[data-test-course-completed-item]',
 };
