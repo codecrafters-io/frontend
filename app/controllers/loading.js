04130009802d0724659d9eff9ab9e040a7020ab3
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-07-01 13:26:11 +0100

    speed up tests

--- app/controllers/loading.js
+++ app/controllers/loading.js
@@ -1,15 +1,23 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { later } from '@ember/runloop';
+import { cancel } from '@ember/runloop';
 
 export default class LoadingController extends Controller {
+  cancellable;
+
   @action
   async handleDidInsertLoadingIndicator(element) {
-    later(() => {
+    this.cancellable = later(() => {
       if (element) {
         element.classList.add('opacity-100');
         element.classList.remove('opacity-0');
       }
     }, 1000);
+  }
+
+  @action
+  async handleWillDestroyLoadingIndicator() {
+    cancel(this.cancellable);
   }
 }
