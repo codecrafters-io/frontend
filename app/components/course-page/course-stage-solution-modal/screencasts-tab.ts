22262492bc55c6ab48d0f7b9e9ee73f002c58ceb
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-07-05 22:31:47 +0100

    feat: Add "Submit a screencast" button to course stage solution modal

--- app/components/course-page/course-stage-solution-modal/screencasts-tab.ts
+++ app/components/course-page/course-stage-solution-modal/screencasts-tab.ts
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import Store from '@ember-data/store';
 import { action } from '@ember/object';
+import { createPopup } from '@typeform/embed';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { next } from '@ember/runloop';
@@ -10,6 +11,10 @@ interface Signature {
 
   Args: {
     courseStage: {
+      slug: string;
+      course: {
+        slug: string;
+      };
       screencasts: {
         id: string;
       }[];
@@ -21,6 +26,10 @@ export default class ScreencastsTabComponent extends Component<Signature> {
   @tracked embedHtml: string | undefined;
   @tracked selectedScreencast: any | undefined;
   @tracked screencastPlayerElement: HTMLDivElement | undefined;
+
+  // @ts-ignore
+  @service declare authenticator;
+
   @service declare store: Store;
 
   get screencasts() {
@@ -40,6 +49,17 @@ export default class ScreencastsTabComponent extends Component<Signature> {
         this.screencastPlayerElement.scrollIntoView();
       }
     });
+  }
+
+  @action
+  handleSubmitScreencastButtonClicked() {
+    createPopup('Dl5342qZ', {
+      hidden: {
+        github_username: this.authenticator.currentUser.username,
+        course_slug: this.args.courseStage.course.slug,
+        course_stage_slug: this.args.courseStage.slug,
+      },
+    }).toggle();
   }
 
   @action
