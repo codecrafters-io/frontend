5d09dbe9465a7161c506b34ef4d5c5922c62475e
lib.martinito@pm.me <lib.martinito@pm.me>
2023-07-07 21:43:45 +0800

    CC-598 added viewed logs analytics event

--- app/components/course-page/step-list/course-stage-item/footer.js
+++ app/components/course-page/step-list/course-stage-item/footer.js
@@ -1,9 +1,12 @@
 import { isToday, isYesterday } from 'date-fns';
 import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
 export default class CourseStageItemFooterComponent extends Component {
   @tracked logsAreExpanded = false;
+
+  @service analyticsEventTracker;
 
   get completedAt() {
     return this.args.repository.stageCompletedAt(this.args.courseStage);
@@ -34,6 +37,16 @@ export default class CourseStageItemFooterComponent extends Component {
       return this.args.repository.lastSubmission;
     } else {
       return null;
+    }
+  }
+
+  handleLogsButtonClick() {
+    this.logsAreExpanded = !this.logsAreExpanded;
+
+    if (this.logsAreExpanded && this.lastFailedSubmission) {
+      this.analyticsEventTracker.track('viewed_logs', {
+        submussion_id: this.lastFailedSubmission.id,
+      });
     }
   }
 }
