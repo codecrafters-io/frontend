f2eb5b41646d74ddd3923222cee4bfe03f520b58
Vasyl Marchuk <public@vasylmarchuk.com>
2023-07-09 14:45:31 +0300

    Rename `lib/application-route` to `lib/base-route`

--- app/lib/base-route.js
+++ app/lib/base-route.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import window from 'ember-window-mock';
 
-export default class ApplicationRoute extends Route {
+export default class BaseRoute extends Route {
   allowsAnonymousAccess = false;
   @service authenticator;
   @service router;
