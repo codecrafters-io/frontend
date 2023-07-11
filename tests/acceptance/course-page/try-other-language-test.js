c105973ecdfff326645ec044ce12b0fa73f5e52c
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-07-11 01:10:19 +0100

    "Refactor try-other-language-test.js

--- tests/acceptance/course-page/try-other-language-test.js
+++ tests/acceptance/course-page/try-other-language-test.js
@@ -1,7 +1,6 @@
 import apiRequestsCount from 'codecrafters-frontend/tests/support/api-requests-count';
 import coursePage from 'codecrafters-frontend/tests/pages/course-page';
 import catalogPage from 'codecrafters-frontend/tests/pages/catalog-page';
-import finishRender from 'codecrafters-frontend/tests/support/finish-render';
 import testScenario from 'codecrafters-frontend/mirage/scenarios/test';
 import { animationsSettled, setupAnimationTest } from 'ember-animated/test-support';
 import { currentURL, settled } from '@ember/test-helpers';
