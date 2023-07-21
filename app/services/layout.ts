9fbac730470b8600753605ea824fde776895035a
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-07-21 17:05:11 +0100

    styling fixes

--- app/services/layout.ts
+++ app/services/layout.ts
@@ -5,7 +5,7 @@ export default class LayoutService extends Service {
   @service declare router: RouterService;
 
   get shouldShowHeader(): boolean {
-    if (this.router.currentRouteName === 'course' || this.router.currentRouteName.startsWith('course.')) {
+    if (this.router.currentRouteName === 'course' || this.router.currentRouteName.startsWith('course.') || this.router.currentRouteName.startsWith('course-admin')) {
       return false;
     } else {
       return true;
