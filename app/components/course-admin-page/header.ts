1c0014e896348fa7ecb856384069eafe9a7f78a8
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-07-21 17:51:54 +0100

    use tabs

---
+++ app/components/course-admin-page/header.ts
@@ -0,0 +1,45 @@
+import Component from '@glimmer/component';
+import RouterService from '@ember/routing/router-service';
+import { inject as service } from '@ember/service';
+
+type Signature = {
+  Element: HTMLDivElement;
+
+  Args: {
+    course: { slug: string };
+  };
+};
+
+type Tab = {
+  icon: string;
+  name: string;
+  slug: string;
+  route: string;
+  models: unknown[];
+  isActive: boolean;
+};
+
+export default class CourseAdminHeaderComponent extends Component<Signature> {
+  @service declare router: RouterService;
+
+  get tabs(): Tab[] {
+    return [
+      {
+        icon: 'code',
+        name: 'Submissions',
+        slug: 'submissions',
+        route: 'course-admin.submissions',
+        models: [this.args.course.slug],
+        isActive: this.router.currentRouteName === 'course-admin.submissions',
+      },
+      {
+        icon: 'github',
+        name: 'Updates',
+        slug: 'updates',
+        route: 'course-admin.submissions',
+        models: [this.args.course.slug],
+        isActive: this.router.currentRouteName === 'course-admin.updates',
+      },
+    ];
+  }
+}
