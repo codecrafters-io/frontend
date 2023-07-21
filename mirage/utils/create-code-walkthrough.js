df0a8535f0b408afe921e8f04264f34f3b03322a
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-07-21 18:11:18 +0100

    update test

---
+++ mirage/utils/create-code-walkthrough.js
@@ -0,0 +1,44 @@
+export default function createCodeWalkthroughWalkthrough(server, slug) {
+  return server.create('code-walkthrough', {
+    sections: [
+      {
+        type: 'prose',
+        markdown: "Here's how Redis does this. [ping_command](https://github.com/redis/redis)",
+      },
+      {
+        type: 'referenced_code',
+        language_slug: 'c',
+        code: `void pingCommand(client *c) {
+    /* The command takes zero or one arguments. */
+    if (c->argc > 2) {
+        addReplyErrorArity(c);
+        return;
+    }
+
+    if (c->flags & CLIENT_PUBSUB && c->resp == 2) {
+        addReply(c,shared.mbulkhdr[2]);
+        addReplyBulkCBuffer(c,"pong",4);
+        if (c->argc == 1)
+            addReplyBulkCBuffer(c,"",0);
+        else
+            addReplyBulk(c,c->argv[1]);
+    } else {
+        if (c->argc == 1)
+            addReply(c,shared.pong);
+        else
+            addReplyBulk(c,c->argv[1]);
+    }
+}
+`,
+        file_path: 'app/server.c',
+        link: 'https://github.com',
+      },
+      {
+        type: 'prose',
+        markdown: "Here's how Redis does this.",
+      },
+    ],
+
+    slug: slug,
+  });
+}
