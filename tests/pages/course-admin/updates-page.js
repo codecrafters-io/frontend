7e722baf70c3df7d76a1f5506e03a22985fa5165
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-07-21 20:47:52 +0100

    add updates page [percy]

--- tests/pages/course-admin/updates-page.js
+++ tests/pages/course-admin/updates-page.js
@@ -1,7 +1,10 @@
-import { create, clickOnText, visitable } from 'ember-cli-page-object';
+import { clickable, collection, create, clickOnText, visitable } from 'ember-cli-page-object';
 
 export default create({
   clickOnLink: clickOnText(),
 
+  updateListItems: collection('[data-test-update-list-item]', {
+    clickOnViewUpdateButton: clickable('[data-test-view-update-button]'),
+  }),
   visit: visitable('/courses/:course_slug/admin/updates'),
 });
