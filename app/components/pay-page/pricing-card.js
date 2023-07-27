7e00025bc9a9325389c18c52e2390b943de21f4b
lib.martinito@pm.me <lib.martinito@pm.me>
2023-07-27 23:22:28 +0800

    Updated copy for all cards

--- app/components/pay-page/pricing-card.js
+++ app/components/pay-page/pricing-card.js
@@ -22,17 +22,7 @@ export default class PricingCardComponent extends Component {
   }
 
   get featureDescriptions() {
-    if (this.args.pricingFrequency === 'quarterly') {
-      return ['One time payment', 'No limits on content', 'Members community for Q&A', 'Priority builds', 'Priority support'];
-    } else {
-      return [
-        'One time payment',
-        'Unrestricted content access',
-        'Members community for Q&A',
-        'Download invoice for expensing',
-        'Private leaderboard for your team',
-      ];
-    }
+    return ['One time payment', 'No limits on content', 'Members community for Q&A', 'Priority builds', 'Priority support'];
   }
 
   @action
