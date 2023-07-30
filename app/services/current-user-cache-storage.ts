56703f4703adeb0d0d918744cc4a10ec95f3ee5e
Vasyl Marchuk <public@vasylmarchuk.com>
2023-07-30 14:16:50 +0300

    Use `LocalStorage` service in `CurrentUserCacheStorage` and `SessionTokenStorage` services

--- app/services/current-user-cache-storage.ts
+++ app/services/current-user-cache-storage.ts
@@ -1,24 +1,27 @@
-import Service from '@ember/service';
+import Service, { service } from '@ember/service';
+import LocalStorageService from 'codecrafters-frontend/services/local-storage';
 
 export default class CurrentUserCacheStorageService extends Service {
   static USER_ID_LOCAL_STORAGE_KEY = 'current_user_cache_v1:user_id';
   static USERNAME_LOCAL_STORAGE_KEY = 'current_user_cache_v1:username';
 
+  @service declare localStorage: LocalStorageService;
+
   clear(): void {
-    localStorage.removeItem(CurrentUserCacheStorageService.USER_ID_LOCAL_STORAGE_KEY);
-    localStorage.removeItem(CurrentUserCacheStorageService.USERNAME_LOCAL_STORAGE_KEY);
+    this.localStorage.removeItem(CurrentUserCacheStorageService.USER_ID_LOCAL_STORAGE_KEY);
+    this.localStorage.removeItem(CurrentUserCacheStorageService.USERNAME_LOCAL_STORAGE_KEY);
   }
 
   get userId(): string | null {
-    return localStorage.getItem(CurrentUserCacheStorageService.USER_ID_LOCAL_STORAGE_KEY);
+    return this.localStorage.getItem(CurrentUserCacheStorageService.USER_ID_LOCAL_STORAGE_KEY);
   }
 
   get username(): string | null {
-    return localStorage.getItem(CurrentUserCacheStorageService.USERNAME_LOCAL_STORAGE_KEY);
+    return this.localStorage.getItem(CurrentUserCacheStorageService.USERNAME_LOCAL_STORAGE_KEY);
   }
 
   setValues(userId: string, username: string): void {
-    localStorage.setItem(CurrentUserCacheStorageService.USER_ID_LOCAL_STORAGE_KEY, userId);
-    localStorage.setItem(CurrentUserCacheStorageService.USERNAME_LOCAL_STORAGE_KEY, username);
+    this.localStorage.setItem(CurrentUserCacheStorageService.USER_ID_LOCAL_STORAGE_KEY, userId);
+    this.localStorage.setItem(CurrentUserCacheStorageService.USERNAME_LOCAL_STORAGE_KEY, username);
   }
 }
