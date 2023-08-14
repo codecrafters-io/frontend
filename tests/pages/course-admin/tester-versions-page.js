cfe4302f8f46cb71a91403d8c690b0a5cf960ef8
libmartinito <lib.martinito@pm.me>
2023-08-14 14:48:17 +0800

    CC-668 merged created test files from CC-677

---
+++ tests/pages/course-admin/tester-versions-page.js
@@ -0,0 +1,13 @@
+import { clickable, collection, create, visitable } from 'ember-cli-page-object';
+
+export default create({
+  clickOnSyncWithGithubButton: clickable('[data-test-sync-with-github-button]'),
+
+  testerRepositoryLink: {
+    scope: '[data-test-tester-repository-link]',
+  },
+
+  testerVersionListItem: collection('[data-test-version-list-item]'),
+
+  visit: visitable('/courses/:course_slug/admin/tester-versions'),
+});
