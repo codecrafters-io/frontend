306d0d27a11acbebd068cdcf402a6dfaa9b9c14d
libmartinito <lib.martinito@pm.me>
2023-08-17 16:27:05 +0800

    CC-662 added comment to explain moving image declaration files to a separate file

--- types/images.d.ts
+++ types/images.d.ts
@@ -1,2 +1,7 @@
+// Moved these declarations from types/codecrafters-frontend/index.d.ts
+// to avoid typescript errors when importing images.
+// Isn't clear why this is necessary as we include the index.d.ts file
+// in the tsconfig.json file.
+
 declare module '*.jpg';
 declare module '*.svg';
