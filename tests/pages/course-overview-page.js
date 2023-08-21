5b26655439fea7eed48e02f4204ef1e298738820
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-08-21 13:31:58 +0100

    Update course overview header and start course button selector

--- tests/pages/course-overview-page.js
+++ tests/pages/course-overview-page.js
@@ -1,6 +1,6 @@
 import { clickable, create, visitable } from 'ember-cli-page-object';
 
 export default create({
-  clickOnStartCourse: clickable('[data-test-start-course-button]'),
+  clickOnStartCourse: clickable('[data-test-course-overview-header] [data-test-start-course-button]'),
   visit: visitable('/courses/:course_slug/overview'),
 });
