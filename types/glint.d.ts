64df2e357f847cb41d85b380cb2c329868231936
libmartinito <lib.martinito@pm.me>
2023-08-22 21:17:59 +0800

    CC-682 add 'named' to args type

--- types/glint.d.ts
+++ types/glint.d.ts
@@ -36,7 +36,7 @@ declare module '@glint/environment-ember-loose/registry' {
     'page-title': HelperLike<{ Return: string; Args: { Positional: [string] } }>;
     repeat: HelperLike<{ Return: string[]; Args: { Positional: [number] } }>;
     gt: HelperLike<{ Args: { Positional: [unknown, unknown] }; Return: boolean }>;
-    FeatureCard: ComponentLike<{ Args: { Content: SafeString, imageUrl: string, title: string  } };
+    FeatureCard: ComponentLike<{ Args: { Named: { Content: SafeString, imageUrl: string, title: string } } };
     // ...
   }
 }
