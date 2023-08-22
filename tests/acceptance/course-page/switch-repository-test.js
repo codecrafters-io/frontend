b3900cbe6083769b97705b3d5805dd496fde3bf4
libmartinito <lib.martinito@pm.me>
2023-08-22 15:41:03 +0800

    CC-678 fixed other failing tests

--- tests/acceptance/course-page/switch-repository-test.js
+++ tests/acceptance/course-page/switch-repository-test.js
@@ -47,6 +47,7 @@ module('Acceptance | course-page | switch-repository', function (hooks) {
       'fetch repositories (course page)',
       'fetch leaderboard entries (course page)',
       'fetch hints (course page)',
+      'fetch regional discounts',
     ].length;
 
     assert.strictEqual(coursePage.repositoryDropdown.activeRepositoryName, goRepository.name, 'repository with last push should be active');
--- tests/acceptance/course-page/try-other-language-test.js
+++ tests/acceptance/course-page/try-other-language-test.js
@@ -41,6 +41,7 @@ module('Acceptance | course-page | try-other-language', function (hooks) {
       'fetch repositories (course page)',
       'fetch leaderboard entries (course page)',
       'fetch hints (course page)',
+      'fetch regional discounts',
     ].length;
 
     await catalogPage.visit();
