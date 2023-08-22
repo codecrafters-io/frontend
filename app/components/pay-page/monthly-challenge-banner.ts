24ae82808196951227962e58700ea4ab9fb0551d
libmartinito <lib.martinito@pm.me>
2023-08-22 21:55:08 +0800

    CC-686 removed course argument

--- app/components/pay-page/monthly-challenge-banner.ts
+++ app/components/pay-page/monthly-challenge-banner.ts
@@ -4,12 +4,6 @@ import airpodsImage from '/assets/images/monthly-challenges/airpods.png';
 
 interface Signature {
   Element: HTMLAnchorElement;
-
-  Args: {
-    course: {
-      id: number,
-    },
-  };
 }
 
 export default class MonthlyChallengeBannerComponent extends Component<Signature> {
