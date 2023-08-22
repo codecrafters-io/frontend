cda8bfa9b8cdc173e7d0969aa2e806b7edf5b0d2
libmartinito <lib.martinito@pm.me>
2023-08-23 00:06:53 +0800

    CC-678 try using [unknown, unknown]

--- types/glint.d.ts
+++ types/glint.d.ts
@@ -35,7 +35,7 @@ declare module '@glint/environment-ember-loose/registry' {
     'page-title': HelperLike<{ Return: string; Args: { Positional: [string] } }>;
     repeat: HelperLike<{ Return: string[]; Args: { Positional: [number] } }>;
     gt: HelperLike<{ Args: { Positional: [unknown, unknown] }; Return: boolean }>;
-    'did-resize': ModifierLike<{ Args: { Positional: unknown }; Return: void }>;
+    'did-resize': ModifierLike<{ Args: { Positional: [unknown, unknown] }; }>;
     // ...
   }
 }
