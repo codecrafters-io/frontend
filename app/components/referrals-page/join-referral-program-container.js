ff086c08be1d4d00e7eb253231b8c10adb51ab43
libmartinito <lib.martinito@pm.me>
2023-08-22 18:20:13 +0800

    CC-682 extracted referral page feature cards into own component

--- app/components/referrals-page/join-referral-program-container.js
+++ app/components/referrals-page/join-referral-program-container.js
@@ -1,13 +1,7 @@
 import Component from '@glimmer/component';
-import showdown from 'showdown';
 import { action } from '@ember/object';
-import { htmlSafe } from '@ember/template';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-
-import freeForFriendImage from '/assets/images/referral-program-features/free-for-friend.jpg';
-import lifetimeEarningsImage from '/assets/images/referral-program-features/lifetime-earnings.jpg';
-import simplePayoutImage from '/assets/images/referral-program-features/simple-payout.jpg';
 
 import adobeImage from '/assets/images/company-logos/adobe-company-logo.svg';
 import amazonImage from '/assets/images/company-logos/amazon-company-logo.svg';
@@ -56,38 +50,6 @@ export default class JoinReferralProgramContainerComponent extends Component {
       {
         name: 'Linear',
         url: linearImage,
-      },
-    ];
-  }
-
-  get features() {
-    return this.featuresMarkdown.map((feature) => {
-      return {
-        ...feature,
-        body: htmlSafe(new showdown.Converter().makeHtml(feature.bodyMarkdown)),
-      };
-    });
-  }
-
-  get featuresMarkdown() {
-    return [
-      {
-        title: '60% Revenue Share.',
-        bodyMarkdown:
-          'Earn 60% of what we make through your referrals. Example payouts for a single paid referral (before discounting):<br /><br />\n- **$594** (lifetime plan)\n- **$216** (one year plan)\n- **$72** (3-month plan)',
-        imageUrl: lifetimeEarningsImage,
-      },
-      {
-        title: 'No forms to fill. Simple payout.',
-        bodyMarkdown:
-          'Activate your link with one click.<br /><br />Monitor the status of your referrals in real-time on your CodeCrafters dashboard. Get paid via PayPal or any of the 30+ gift cards that we support.',
-        imageUrl: simplePayoutImage,
-      },
-      {
-        title: '(Probably) Free for your friend.',
-        bodyMarkdown:
-          'Most developers can get their CodeCrafters fees fully reimbursed through their corporate L&D budget.<br /><br />Remind them about it, help them save money, and help make their decision easier.',
-        imageUrl: freeForFriendImage,
       },
     ];
   }
