25fd4b04782dbf5103b159981fa799585787aea5
libmartinito <lib.martinito@pm.me>
2023-09-06 15:20:53 +0800

    CC-693 added code for redirection

--- app/routes/perk/claim.ts
+++ app/routes/perk/claim.ts
@@ -1,17 +1,26 @@
 import BaseRoute from 'codecrafters-frontend/lib/base-route';
+import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import JSONAPISerializer from '@ember-data/serializer/json-api';
+import PerkModel from 'codecrafters-frontend/models/perk';
 import Store from '@ember-data/store';
 import { inject as service } from '@ember/service';
-import JSONAPIAdapter from '@ember-data/adapter/json-api';
 
 export default class PerksClaimRoute extends BaseRoute {
   @service declare store: Store;
 
-  async model(params: { slug: string }) {
+  async model(params: { slug: string }): Promise<PerkModel> {
     const adapter = this.store.adapterFor('application' as never) as JSONAPIAdapter;
     const url = adapter.buildURL() + `/perks/${params.slug}/claim`;
-    const perk = await adapter.ajax(url, 'GET');
+    const rawResponse = await adapter.ajax(url, 'GET');
+    const serializer = this.store.serializerFor('application' as never) as JSONAPISerializer;
+    const normalizedResponse = serializer.normalizeResponse(this.store, this.store.modelFor('perk'), rawResponse, rawResponse["data"]["id"], 'findRecord') as { data: { attributes: PerkModel } };
 
-    console.log(perk);
-    return perk;
+    return normalizedResponse.data.attributes;
+  }
+
+  async afterModel(perk: PerkModel): Promise<void> {
+    if (perk.claimUrl) {
+      window.location.href = perk.claimUrl;
+    }
   }
 }
