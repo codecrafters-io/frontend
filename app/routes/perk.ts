233a5b224bc258144080142233ecfdd722ede146
libmartinito <lib.martinito@pm.me>
2023-09-06 15:33:23 +0800

    CC-693 added redirection to catalog when visiting /perks

--- app/routes/perk.ts
+++ app/routes/perk.ts
@@ -1,11 +1,17 @@
-import { inject as service } from '@ember/service';
 import BaseRoute from 'codecrafters-frontend/lib/base-route';
+import RouterService from '@ember/routing/router-service';
 import Store from '@ember-data/store';
+import { inject as service } from '@ember/service';
 
 export default class PerkRoute extends BaseRoute {
+  @service declare router: RouterService;
   @service declare store: Store;
 
   async model () {
-    return await this.store.query('perk', {})
+    return await this.store.query('perk', {});
+  }
+
+  async afterModel() {
+    this.router.transitionTo('catalog');
   }
 }
