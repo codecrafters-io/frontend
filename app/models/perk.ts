7c1a842e0d9360467172903e23fff82aa136774d
libmartinito <lib.martinito@pm.me>
2023-09-07 20:13:27 +0800

    CC-693 removed claimUrl from perk model

--- app/models/perk.ts
+++ app/models/perk.ts
@@ -2,8 +2,7 @@ import Model, { attr } from '@ember-data/model';
 import { memberAction } from 'ember-api-actions';
 
 export default class PerkModel extends Model {
-  @attr('string') declare claimUrl?: string;
-  @attr('string') declare slug?: string;
+  @attr('string') declare slug: string;
 
   declare claim: (this: Model, payload: any) => Promise<void>;
 }
