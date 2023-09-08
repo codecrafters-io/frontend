87847fd294e424ca97b1e1a71b8470e3d014a267
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-09-08 14:49:46 +0100

    Update course model with new challenge logos

--- app/models/course.js
+++ app/models/course.js
@@ -4,12 +4,13 @@ import { equal } from '@ember/object/computed'; // eslint-disable-line ember/no-
 import Model from '@ember-data/model';
 
 import bittorrentLogo from '/assets/images/challenge-logos/challenge-logo-bittorrent.svg';
-import redisLogo from '/assets/images/challenge-logos/challenge-logo-redis.svg';
 import dockerLogo from '/assets/images/challenge-logos/challenge-logo-docker.svg';
 import gitLogo from '/assets/images/challenge-logos/challenge-logo-git.svg';
-import sqliteLogo from '/assets/images/challenge-logos/challenge-logo-sqlite.svg';
-import reactLogo from '/assets/images/challenge-logos/challenge-logo-react.svg';
 import grepLogo from '/assets/images/challenge-logos/challenge-logo-grep.svg';
+import httpLogo from '/assets/images/challenge-logos/challenge-logo-http.svg';
+import reactLogo from '/assets/images/challenge-logos/challenge-logo-react.svg';
+import redisLogo from '/assets/images/challenge-logos/challenge-logo-redis.svg';
+import sqliteLogo from '/assets/images/challenge-logos/challenge-logo-sqlite.svg';
 
 export default class CourseModel extends Model {
   @attr('number') completionPercentage;
@@ -83,6 +84,7 @@ export default class CourseModel extends Model {
         docker: dockerLogo,
         git: gitLogo,
         grep: grepLogo,
+        http: httpLogo,
         react: reactLogo,
         redis: redisLogo,
         sqlite: sqliteLogo,
