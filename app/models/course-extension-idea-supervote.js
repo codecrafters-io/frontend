bbb6372466a3d60f80c6628b07641eb5540dd470
libmartinito <lib.martinito@pm.me>
2023-09-11 15:19:52 +0800

    CC-731 removed supervote models

--- app/models/course-extension-idea-supervote.js
+++
@@ -1,9 +0,0 @@
-import Model from '@ember-data/model';
-import { attr, belongsTo } from '@ember-data/model';
-
-export default class CourseExtensionIdeaSupervoteModel extends Model {
-  @attr('date') createdAt;
-
-  @belongsTo('course-extension-idea', { async: false, inverse: null }) courseExtensionIdea;
-  @belongsTo('user', { async: false, inverse: 'courseExtensionIdeaSupervotes' }) user;
-}
