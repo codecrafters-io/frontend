3c93e43c95ef536987baf910908d381e9cdb2c08
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-09-14 16:16:36 +0100

    Refactor CourseCardComponent to remove unused import

--- app/components/course-card.js
+++ app/components/course-card.js
@@ -1,5 +1,4 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class CourseCardComponent extends Component {
