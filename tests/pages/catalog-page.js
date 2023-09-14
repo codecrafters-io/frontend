82f50f42847d9dec11798a5bd15cdecdc9b10290
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-09-14 21:04:40 +0100

    Refactor clickOnCourse and clickOnTrack methods in catalog-page.js

--- tests/pages/catalog-page.js
+++ tests/pages/catalog-page.js
@@ -1,22 +1,21 @@
 import { collection, create, visitable } from 'ember-cli-page-object';
+import { waitUntil } from '@ember/test-helpers';
 import AccountDropdown from 'codecrafters-frontend/tests/pages/components/account-dropdown';
 import CourseCard from 'codecrafters-frontend/tests/pages/components/course-card';
 import TrackCard from 'codecrafters-frontend/tests/pages/components/tracks-page/track-card';
 import Header from 'codecrafters-frontend/tests/pages/components/header';
 
-import finishRender from 'codecrafters-frontend/tests/support/finish-render';
-
 export default create({
   accountDropdown: AccountDropdown,
 
   async clickOnCourse(courseName) {
-    this.courseCards.toArray().findBy('name', courseName).click();
-    await finishRender(); // Page has poller
+    await waitUntil(() => this.courseCards.length > 0); // Ensure skeleton UI is gone
+    await this.courseCards.toArray().findBy('name', courseName).click();
   },
 
   async clickOnTrack(trackName) {
-    this.trackCards.toArray().findBy('name', trackName).click();
-    await finishRender(); // Page has poller
+    await waitUntil(() => this.trackCards.length > 0); // Ensure skeleton UI is gone
+    await this.trackCards.toArray().findBy('name', trackName).click();
   },
 
   courseCards: collection('[data-test-course-card]', CourseCard),
