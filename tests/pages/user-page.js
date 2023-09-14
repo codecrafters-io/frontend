549983ab71c5ea8708da2ad3ebfe9b36de4bab93
libmartinito <lib.martinito@pm.me>
2023-09-14 23:28:46 +0800

    CC-735 added tests

--- tests/pages/user-page.js
+++ tests/pages/user-page.js
@@ -2,6 +2,10 @@ import { triggerable, visitable } from 'ember-cli-page-object';
 import createPage from 'codecrafters-frontend/tests/support/create-page';
 
 export default createPage({
+  adminProfileButton: {
+    scope: '[data-test-admin-profile-button]',
+  },
+
   userLabel: {
     scope: '[data-test-user-label]',
     hover: triggerable('mouseenter'),
