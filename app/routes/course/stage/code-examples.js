ab2821ade587e4efb12239bd9748c67ac634538d
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-09-15 13:05:46 +0100

    Delete code-examples.js and screencasts.js routes

--- app/routes/course/stage/code-examples.js
+++
@@ -1,7 +0,0 @@
-import BaseRoute from 'codecrafters-frontend/lib/base-route';
-
-export default class CodeExamplesRoute extends BaseRoute {
-  async model() {
-    return this.modelFor('course.stage');
-  }
-}
--- app/routes/course/stage/screencasts.js
+++
@@ -1,7 +0,0 @@
-import BaseRoute from 'codecrafters-frontend/lib/base-route';
-
-export default class ScreencastsRoute extends BaseRoute {
-  async model() {
-    return this.modelFor('course.stage');
-  }
-}
