ce7cd057e4495d110ec29de8e5cb36a360fa19be
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-09-22 21:44:39 +0100

    WIP

---
+++ tests/acceptance/course-page/extensions/enable-extensions-after-completion-test.js
@@ -0,0 +1,67 @@
+import { setupAnimationTest } from 'ember-animated/test-support';
+import { currentURL } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import { signInAsStaff } from 'codecrafters-frontend/tests/support/authentication-helpers';
+import catalogPage from 'codecrafters-frontend/tests/pages/catalog-page';
+import coursePage from 'codecrafters-frontend/tests/pages/course-page';
+import testScenario from 'codecrafters-frontend/mirage/scenarios/test';
+
+module('Acceptance | course-page | extensions | enable-extensions-after-completion', function (hooks) {
+  setupApplicationTest(hooks);
+  setupAnimationTest(hooks);
+  setupMirage(hooks);
+
+  test('can enable extensions after completing base stages', async function (assert) {
+    testScenario(this.server);
+    signInAsStaff(this.owner, this.server);
+
+    let currentUser = this.server.schema.users.first();
+    let python = this.server.schema.languages.findBy({ name: 'Python' });
+    let course = this.server.schema.courses.findBy({ slug: 'dummy' });
+
+    const repository = this.server.create('repository', 'withFirstStageCompleted', {
+      course: course,
+      language: python,
+      user: currentUser,
+    });
+
+    [2, 3, 4].forEach((position) => {
+      this.server.create('submission', 'withSuccessStatus', {
+        repository: repository,
+        courseStage: course.stages.models.find((stage) => stage.position === position),
+      });
+    });
+
+    await catalogPage.visit();
+    await catalogPage.clickOnCourse('Build your own Dummy');
+
+    // await this.pauseTest();
+
+    // assert.strictEqual(currentURL(), '/courses/dummy/stages/2', 'current URL is course page URL');
+
+    // assert.strictEqual(coursePage.sidebar.stepListItems.length, 4, 'step list has 4 items');
+
+    // await coursePage.sidebar.clickOnConfigureExtensionsButton();
+
+    // // Enable Extension 1
+    // await coursePage.configureExtensionsModal.toggleExtension('Extension 1');
+    // assert.strictEqual(coursePage.sidebar.stepListItems.length, 6, 'step list has 6 items when first extension is enabled');
+
+    // // Enable Extension 2
+    // await coursePage.configureExtensionsModal.toggleExtension('Extension 2');
+    // assert.strictEqual(coursePage.sidebar.stepListItems.length, 8, 'step list has 8 items when both extensions are enabled');
+
+    // // Disable Extension 1
+    // await coursePage.configureExtensionsModal.toggleExtension('Extension 1');
+    // assert.strictEqual(coursePage.sidebar.stepListItems.length, 6, 'step list has 6 items when first extension is disabled');
+
+    // // TODO: This has something to do with the RecordCacheData error, investigate
+    // await new Promise((resolve) => setTimeout(resolve, 500));
+
+    // // Disable Extension 2
+    // await coursePage.configureExtensionsModal.toggleExtension('Extension 2');
+    // assert.strictEqual(coursePage.sidebar.stepListItems.length, 4, 'step list has 4 items both extensions are disabled');
+  });
+});
