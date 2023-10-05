0f4f1d30b42082324e5ca400da89cb392c553105
libmartinito <lib.martinito@pm.me>
2023-10-05 21:59:01 +0800

    CC-773 lint fix

--- app/components/pay-page/monthly-challenge-banner.ts
+++ app/components/pay-page/monthly-challenge-banner.ts
@@ -1,11 +1,7 @@
 import Component from '@glimmer/component';
 
-import ouraRingHorizonBannerImage from '/assets/images/monthly-challenges/oura-ring-horizon-banner.png';
-
 interface Signature {
   Element: HTMLAnchorElement;
 }
 
-export default class MonthlyChallengeBannerComponent extends Component<Signature> {
-  ouraRingHorizonBannerImage = ouraRingHorizonBannerImage;
-}
+export default class MonthlyChallengeBannerComponent extends Component<Signature> {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -45,6 +45,9 @@ module.exports = {
       mono: 'Consolas, Monaco, "Andale Mono", "Ubuntu Mono", monospace',
     },
     extend: {
+      backgroundImage: {
+        'monthly-challenge-banner': "url('/assets/images/monthly-challenges/oura-ring-horizon-banner.png')",
+      },
       brightness: {
         102: '1.02',
       },
