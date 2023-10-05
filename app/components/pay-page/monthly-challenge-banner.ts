32c0d816d17a729b72546785d571164526585c10
libmartinito <lib.martinito@pm.me>
2023-10-05 21:06:27 +0800

    CC-773 updated sidebar monthly challenge banner

--- app/components/pay-page/monthly-challenge-banner.ts
+++ app/components/pay-page/monthly-challenge-banner.ts
@@ -1,11 +1,11 @@
 import Component from '@glimmer/component';
 
-import ouraRingHorizonImage from '/assets/images/monthly-challenges/oura-ring-horizon.png';
+import ouraRingHorizonBannerImage from '/assets/images/monthly-challenges/oura-ring-horizon-banner.png';
 
 interface Signature {
   Element: HTMLAnchorElement;
 }
 
 export default class MonthlyChallengeBannerComponent extends Component<Signature> {
-  ouraRingHorizonImage = ouraRingHorizonImage;
+  ouraRingHorizonBannerImage = ouraRingHorizonBannerImage;
 }
