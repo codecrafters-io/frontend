8f83304d976f8b6a65b892a66d7ac4c43175f99d
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-10-06 17:29:48 +0100

    Add tests for adding blocks

--- app/components/pill.ts
+++ app/components/pill.ts
@@ -15,7 +15,7 @@ type Signature = {
 export default class PillComponent extends Component<Signature> {
   get colorClasses() {
     return {
-      green: 'bg-yellow-50 text-yellow-700 ring-yellow-600/20',
+      green: 'bg-green-50 text-green-700 ring-green-600/20',
       yellow: 'bg-yellow-50 text-yellow-700 ring-yellow-600/20',
       red: 'bg-red-50 text-red-700 ring-red-600/20',
       blue: 'bg-blue-50 text-blue-700 ring-blue-600/20',
--- tests/pages/concept-admin/blocks-page.ts
+++ tests/pages/concept-admin/blocks-page.ts
@@ -1,6 +1,24 @@
-import { clickable, collection, create, fillable, visitable } from 'ember-cli-page-object';
+import { clickable, collection, create, fillable, text, visitable } from 'ember-cli-page-object';
 
 export default create({
+  insertBlockMarkers: collection('[data-test-insert-block-marker]', {
+    dropdown: {
+      async clickOnItem(title: string) {
+        // @ts-ignore
+        await Array.from(this.items)
+          // @ts-ignore
+          .find((item) => item.title.trim() === title)
+          .click();
+      },
+
+      items: collection('[data-test-insert-block-marker-dropdown-item]', {
+        title: text('[data-test-title]'),
+      }),
+
+      scope: '[data-test-insert-block-marker-dropdown-content]',
+      resetScope: true,
+    },
+  }),
   editableBlocks: collection('[data-test-editable-block]', {
     clickOnSaveButton: clickable('[data-test-save-button]'),
     clickOnDeleteButton: clickable('[data-test-delete-button]'),
