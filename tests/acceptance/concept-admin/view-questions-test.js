701a0ffc9e3a987ab8ddd50afc2f012ce3d17a82
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-10-09 23:52:35 +0100

    Added a new route and template for viewing questions in the concept admin section. Also created a new component for rendering question cards and a controller for handling the questions. Implemented an acceptance test for viewing questions.

---
+++ tests/acceptance/concept-admin/view-questions-test.js
@@ -0,0 +1,57 @@
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import { signInAsStaff } from 'codecrafters-frontend/tests/support/authentication-helpers';
+import questionsPage from 'codecrafters-frontend/tests/pages/concept-admin/questions-page';
+import testScenario from 'codecrafters-frontend/mirage/scenarios/test';
+import percySnapshot from '@percy/ember';
+
+module('Acceptance | concept-admin | view-questions', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('can view basic details', async function (assert) {
+    testScenario(this.server);
+    signInAsStaff(this.owner, this.server);
+
+    const concept = this.server.create('concept', { slug: 'dummy' });
+
+    const question1 = this.server.create('concept-question', {
+      concept: concept,
+      queryMarkdown: 'What is question 1?',
+      slug: 'question-1',
+      options: [
+        { markdown: '42', is_correct: true, explanation_markdown: 'Explanation 1' },
+        { markdown: '24', isS_correct: false, explanation_markdown: 'Explanation 2' },
+      ],
+    });
+
+    const question2 = this.server.create('concept-question', {
+      concept: concept,
+      queryMarkdown: 'What is question 2?',
+      slug: 'question-2',
+      options: [
+        { markdown: '42', is_correct: true, explanation_markdown: 'Explanation 1' },
+        { markdown: '24', isS_correct: false, explanation_markdown: 'Explanation 2' },
+      ],
+    });
+
+    concept.update({ questions: [question1, question2] });
+
+    concept.update({
+      blocks: [
+        {
+          type: 'concept_question',
+          args: {
+            concept_question_slug: question1.slug,
+          },
+        },
+      ],
+    });
+
+    await questionsPage.visit({ concept_slug: 'dummy' });
+    assert.strictEqual(1, 1);
+
+    await percySnapshot('Concept Admin - Questions');
+  });
+});
