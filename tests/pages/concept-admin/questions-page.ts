1237150e31b148c8498e60c1d792ba2300301e84
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-10-10 17:07:04 +0100

    Add editing questions & options

--- tests/pages/concept-admin/questions-page.ts
+++ tests/pages/concept-admin/questions-page.ts
@@ -1,5 +1,14 @@
-import { create, visitable } from 'ember-cli-page-object';
+import { waitUntil } from '@ember/test-helpers';
+import { collection, create, text, visitable } from 'ember-cli-page-object';
 
 export default create({
+  async clickOnQuestionCard(slug: string) {
+    await waitUntil(() => this.questionCards.length > 0); // Ensure skeleton UI is gone
+    await this.questionCards.toArray().findBy('slug', slug)!.click();
+  },
+
+  questionCards: collection('[data-test-question-card]', {
+    slug: text('[data-test-question-slug]'),
+  }),
   visit: visitable('/concepts/:concept_slug/admin/questions'),
 });
