6d6c3ccebda4c29844549c115d283551922fe296
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-10-20 16:11:37 +0100

    add failing test

---
+++ tests/unit/models/concept-test.js
@@ -0,0 +1,29 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+
+module('Unit | Model | concept', function (hooks) {
+  setupTest(hooks);
+  setupMirage(hooks);
+
+  // Replace this with your real tests.
+  test('changedAttributes works', async function (assert) {
+    let store = this.owner.lookup('service:store');
+    let concept = store.createRecord('concept', {});
+    await concept.save();
+
+    assert.strictEqual(JSON.stringify(concept.changedAttributes()), JSON.stringify({}));
+    assert.ok(concept.id);
+
+    concept.title = 'New Title';
+    assert.ok(concept.hasDirtyAttributes, 'should have dirty attributes');
+    assert.strictEqual(JSON.stringify(concept.changedAttributes()), JSON.stringify({ title: ['New Concept', 'New Title'] }));
+
+    await concept.save();
+
+    concept.title = 'New Title';
+
+    assert.notOk(concept.hasDirtyAttributes, 'should have dirty attributes');
+    assert.strictEqual(JSON.stringify(concept.changedAttributes()), JSON.stringify({}));
+  });
+});
