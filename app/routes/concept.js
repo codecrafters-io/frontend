8c9e31eeb0b9fdd923a88ff25208ee9dd782ee5c
libmartinito <lib.martinito@pm.me>
2023-11-06 11:22:24 +0800

    CC-817 added scroll to top on route change to concept route

--- app/routes/concept.js
+++ app/routes/concept.js
@@ -1,9 +1,20 @@
-import { inject as service } from '@ember/service';
 import BaseRoute from 'codecrafters-frontend/lib/base-route';
+import scrollToTop from 'codecrafters-frontend/lib/scroll-to-top';
+import { inject as service } from '@ember/service';
+import { scheduleOnce } from '@ember/runloop';
 
 export default class ConceptRoute extends BaseRoute {
   allowsAnonymousAccess = true;
+  @service router;
   @service store;
+
+  constructor() {
+    super(...arguments);
+
+    this.router.on('routeDidChange', () => {
+      scheduleOnce('afterRender', this, scrollToTop);
+    });
+  }
 
   async model(params) {
     const allConcepts = await this.store.findAll('concept', { include: 'author,questions' });
