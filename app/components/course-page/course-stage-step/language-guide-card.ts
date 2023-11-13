2364606d3dc73a308bc41792067319d16cc0fa9b
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-11-13 18:44:58 +0000

    update

--- app/components/course-page/course-stage-step/language-guide-card.ts
+++ app/components/course-page/course-stage-step/language-guide-card.ts
@@ -1,10 +1,12 @@
 import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
 import CoursePageStateService from 'codecrafters-frontend/services/course-page-state';
 import Store from '@ember-data/store';
-import type { TemporaryCourseStageModel, TemporaryRepositoryModel } from 'codecrafters-frontend/lib/temporary-types';
-import { task } from 'ember-concurrency';
+import fade from 'ember-animated/transitions/fade';
+import type { TemporaryCourseStageModel, TemporaryLanguageModel, TemporaryRepositoryModel } from 'codecrafters-frontend/lib/temporary-types';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
 
 interface Signature {
   Element: HTMLDivElement;
@@ -19,6 +21,8 @@ export default class LanguageGuideCardComponent extends Component<Signature> {
   @service declare coursePageState: CoursePageStateService;
   @service declare store: Store;
 
+  @tracked requestedLanguage: TemporaryLanguageModel | null = null;
+
   loadLanguageGuidesTask = task({ keepLatest: true }, async (): Promise<void> => {
     await this.store.query('course-stage-language-guide', {
       course_stage_id: this.args.courseStage.id,
@@ -26,17 +30,33 @@ export default class LanguageGuideCardComponent extends Component<Signature> {
     });
   });
 
+  get currentLanguage(): TemporaryLanguageModel | null {
+    return this.requestedLanguage || this.defaultLanguage;
+  }
+
+  get defaultLanguage(): TemporaryLanguageModel | null {
+    if (this.args.repository.language) {
+      return this.args.repository.language;
+    }
+
+    return this.args.courseStage.languageGuides.sortBy('language.name')[0]?.language || null;
+  }
+
   get isLoading(): boolean {
     return this.loadLanguageGuidesTask.isRunning;
   }
 
   // Can return undefined if the language guide hasn't been loaded yet
   get languageGuide() {
-    if (!this.args.repository.language) {
+    if (!this.currentLanguage) {
       return undefined;
     }
 
-    return this.args.courseStage.languageGuides.findBy('language', this.args.repository.language);
+    return this.args.courseStage.languageGuides.findBy('language', this.currentLanguage);
+  }
+
+  get sortedLanguagesForDropdown(): TemporaryLanguageModel[] {
+    return this.args.courseStage.course.betaOrLiveLanguages.sortBy('name');
   }
 
   @action
@@ -44,6 +64,15 @@ export default class LanguageGuideCardComponent extends Component<Signature> {
     if (this.languageGuide) {
       this.languageGuide.createView();
     }
+  }
+
+  @action
+  handleRequestedLanguageChange(language: TemporaryLanguageModel): void {
+    this.requestedLanguage = language;
+  }
+
+  languageNameAnimationRules() {
+    return fade;
   }
 
   @action
