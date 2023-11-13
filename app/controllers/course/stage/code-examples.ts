173afb8f3fa999a0222676659e5b016c3a542b82
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-11-13 18:29:14 +0000

    extract language dropdown, ts-ify

--- app/controllers/course/stage/code-examples.ts
+++ app/controllers/course/stage/code-examples.ts
@@ -3,16 +3,25 @@ import rippleSpinnerImage from '/assets/images/icons/ripple-spinner.svg';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import type { TemporaryCourseStageModel, TemporaryLanguageModel, TemporaryRepositoryModel } from 'codecrafters-frontend/lib/temporary-types';
+import type Store from '@ember-data/store';
+import type CommunityCourseStageSolutionModel from 'codecrafters-frontend/models/community-course-stage-solution';
 
 export default class CodeExamplesController extends Controller {
+  declare model: {
+    courseStage: TemporaryCourseStageModel;
+    activeRepository: TemporaryRepositoryModel;
+  };
+
+  @service declare store: Store;
+
   rippleSpinnerImage = rippleSpinnerImage;
   @tracked isLoading = true;
-  @tracked solutions = [];
-  @service store;
-  @tracked requestedLanguage = null; // This shouldn't be state on the controller, see if we can move it to a query param or so?
+  @tracked solutions: CommunityCourseStageSolutionModel[] = [];
+  @tracked requestedLanguage: TemporaryLanguageModel | null = null; // This shouldn't be state on the controller, see if we can move it to a query param or so?
 
   get communitySolutionsAreAvailableForCurrentLanguage() {
-    return this.courseStage.hasCommunitySolutionsForLanguage(this.currentLanguage);
+    return this.currentLanguage && this.courseStage.hasCommunitySolutionsForLanguage(this.currentLanguage);
   }
 
   get courseStage() {
@@ -30,11 +39,19 @@ export default class CodeExamplesController extends Controller {
 
     const sortedBetaOrLiveLanguages = this.repository.course.betaOrLiveLanguages.sortBy('language.name');
 
-    return sortedBetaOrLiveLanguages.find((language) => this.courseStage.hasCommunitySolutionsForLanguage(language)) || sortedBetaOrLiveLanguages[0];
+    return (
+      sortedBetaOrLiveLanguages.find((language) => this.courseStage.hasCommunitySolutionsForLanguage(language)) ||
+      sortedBetaOrLiveLanguages[0] ||
+      null
+    );
   }
 
   get repository() {
     return this.model.activeRepository;
+  }
+
+  get sortedLanguagesForDropdown() {
+    return this.courseStage.course.betaOrLiveLanguages.sortBy('name');
   }
 
   get sortedSolutions() {
@@ -42,7 +59,7 @@ export default class CodeExamplesController extends Controller {
   }
 
   @action
-  handleRequestedLanguageChange(language) {
+  handleRequestedLanguageChange(language: TemporaryLanguageModel) {
     if (language === this.repository.language) {
       this.requestedLanguage = null;
     } else {
@@ -54,13 +71,17 @@ export default class CodeExamplesController extends Controller {
 
   @action
   async loadSolutions() {
+    if (!this.currentLanguage) {
+      return;
+    }
+
     this.isLoading = true;
 
-    this.solutions = await this.store.query('community-course-stage-solution', {
+    this.solutions = (await this.store.query('community-course-stage-solution', {
       course_stage_id: this.courseStage.id,
       language_id: this.currentLanguage.id,
       include: 'user,language,comments,comments.user,comments.target,course-stage',
-    });
+    })) as unknown as CommunityCourseStageSolutionModel[]; // TODO: Doesn't store.query support model type inference?
 
     this.isLoading = false;
   }
