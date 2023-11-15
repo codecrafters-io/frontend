f5392c325fefcb58b2ca88c4f0c44dba57bb2647
libmartinito <lib.martinito@pm.me>
2023-11-16 07:54:08 +0800

    CC-818 updated image import

--- app/components/affiliate-page/affiliate-feature-cards.ts
+++ app/components/affiliate-page/affiliate-feature-cards.ts
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
-import freeForFriendImage from '/assets/images/referral-program-features/free-for-friend.jpg';
-import lifetimeEarningsImage from '/assets/images/referral-program-features/lifetime-earnings.jpg';
-import simplePayoutImage from '/assets/images/referral-program-features/simple-payout.jpg';
+import freeForFriendImage from '/assets/images/affiliate-program-features/free-for-friend.jpg';
+import lifetimeEarningsImage from '/assets/images/affiliate-program-features/lifetime-earnings.jpg';
+import simplePayoutImage from '/assets/images/affiliate-program-features/simple-payout.jpg';
 
 interface Signature {
   Element: HTMLDivElement;
