4c9b533a4c2eeca27aedc2952e8675984c7722c7
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-11-17 19:10:37 +0000

    Refactor breadcrumb rendering in course-page header components

--- app/components/course-page/header/sticky-section.ts
+++ app/components/course-page/header/sticky-section.ts
@@ -4,6 +4,7 @@ import { action } from '@ember/object';
 import { StepList } from 'codecrafters-frontend/lib/course-page-step-list';
 import { TemporaryCourseModel } from 'codecrafters-frontend/lib/temporary-types';
 import { tracked } from '@glimmer/tracking';
+import CourseStageStep from 'codecrafters-frontend/lib/course-page-step-list/course-stage-step';
 
 type Signature = {
   Element: HTMLDivElement;
@@ -19,6 +20,10 @@ type Signature = {
 
 export default class StickySectionComponent extends Component<Signature> {
   @tracked scrollMarkerIsInViewport = true;
+
+  get currentStepAsCourseStageStep() {
+    return this.args.currentStep as CourseStageStep;
+  }
 
   get isSticky() {
     return !this.scrollMarkerIsInViewport;
