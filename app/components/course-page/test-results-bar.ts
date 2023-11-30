8de736aaa885196844537a71dbb2286eda264cb8
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-11-30 23:06:28 +0000

    Fix UI issues in course-page components

--- app/components/course-page/test-results-bar.ts
+++ app/components/course-page/test-results-bar.ts
@@ -1,4 +1,7 @@
+import { action } from '@ember/object';
+import { next } from '@ember/runloop';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { Step } from 'codecrafters-frontend/lib/course-page-step-list';
 import type CourseStageStep from 'codecrafters-frontend/lib/course-page-step-list/course-stage-step';
 
@@ -11,8 +14,29 @@ type Signature = {
 };
 
 export default class TestResultsBarComponent extends Component<Signature> {
+  @tracked isExpanded = false;
+
   get activeStepAsCourseStageStep() {
     return this.args.activeStep as CourseStageStep;
+  }
+
+  get isCollapsed() {
+    return !this.isExpanded;
+  }
+
+  @action
+  handleCollapseButtonClick() {
+    // Avoid conflict with handleCollapsedBarClick
+    next(() => {
+      this.isExpanded = false;
+    });
+  }
+
+  @action
+  handleCollapsedBarClick() {
+    if (this.isCollapsed) {
+      this.isExpanded = true;
+    }
   }
 }
 
