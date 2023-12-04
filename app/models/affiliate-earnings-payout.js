ed6ad732f715e762e92d1dc41dd21b7ef0233e64
libmartinito <lib.martinito@pm.me>
2023-12-04 18:47:22 +0800

    CC-850 added createdAt to the affiliate earnings payout model

--- app/models/affiliate-earnings-payout.js
+++ app/models/affiliate-earnings-payout.js
@@ -5,6 +5,7 @@ export default class AffiliateEarningsPayoutModel extends Model {
 
   @attr('number') amountInCents;
   @attr('date') completedAt;
+  @attr('date') createdAt;
   @attr('date') initiatedAt;
   @attr('string') lastFailureReason;
   @attr('') payoutMethodArgs;
