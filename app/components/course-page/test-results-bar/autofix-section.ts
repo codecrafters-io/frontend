fb0fc8790713180b7baa281cd79c47b90c18b34f
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-12-04 22:32:18 +0000

    Add autofix request creation task to AutofixSectionComponent

--- app/components/course-page/test-results-bar/autofix-section.ts
+++ app/components/course-page/test-results-bar/autofix-section.ts
@@ -1,8 +1,11 @@
+import type Store from '@ember-data/store';
+import { service } from '@ember/service';
 import Component from '@glimmer/component';
 import { Step } from 'codecrafters-frontend/lib/course-page-step-list';
 import type CourseStageStep from 'codecrafters-frontend/lib/course-page-step-list/course-stage-step';
 import type AutofixRequestModel from 'codecrafters-frontend/models/autofix-request';
 import type RepositoryModel from 'codecrafters-frontend/models/repository';
+import { task } from 'ember-concurrency';
 
 type Signature = {
   Element: HTMLDivElement;
@@ -17,6 +20,8 @@ type Signature = {
 };
 
 export default class AutofixSectionComponent extends Component<Signature> {
+  @service declare store: Store;
+
   get activeCourseStage() {
     if (this.args.activeStep.type === 'CourseStageStep') {
       return this.activeStepAsCourseStageStep.courseStage;
@@ -40,6 +45,20 @@ export default class AutofixSectionComponent extends Component<Signature> {
       return null;
     }
   }
+
+  createAutofixRequestTask = task({ drop: true }, async (): Promise<void> => {
+    if (!this.lastSubmission) {
+      return;
+    }
+
+    const autofixRequest = this.store.createRecord('autofix-request', {
+      submission: this.lastSubmission,
+    });
+
+    await autofixRequest.save();
+
+    this.args.onAutofixRequestCreated(autofixRequest);
+  });
 }
 
 declare module '@glint/environment-ember-loose/registry' {
