53ed0f8c316a8a9e55aa43f525a71be8f8399c39
Vasyl Marchuk <public@vasylmarchuk.com>
2023-12-05 14:08:42 +0200

    Skip the request instead of throwing an Error if unable to parse username

--- middleware.js
+++ middleware.js
@@ -6,11 +6,15 @@
  * - reads the contents of `dist/_empty.html`
  * - replaces default meta Image URL with Profile OG Image URL
  * - serves the result as an HTML response
+ * - passes request down the stack and returns if unable to extract `username`
  *
  * Related Docs:
  * - https://vercel.com/docs/functions/edge-middleware
  * - https://nodejs.org/api/esm.html#importmeta
+ * - https://vercel.com/docs/functions/edge-functions/vercel-edge-package#next
  */
+
+import { next } from '@vercel/edge';
 
 export const config = {
   matcher: '/users/:path*',
@@ -20,9 +24,13 @@ export default async function middleware(request) {
   // Parse the username from the request URL
   const username = request.url.match(/\/users\/([^/?]+)/)[1];
 
-  // Throw an error if username is missing
+  // Skip the request if username is missing
   if (!username) {
-    throw new Error('Invalid arguments');
+    // Log an error to the console
+    console.error('Unable to parse username from the URL:', request.url);
+
+    // Pass the request down the stack for processing and return
+    return next(request);
   }
 
   // Generate a proper OG Image URL for the username's Profile
