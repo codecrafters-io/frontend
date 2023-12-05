e202386c2f14d8fd50d2aefc68f8bd63088094ed
libmartinito <lib.martinito@pm.me>
2023-12-06 04:31:27 +0800

    CC-855 added og image for concept group route

--- app/routes/concept-group.ts
+++ app/routes/concept-group.ts
@@ -1,12 +1,28 @@
-import { inject as service } from '@ember/service';
 import BaseRoute from 'codecrafters-frontend/lib/base-route';
 import ConceptModel from 'codecrafters-frontend/models/concept';
+import ConceptGroupModel from 'codecrafters-frontend/models/concept-group';
+import config from 'codecrafters-frontend/config/environment';
+import HeadDataService from 'codecrafters-frontend/services/meta-data';
 import Store from '@ember-data/store';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class ConceptGroupRoute extends BaseRoute {
   allowsAnonymousAccess = true;
 
-  @service store!: Store;
+  @service declare metaData: HeadDataService;
+  @service declare store: Store;
+  @tracked previousMetaImageUrl: string | undefined = undefined;
+
+  async afterModel(model: { conceptGroup: ConceptGroupModel }) {
+    this.previousMetaImageUrl = this.metaData.imageUrl;
+    // @ts-ignore
+    this.metaData.imageUrl = `${config.x.metaTagImagesBaseURL}collection-${model.conceptGroup.slug}.png`;
+  }
+
+  deactivate() {
+    this.metaData.imageUrl = this.previousMetaImageUrl;
+  }
 
   async model(params: { concept_group_slug: string }) {
     const conceptGroup = await this.store.findRecord('concept-group', params.concept_group_slug, { include: 'author' });
