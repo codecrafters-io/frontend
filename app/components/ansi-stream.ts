6c6c683460439cec30585f5ffe55c38b8bf018c1
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-12-08 17:35:03 +0000

    Update ansi-stream.hbs and ansi-stream.ts in the app/components directory. The changes include adding a new import statement, adding a new tracked property, and updating the handleDidUpdateContent action.

--- app/components/ansi-stream.ts
+++ app/components/ansi-stream.ts
@@ -1,4 +1,7 @@
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
+import { task, timeout } from 'ember-concurrency';
+import { tracked } from 'tracked-built-ins';
 
 interface Signature {
   Element: HTMLPreElement;
@@ -8,7 +11,46 @@ interface Signature {
   };
 }
 
-export default class AnsiStreamComponent extends Component<Signature> {}
+export default class AnsiStreamComponent extends Component<Signature> {
+  @tracked displayContent: string = this.args.content;
+
+  @action
+  handleDidUpdateContent() {
+    this.consumeContentDeltasTask.perform();
+  }
+
+  consumeContentDeltasTask = task({ keepLatest: true }, async (): Promise<void> => {
+    // Content has been changed somehow
+    if (this.args.content.slice(0, this.displayContent.length) !== this.displayContent) {
+      this.displayContent = this.args.content;
+
+      return;
+    }
+
+    const newContentDelta = this.args.content.slice(this.displayContent.length);
+    const newContentDeltaChunks = newContentDelta.split(/(\s+)/);
+
+    let counter = 10;
+
+    while (newContentDeltaChunks.length > 0 && counter > 0) {
+      const chunksCount = Math.min(1, Math.floor(newContentDeltaChunks.length / 10));
+      const chunksToFlush = newContentDeltaChunks.splice(0, chunksCount);
+
+      this.displayContent += chunksToFlush.join('');
+      await timeout(100);
+      counter--;
+    }
+
+    // In case our math didn't work out, just flush the rest
+    if (newContentDeltaChunks.length > 0) {
+      this.displayContent += newContentDeltaChunks.join('');
+    }
+
+    if (this.args.content !== this.displayContent) {
+      this.consumeContentDeltasTask.perform();
+    }
+  });
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
