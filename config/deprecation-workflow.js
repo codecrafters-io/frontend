a23568b709255aa32e77ecb8339e1b522c8675e7
Vasyl Marchuk <public@vasylmarchuk.com>
2023-12-17 17:40:32 +0200

    Remove muted deprecation warnings that are no longer happening

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,12 +2,7 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'ember-global' },
-    { handler: 'silence', matchId: 'deprecated-run-loop-and-computed-dot-access' },
-    { handler: 'silence', matchId: 'implicit-injections' },
-    { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' },
     { handler: 'silence', matchId: 'ember-data:deprecate-non-strict-relationships' },
-    { handler: 'silence', matchId: 'ember-data:deprecate-early-static' }, // ember-cli-mirage
     { handler: 'silence', matchId: 'ember-data:deprecate-array-like' }, // needs to be fixed
     { handler: 'silence', matchId: 'ember-polyfills.deprecate-assign' }, // ember-api-actions
   ],
