b3f5cc735f01ddce9f2cfcfde246071a78e9fa56
Vasyl Marchuk <public@vasylmarchuk.com>
2024-01-03 16:23:39 +0200

    Fix all `hasMany(course-extension-activation)` inverse definitions

--- app/models/repository.ts
+++ app/models/repository.ts
@@ -38,7 +38,7 @@ export default class RepositoryModel extends Model {
 
   @hasMany('autofix-request', { async: false, inverse: 'repository' }) declare autofixRequests: AutofixRequestModel[];
   @belongsTo('course', { async: false, inverse: null }) declare course: CourseModel;
-  @hasMany('course-extension-activation', { async: false }) declare courseExtensionActivations: CourseExtensionActivation[];
+  @hasMany('course-extension-activation', { async: false, inverse: 'repository' }) declare courseExtensionActivations: CourseExtensionActivation[];
   @hasMany('course-stage-completion', { async: false, inverse: 'repository' }) declare courseStageCompletions: CourseStageCompletionModel[];
   @hasMany('course-stage-feedback-submission', { async: false, inverse: 'repository' })
   declare courseStageFeedbackSubmissions: CourseStageFeedbackSubmissionModel[];
