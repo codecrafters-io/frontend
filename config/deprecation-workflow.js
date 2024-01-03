6b28c559d3c789243a9e9301dab5f26e297ae93d
Vasyl Marchuk <public@vasylmarchuk.com>
2024-01-03 14:07:00 +0200

    Bump `ember-data:deprecate-non-strict-relationships` from `silence` to `throw`

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,7 +2,7 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'ember-data:deprecate-non-strict-relationships' },
+    { handler: 'throw', matchId: 'ember-data:deprecate-non-strict-relationships' },
     { handler: 'silence', matchId: 'ember-data:deprecate-array-like' }, // needs to be fixed
     { handler: 'silence', matchId: 'ember-polyfills.deprecate-assign' }, // ember-api-actions
   ],
