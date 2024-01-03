9051a85b54f58fc81bd0b5dc7927b3ed6acf58b8
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-01-03 17:09:03 +0000

    More layout

---
+++ app/components/contest-page/how-it-works-card.ts
@@ -0,0 +1,19 @@
+import Component from '@glimmer/component';
+import ContestModel from 'codecrafters-frontend/models/contest';
+
+type Signature = {
+  Element: HTMLDivElement;
+
+  Args: {
+    contest: ContestModel;
+  };
+};
+
+export default class ContestPageHowItWorksCardComponent extends Component<Signature> {
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'ContestPage::HowItWorksCard': typeof ContestPageHowItWorksCardComponent;
+  }
+}
---
+++ app/components/contest-page/prize-details-card.ts
@@ -0,0 +1,18 @@
+import Component from '@glimmer/component';
+import ContestModel from 'codecrafters-frontend/models/contest';
+
+type Signature = {
+  Element: HTMLDivElement;
+
+  Args: {
+    contest: ContestModel;
+  };
+};
+
+export default class ContestPagePrizeDetailsCardComponent extends Component<Signature> {}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'ContestPage::PrizeDetailsCard': typeof ContestPagePrizeDetailsCardComponent;
+  }
+}
