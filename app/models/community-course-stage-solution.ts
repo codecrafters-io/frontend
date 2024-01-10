412a2998be60b0d7636d8649fca3329d2a0ed8b2
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-01-10 21:19:05 +0000

    Update community-solution-card.hbs and community-solution-card.ts files, and add rating-related attributes to the community-course-stage-solution.ts file.

--- app/models/community-course-stage-solution.ts
+++ app/models/community-course-stage-solution.ts
@@ -10,15 +10,18 @@ export default class CommunityCourseStageSolutionModel extends Model {
   @belongsTo('language', { async: false, inverse: null }) declare language: LanguageModel;
   @belongsTo('user', { async: false, inverse: null }) declare user: UserModel;
 
-  @hasMany('community-course-stage-solution-comment', { async: false, inverse: 'target' }) declare comments: CourseStageCommentModel;
+  @hasMany('community-course-stage-solution-comment', { async: false, inverse: 'target' }) declare comments: CourseStageCommentModel[];
 
   // @ts-expect-error empty '' not supported
-  @attr('') changedFiles: unknown; // free-form JSON
+  @attr('') changedFiles: { diff: string; filename: string }[]; // free-form JSON
 
   @attr('string') declare explanationMarkdown: string;
   @attr('string') declare commitSha: string;
   @attr('string') declare githubRepositoryName: string;
   @attr('boolean') declare githubRepositoryIsPrivate: boolean;
+  @attr('number') declare ratingEstimate: number | null;
+  @attr('number') declare ratingMean: number | null;
+  @attr('number') declare ratingStandardDeviation: number | null;
   @attr('date') declare submittedAt: Date;
   @attr('boolean') declare isRestrictedToTeam: boolean; // if true, only fellow team members can see this solution
 
