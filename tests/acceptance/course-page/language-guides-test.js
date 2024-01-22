d587d4e5383f193f41206754fcd3d26c47a5a76f
libmartinito <lib.martinito@pm.me>
2024-01-22 13:35:40 +0800

    Added test for feedback submissions for the case where we do have data

--- tests/acceptance/course-page/language-guides-test.js
+++ tests/acceptance/course-page/language-guides-test.js
@@ -48,4 +48,47 @@ module('Acceptance | course-page | language-guides', function (hooks) {
     await coursePage.languageGuideCard.backToRepositoryLanguageButton.click();
     assert.strictEqual(coursePage.languageGuideCard.languageDropdown.currentLanguageName, 'Python');
   });
+
+  test('can submit feedback for language guides', async function (assert) {
+    testScenario(this.server);
+    signInAsStaff(this.owner, this.server);
+
+    let currentUser = this.server.schema.users.first();
+    let python = this.server.schema.languages.findBy({ name: 'Python' });
+    let redis = this.server.schema.courses.findBy({ slug: 'redis' });
+
+    this.server.create('course-stage-language-guide', {
+      markdownForBeginner: 'In this stage, blah blah...',
+      courseStage: redis.stages.models.sortBy('position')[1],
+      language: python,
+    });
+
+    this.server.create('repository', 'withFirstStageCompleted', {
+      course: redis,
+      language: python,
+      user: currentUser,
+    });
+
+    await catalogPage.visit();
+    await catalogPage.clickOnCourse('Build your own Redis');
+
+    document.getElementById('language-guide-card')?.scrollIntoView();
+
+    await coursePage.languageGuideCard.clickOnExpandButton();
+    await coursePage.languageGuideCard.clickOnCollapseButton();
+    await coursePage.languageGuideCard.clickOnExpandButton();
+
+    await coursePage.languageGuideCard.languageDropdown.toggle();
+    await coursePage.languageGuideCard.languageDropdown.clickOnLink('Go');
+    assert.strictEqual(coursePage.languageGuideCard.languageDropdown.currentLanguageName, 'Go');
+
+    const feedbackDropdown = coursePage.languageGuideCard.feedbackDropdown;
+    await feedbackDropdown.toggle();
+    await feedbackDropdown.fillInExplanation('This is test feedback for language guides');
+    await feedbackDropdown.clickOnSendButton();
+
+    const feedbackSubmission = this.server.schema.siteFeedbackSubmissions.first();
+    assert.strictEqual(feedbackSubmission.source, 'course_stage_language_guide')
+    assert.strictEqual(Object.keys(feedbackSubmission.sourceMetadata).length, 1)
+  });
 });
