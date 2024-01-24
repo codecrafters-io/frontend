e23f66719a5971118466633cd7d7f18b24350674
libmartinito <lib.martinito@pm.me>
2024-01-24 22:59:19 +0800

    CC-958 added test for course overview notice

--- tests/acceptance/view-course-overview-test.js
+++ tests/acceptance/view-course-overview-test.js
@@ -1,5 +1,6 @@
 import catalogPage from 'codecrafters-frontend/tests/pages/catalog-page';
 import courseOverviewPage from 'codecrafters-frontend/tests/pages/course-overview-page';
+import FakeDateService from 'codecrafters-frontend/tests/support/fake-date-service';
 import percySnapshot from '@percy/ember';
 import testScenario from 'codecrafters-frontend/mirage/scenarios/test';
 import { currentURL } from '@ember/test-helpers';
@@ -62,5 +63,36 @@ module('Acceptance | view-course-overview', function (hooks) {
     await percySnapshot('Course Overview - No Extensions');
 
     assert.strictEqual(currentURL(), '/courses/docker/overview');
+  });
+
+  test('it has the notice for when a course is in beta status', async function (assert) {
+    testScenario(this.server);
+    signIn(this.owner, this.server);
+
+    this.server.schema.courses.findBy({ slug: 'redis' }).update({ releaseStatus: 'beta' });
+
+    await catalogPage.visit();
+    await catalogPage.clickOnCourse('Build your own Redis');
+
+    assert.strictEqual(courseOverviewPage.betaNoticeText, "This challenge is free to try when it's in beta. We keep challenges in beta for a few weeks to gather feedback.");
+  });
+
+  test('it has the notice for when a course is free', async function (assert) {
+    testScenario(this.server);
+    signIn(this.owner, this.server);
+
+    this.owner.register('service:date', FakeDateService)
+
+    let dateService = this.owner.lookup('service:date');
+    let now = new Date('2024-01-01').getTime();
+    dateService.setNow(now);
+
+    let isFreeExpirationDate = new Date(dateService.now() + 20 * 24 * 60 * 60 * 1000);
+    this.server.schema.courses.findBy({ slug: 'redis' }).update('isFreeUntil', isFreeExpirationDate);
+
+    await catalogPage.visit();
+    await catalogPage.clickOnCourse('Build your own Redis');
+
+    assert.strictEqual(courseOverviewPage.freeNoticeText, "We're keeping this course free until 21 January 2024 to gather feedback.");
   });
 });
