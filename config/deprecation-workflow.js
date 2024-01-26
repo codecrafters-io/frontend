a4a3bddcc2f363284fc0a8f70234219cad47901b
Vasyl Marchuk <public@vasylmarchuk.com>
2024-01-26 15:17:38 +0200

    Add fixed deprecatations as `throw` to `deprecation-workflow.js`

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -3,5 +3,8 @@ self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
     { handler: 'silence', matchId: 'ember-data:deprecate-array-like' }, // needs to be fixed
+    { handler: 'throw', matchId: 'ember-data:deprecate-legacy-imports' }, // will be removed in ember 6
+    { handler: 'throw', matchId: 'ember-data:deprecate-non-strict-types' }, // will be removed in ember 6
+    { handler: 'throw', matchId: 'ember-data:deprecate-non-unique-relationship-entries' }, // will be removed in ember 6
   ],
 };
