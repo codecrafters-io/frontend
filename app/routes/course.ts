cfbdc92b8830d8cb360cd507bd23b5e6e6ff9b1c
libmartinito <lib.martinito@pm.me>
2024-01-31 16:03:35 +0800

    CC-972 reverted changes to course route

--- app/routes/course.ts
+++ app/routes/course.ts
@@ -69,13 +69,9 @@ export default class CourseRoute extends BaseRoute {
     } else {
       // @ts-ignore
       const lastPushedRepository = repositories.filterBy('firstSubmissionCreated').sortBy('lastSubmissionAt').at(-1);
-      // @ts-ignore
-      const lastCreatedRepository = repositories.sortBy('createdAt').at(-1);
 
       if (lastPushedRepository) {
         return lastPushedRepository;
-      } else if (lastCreatedRepository) {
-        return lastCreatedRepository;
       } else {
         return this.store.createRecord('repository', { course: course, user: this.authenticator.currentUser });
       }
