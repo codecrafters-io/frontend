5c6ff907e1bc2687a330ae5cffd0da281ad2fde6
Vasyl Marchuk <public@vasylmarchuk.com>
2024-02-02 14:11:07 +0200

    Add tests for redirections when visiting a valid or invalid `concept-group`

--- tests/acceptance/concept-groups-test.js
+++ tests/acceptance/concept-groups-test.js
@@ -7,6 +7,7 @@ import testScenario from 'codecrafters-frontend/mirage/scenarios/test';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
+import { currentURL } from '@ember/test-helpers';
 
 function createConcepts(server) {
   createConceptFromFixture(server, tcpOverview);
@@ -16,6 +17,34 @@ function createConcepts(server) {
 module('Acceptance | concept-groups-test', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+
+  test('displays concept-group page when visiting a valid concept-group', async function (assert) {
+    testScenario(this.server);
+    createConcepts(this.server);
+
+    const user = this.server.schema.users.first();
+
+    const conceptGroup = this.server.create('concept-group', {
+      author: user,
+      description_markdown: 'Dummy description',
+      concept_slugs: ['tcp-overview', 'network-protocols'],
+      slug: 'test-concept-group',
+      title: 'Test Concept Group',
+    });
+
+    await conceptGroupsPage.visit({ concept_group_slug: conceptGroup.slug });
+
+    assert.strictEqual(currentURL(), '/collections/test-concept-group');
+  });
+
+  test('redirects to / when visiting a non-existing concept-group', async function (assert) {
+    testScenario(this.server);
+    createConcepts(this.server);
+
+    await conceptGroupsPage.visit({ concept_group_slug: 'non-existent-concept-group' });
+
+    assert.strictEqual(currentURL(), '/catalog');
+  });
 
   test('displays the correct concept group details for the header', async function (assert) {
     testScenario(this.server);
