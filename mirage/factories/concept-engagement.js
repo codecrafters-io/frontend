0f9e5f46b42b49c9fea504355c24fbb23404171d
libmartinito <lib.martinito@pm.me>
2024-02-14 17:51:40 +0800

    CC-711 added default values in concept engagement factory

---
+++ mirage/factories/concept-engagement.js
@@ -0,0 +1,7 @@
+import { Factory, trait } from 'miragejs';
+
+export default Factory.extend({
+  currentProgressPercentage: 0,
+  lastActivityAt: () => new Date(),
+  startedAt: () => new Date(),
+});
