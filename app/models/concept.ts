6e95563a66caa9a6425fa16f9b66a30616bd6bed
libmartinito <lib.martinito@pm.me>
2024-02-14 18:15:36 +0800

    CC-711 renamed conceptEngagements to engagements

--- app/models/concept.ts
+++ app/models/concept.ts
@@ -16,7 +16,7 @@ export type BlockJSON = {
 export default class ConceptModel extends Model {
   @belongsTo('user', { async: false, inverse: null }) declare author: UserModel;
 
-  @hasMany('concept-engagement', { async: false, inverse: 'concept' }) declare conceptEngagements: ConceptEngagementModel[];
+  @hasMany('concept-engagement', { async: false, inverse: 'concept' }) declare engagements: ConceptEngagementModel[];
   @hasMany('concept-question', { async: false, inverse: 'concept' }) declare questions: SyncHasMany<ConceptQuestion>;
 
   @attr('string') declare descriptionMarkdown: string;
