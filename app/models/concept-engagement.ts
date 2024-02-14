fdfc83eab1d9458a4e4b02a730bb92b6abc3e310
libmartinito <lib.martinito@pm.me>
2024-02-14 18:18:41 +0800

    CC-711 renamed conceptEngagements to engagements in concept model

--- app/models/concept-engagement.ts
+++ app/models/concept-engagement.ts
@@ -4,7 +4,7 @@ import Model from '@ember-data/model';
 import { attr, belongsTo } from '@ember-data/model';
 
 export default class ConceptEngagementModel extends Model {
-  @belongsTo('concept', { async: false, inverse: 'conceptEngagements' }) declare concept: ConceptModel;
+  @belongsTo('concept', { async: false, inverse: 'engagements' }) declare concept: ConceptModel;
   @belongsTo('user', { async: false, inverse: 'conceptEngagements' }) declare user: UserModel;
   @attr('date') declare completedAt?: Date;
   @attr('number') declare currentProgressPercentage: number;
