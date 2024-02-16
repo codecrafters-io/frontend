eaf026bf636fafa39e6d0b70be24da65ae2414cf
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-02-16 12:14:26 +0000

    feat: add share progress modal component and functionality
    The changes in this commit add a new component called ShareProgressModalComponent. This component is responsible for displaying a modal that allows users to share their progress with friends. It includes a textarea where users can enter a message, as well as buttons to share on different platforms. 
    
    The commit also includes changes to the template file to add the necessary markup for the modal component and its functionality. It adds a button that triggers the modal to open when clicked. 
    
    Additionally, there are changes to the CoursePage component to include the ShareProgressModal component and handle its opening and closing. 
    
    These changes were made to enhance the user experience by providing a way for users to easily share their progress with others.

--- app/components/course-page/completed-step-notice.ts
+++ app/components/course-page/completed-step-notice.ts
@@ -1,5 +1,7 @@
 import { service } from '@ember/service';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import type RepositoryModel from 'codecrafters-frontend/models/repository';
 import type CoursePageStateService from 'codecrafters-frontend/services/course-page-state';
 import type { Step } from 'codecrafters-frontend/utils/course-page-step-list';
 
@@ -8,11 +10,13 @@ type Signature = {
 
   Args: {
     step: Step;
+    repository: RepositoryModel;
   };
 };
 
 export default class CompletedStepNoticeComponent extends Component<Signature> {
   @service declare coursePageState: CoursePageStateService;
+  @tracked x = true; // TEMP for preview
 
   get activeStep() {
     return this.coursePageState.activeStep;
---
+++ app/components/course-page/share-progress-modal.ts
@@ -0,0 +1,51 @@
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import RepositoryModel from 'codecrafters-frontend/models/repository';
+import { task, timeout } from 'ember-concurrency';
+import fade from 'ember-animated/transitions/fade';
+
+interface Signature {
+  Element: HTMLDivElement;
+
+  Args: {
+    repository: RepositoryModel;
+    onClose: () => void;
+  };
+
+  Blocks: {
+    default: [];
+  };
+}
+
+export default class ShareProgressModalComponent extends Component<Signature> {
+  fade = fade;
+
+  @tracked wasCopiedRecently = false;
+  @tracked copyableText = 'I just completed...';
+
+  constructor(owner: unknown, args: Signature['Args']) {
+    super(owner, args);
+    this.copyableText =
+      'Just completed Stage #2 of the @codecraftersio Build your own Redis challenge in Rust.\n\nhttps://app.codecrafters.io/courses/redis/overview';
+  }
+
+  @action
+  handleCopyButtonClick() {
+    this.copyToClipboardAndFlashMessage.perform();
+  }
+
+  copyToClipboardAndFlashMessage = task({ keepLatest: true }, async (): Promise<void> => {
+    await navigator.clipboard.writeText('dummy');
+
+    this.wasCopiedRecently = true;
+    await timeout(3000);
+    this.wasCopiedRecently = false;
+  });
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'CoursePage::ShareProgressModal': typeof ShareProgressModalComponent;
+  }
+}
