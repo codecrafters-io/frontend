f9a8ad75a61e184a3a5cb1613220494be50f5a59
libmartinito <lib.martinito@pm.me>
2024-02-18 23:57:25 +0800

    CC-993 moved remaining blocks count to concept engagement

--- app/components/concept-progress.ts
+++ app/components/concept-progress.ts
@@ -1,18 +1,18 @@
 import Component from '@glimmer/component';
+import ConceptEngagementModel from 'codecrafters-frontend/models/concept-engagement';
 import fade from 'ember-animated/transitions/fade';
 
 interface Signature {
   Element: HTMLDivElement;
 
   Args: {
-    progressPercentage: number;
-    remainingBlocksCount: number;
+    latestConceptEngagement: ConceptEngagementModel;
   };
 }
 
 export default class ConceptProgressComponent extends Component<Signature> {
   get widthStyle(): string {
-    return `width: ${this.args.progressPercentage}%`;
+    return `width: ${this.args.latestConceptEngagement.currentProgressPercentage}%`;
   }
 
   rules() {
