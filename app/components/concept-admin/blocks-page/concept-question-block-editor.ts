407e48cd3af30c2ef0f0db6b51c2292094e36e59
Ryan Gang <ryan-gg@outlook.com>
2024-02-19 16:08:54 +0000

    CC-982 : Update questions dropdown to disable used questions.

--- app/components/concept-admin/blocks-page/concept-question-block-editor.ts
+++ app/components/concept-admin/blocks-page/concept-question-block-editor.ts
@@ -1,4 +1,4 @@
-import { action } from '@ember/object';
+import { action, get } from '@ember/object';
 import Component from '@glimmer/component';
 import { ConceptQuestionBlock } from 'codecrafters-frontend/utils/blocks';
 import ConceptModel from 'codecrafters-frontend/models/concept';
@@ -13,6 +13,41 @@ interface Signature {
 }
 
 export default class ConceptQuestionBlockEditorComponent extends Component<Signature> {
+  get disabledQuestions() {
+    const usedSlugs = this.usedQuestionSlugs;
+
+    return this.args.concept.questions
+      .filter((question) => {
+        return usedSlugs.includes(question.slug);
+      })
+      .sort((a, b) => a.queryMarkdown.localeCompare(b.queryMarkdown));
+  }
+
+  get enabledQuestions() {
+    const usedSlugs = this.usedQuestionSlugs;
+
+    return this.args.concept.questions
+      .filter((question) => {
+        return !usedSlugs.includes(question.slug);
+      })
+      .sort((a, b) => a.queryMarkdown.localeCompare(b.queryMarkdown));
+  }
+
+  get remainingQuestionCount() {
+    const questions = this.args.concept.questions || [];
+
+    // eslint-disable-next-line ember/no-get
+    return get(questions, 'length') - this.usedQuestionSlugs.length;
+  }
+
+  get usedQuestionSlugs() {
+    const questionSlugs = this.args.concept.blocks
+      .filter((block) => block.type === 'concept_question')
+      .map((block) => block.args['concept_question_slug']);
+
+    return [...new Set(questionSlugs)];
+  }
+
   @action
   handleConceptQuestionSlugSelected(event: Event) {
     this.args.model.conceptQuestionSlug = (event.target! as HTMLSelectElement).value;
