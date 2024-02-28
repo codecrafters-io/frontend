67a9f7009b75e252c52fc6e2aa34c66c4f6ff426
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-02-28 13:36:47 +0000

    feat: add voting functionality for future extensions in ConfigureExtensionsModalComponent
    - Added `store` service injection in `ConfigureExtensionsModalComponent`
    - Modified constructor to fetch all `course-extension-idea` records from the store
    - Added `orderedCourseExtensionIdeas` computed property to sort and reverse the `extensionIdeas` array
    - Updated the markup in the template to display a section for voting on future extensions
    - Added a heading and information icon with tooltip for the voting section
    - Added a grid layout for the voting cards, including a card for submitting new extension ideas and cards for existing extension ideas
    
    This change allows users to vote on future extensions they are interested in.

--- app/components/course-page/configure-extensions-modal.ts
+++ app/components/course-page/configure-extensions-modal.ts
@@ -1,5 +1,7 @@
 import Component from '@glimmer/component';
 import RepositoryModel from 'codecrafters-frontend/models/repository';
+import Store from '@ember-data/store';
+import { inject as service } from '@ember/service';
 
 type Signature = {
   Element: HTMLDivElement;
@@ -10,7 +12,19 @@ type Signature = {
   };
 };
 
-export default class ConfigureExtensionsModalComponent extends Component<Signature> {}
+export default class ConfigureExtensionsModalComponent extends Component<Signature> {
+  @service declare store: Store;
+
+  constructor(owner: unknown, args: Signature['Args']) {
+    super(owner, args);
+
+    this.store.findAll('course-extension-idea', { include: 'course' });
+  }
+
+  get orderedCourseExtensionIdeas() {
+    return this.args.repository.course.extensionIdeas.sortBy('reverseSortPositionForVotePage').reverse();
+  }
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
