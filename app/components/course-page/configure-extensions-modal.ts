a9c3c88ae5b6e80f63d1653219514f061c20be05
libmartinito <lib.martinito@pm.me>
2024-03-07 22:51:39 +0800

    refactor: add course extension ideas in ConfigureExtensionsModalComponent

--- app/components/course-page/configure-extensions-modal.ts
+++ app/components/course-page/configure-extensions-modal.ts
@@ -1,16 +1,22 @@
 import Component from '@glimmer/component';
+import CourseExtensionIdeaModel from 'codecrafters-frontend/models/course-extension-idea';
 import RepositoryModel from 'codecrafters-frontend/models/repository';
 
 type Signature = {
   Element: HTMLDivElement;
 
   Args: {
+    courseExtensionIdeas: CourseExtensionIdeaModel[];
     repository: RepositoryModel;
     onClose: () => void;
   };
 };
 
-export default class ConfigureExtensionsModalComponent extends Component<Signature> {}
+export default class ConfigureExtensionsModalComponent extends Component<Signature> {
+  get orderedCourseExtensionIdeas() {
+    return this.args.courseExtensionIdeas.sortBy('reverseSortPositionForVotePage').reverse();
+  }
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
