7569d05d41b4a87193c10df03cc65d0992aff292
libmartinito <lib.martinito@pm.me>
2024-03-12 21:20:57 +0800

    feat: add CourseAdminTesterVersionRoute with model method fetching course-tester-version record and related data

---
+++ app/routes/course-admin/tester-version.ts
@@ -0,0 +1,18 @@
+import { inject as service } from '@ember/service';
+import BaseRoute from 'codecrafters-frontend/utils/base-route';
+import Store from '@ember-data/store';
+
+export default class CourseAdminTesterVersionRoute extends BaseRoute {
+  @service declare store: Store;
+
+  async model(params: { tester_version_id: string }) {
+    // @ts-ignore
+    const course = this.modelFor('course-admin').course;
+    const testerVersion = await this.store.findRecord('course-tester-version', params.tester_version_id, { include: 'applier,course' });
+
+    return {
+      course: course,
+      testerVersion: testerVersion,
+    };
+  }
+}
