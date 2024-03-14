3a1cd82a70e39fda9c40ffb12469b33b1efacfd5
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-03-14 13:55:57 +0000

    refactor: Update class name and method in StageInsightsController
    Updated the class name from CourseTesterVersionsController to StageInsightsController and added a new method sortedDroppedOffParticipations to filter and sort dropped off participations.
    
    feat: Fetch course-stage-participation data in StageInsightsIndexRoute
    
    Added a query to fetch course-stage-participation data including related models like user, language, and stage in StageInsightsIndexRoute.
    
    style: Adjust layout and add Dropped Off Users section
    
    Adjusted the layout in the template, updated margin, and added a new section for Dropped Off Users with relevant information and tooltip.
    
    feat: Create ParticipationListItemComponent for rendering participation list items
    
    Added ParticipationListItemComponent to render individual participation list items in the Dropped Off Users section.

--- app/controllers/course-admin/stage-insights.ts
+++ app/controllers/course-admin/stage-insights.ts
@@ -1,10 +1,17 @@
 import Controller from '@ember/controller';
 import type { ModelType } from 'codecrafters-frontend/routes/course-admin/stage-insights';
 
-export default class CourseTesterVersionsController extends Controller {
+export default class StageInsightsController extends Controller {
   declare model: ModelType;
 
   get participationAnalysis() {
     return this.model.stage.participationAnalysis!;
+  }
+
+  get sortedDroppedOffParticipations() {
+    return this.model.stage.participations
+      .filter((participation) => participation.status === 'dropped_off')
+      .sortBy('lastAttemptAt')
+      .reverse();
   }
 }
