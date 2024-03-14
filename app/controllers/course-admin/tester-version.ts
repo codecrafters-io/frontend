7934508bf9dd912f042585c0ef6f32cfc94f17e9
libmartinito <lib.martinito@pm.me>
2024-03-14 22:11:53 +0800

    refactor: update activation logic to handle confirmation for non-latest tester version before activation.

--- app/controllers/course-admin/tester-version.ts
+++ app/controllers/course-admin/tester-version.ts
@@ -22,15 +22,45 @@ export default class CourseAdminTesterVersionController extends Controller {
   @tracked isDeprovisioningTestRunners = false;
   @tracked shouldShowDeprovisioningNotice = false;
 
+  get activationConfirmationMessage() {
+    // @ts-ignore
+    const latestTesterVersion = this.model.testerVersion.course.latestTesterVersion;
+
+    if (latestTesterVersion) {
+      return `${latestTesterVersion.tagName} is the latest version. Are you sure you want to activate ${this.model.testerVersion.tagName} instead?`;
+    } else {
+      return `${this.model.testerVersion.tagName} is not the latest version. Are you sure you want to activate it?`;
+    }
+  }
+
   @action
-  async handleActivateButtonClick() {
+  async activate() {
     if (!this.isActivating) {
       this.isActivating = true;
 
       // @ts-ignore
       await this.model.testerVersion.activate();
 
+      await this.store.query('course-tester-version', {
+        // @ts-ignore
+        course_id: this.model.testerVersion.course.id,
+        include: ['course', 'activator'].join(','),
+      });
+
       this.isActivating = false;
+    }
+  }
+
+  @action
+  async handleActivateButtonClick() {
+    if (this.model.testerVersion.isLatest) {
+      await this.activate();
+
+      return;
+    }
+
+    if (window.confirm(this.activationConfirmationMessage)) {
+      await this.activate();
     }
   }
 
