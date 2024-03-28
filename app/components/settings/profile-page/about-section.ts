c46a45903023436bab51c7c3ef9cee93a008640d
libmartinito <lib.martinito@pm.me>
2024-03-28 21:34:02 +0800

    fix: add tracking of profile description update in AboutSectionComponent

--- app/components/settings/profile-page/about-section.ts
+++ app/components/settings/profile-page/about-section.ts
@@ -1,7 +1,9 @@
-import { action } from '@ember/object';
+import type AnalyticsEventTrackerService from 'codecrafters-frontend/services/analytics-event-tracker';
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import type UserModel from 'codecrafters-frontend/models/user';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 import { task, timeout } from 'ember-concurrency';
 
 interface Signature {
@@ -13,12 +15,18 @@ interface Signature {
 }
 
 export default class AboutSectionComponent extends Component<Signature> {
+  @service declare analyticsEventTracker: AnalyticsEventTrackerService;
+
   @tracked isUpdating = false;
   @tracked wasUpdatedRecently = false;
 
   @action
   async handleBlur() {
     this.updateValue.perform();
+
+    if (!this.isUpdating && !this.wasUpdatedRecently) {
+      this.analyticsEventTracker.track('updated_profile_description', {});
+    }
   }
 
   updateValue = task({ keepLatest: true }, async (): Promise<void> => {
