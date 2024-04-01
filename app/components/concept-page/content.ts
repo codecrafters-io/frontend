ef7265a9a3198a5e282a769ad1541292b81c1505
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-04-01 11:36:51 +0100

    feat: Update CopyableCode component to support dynamic background color based on prop
    Removed hardcoded background color class and added conditional class based on value of props.backgroundColor in the CopyableCode component in copyable-code.hbs. Also added backgroundColor property to the Args interface in copyable-code.ts for type safety.
    The changes were made to enhance the flexibility of the CopyableCode component by allowing the background color to be dynamically set based on the value passed in the props.

--- app/components/concept-page/content.ts
+++ app/components/concept-page/content.ts
@@ -5,7 +5,6 @@ import ConceptEngagementModel from 'codecrafters-frontend/models/concept-engagem
 import ConceptGroupModel from 'codecrafters-frontend/models/concept-group';
 import ConceptModel from 'codecrafters-frontend/models/concept';
 import Store from '@ember-data/store';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 interface Signature {
@@ -40,12 +39,5 @@ export default class ContentComponent extends Component<Signature> {
     const completedBlocksCount = Math.round((this.currentProgressPercentage / 100) * allBlocks.length);
 
     return allBlocks.length - completedBlocksCount;
-  }
-
-  @action
-  handleCopyButtonClick() {
-    this.analyticsEventTracker.track('clicked_share_concept_button', {
-      concept_id: this.args.concept.id,
-    });
   }
 }
