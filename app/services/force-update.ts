f4ad97b2b6fda62c506d85c886eaba504d4ce488
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-04-02 19:20:02 +0100

    chore: Adjusted poll interval and added workaround to skip first poll

--- app/services/force-update.ts
+++ app/services/force-update.ts
@@ -1,24 +1,36 @@
-import { action } from '@ember/object';
-import { runTask, pollTask, cancelPoll } from 'ember-lifeline';
 import Service, { service } from '@ember/service';
 import VersionTrackerService from './version-tracker';
+import config from 'codecrafters-frontend/config/environment';
 import type RouterService from '@ember/routing/router-service';
+import { action } from '@ember/object';
+import { runTask, pollTask, cancelPoll } from 'ember-lifeline';
+import { tracked } from 'tracked-built-ins';
 
 export default class ForceUpdateService extends Service {
-  POLL_INTERVAL_SECONDS = VersionTrackerService.VERSION_CHECK_INTERVAL_SECONDS;
+  POLL_INTERVAL_SECONDS = VersionTrackerService.VERSION_CHECK_INTERVAL_SECONDS / 10;
 
   @service declare router: RouterService;
   @service declare versionTracker: VersionTrackerService;
 
+  @tracked isFirstPollComplete = false;
+
   @action
   async poll(next: () => void) {
-    console.log('poll');
     await this.versionTracker.fetchLatestVersionIfNeeded();
 
     if (this.versionTracker.currentVersionIsIncompatible && this.router.currentRouteName !== 'update-required') {
       this.router.transitionTo('update-required', { queryParams: { next: `${this.router.currentURL}` } });
+
+      return;
+    }
+
+    // This workaround is required to skip the very first poll, so that tests don't lag
+    // https://github.com/ember-lifeline/ember-lifeline/issues/72
+    if (this.isFirstPollComplete) {
+      runTask(this, next, config.environment == 'test' ? 0 : this.POLL_INTERVAL_SECONDS * 1000);
     } else {
-      runTask(this, next, this.POLL_INTERVAL_SECONDS * 1000);
+      this.isFirstPollComplete = true;
+      next();
     }
   }
 
