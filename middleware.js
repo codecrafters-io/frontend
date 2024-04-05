3366bef7dff884c8aca79f3915f0e7968f1400d6
libmartinito <lib.martinito@pm.me>
2024-04-05 18:59:35 +0800

    refactor: Update middleware to handle user profiles and concept routes, dynamically generate OG Image URLs and meta tags based on route type.

--- middleware.js
+++ middleware.js
@@ -19,17 +19,18 @@ import { next } from '@vercel/edge';
 import replaceMetaTag from './app/utils/replace-meta-tag';
 
 export const config = {
-  // Limit the middleware to run only for user profile routes
+  // Limit the middleware to run only for user profile and concept routes
   // RegExp syntax uses rules from pillarjs/path-to-regexp
-  matcher: '/users/:path*',
+  matcher: ['/users/:path*', '/concepts/:path*']
 };
 
 export default async function middleware(request) {
-  // Parse the username from the request URL
+  // Parse the username or concept slug from the request URL
   const username = request.url.match(/\/users\/([^/?]+)/)[1];
+  const conceptSlug = request.url.match(/\/concepts\/([^/?]+)/)[1];
 
-  // Skip the request if username is missing
-  if (!username) {
+  // Skip the request if username or concept slug is missing
+  if (!username || !conceptSlug) {
     // Log an error to the console
     console.error('Unable to parse username from the URL:', request.url);
 
@@ -37,10 +38,29 @@ export default async function middleware(request) {
     return next(request);
   }
 
-  // Generate a proper OG Image URL for the username's Profile
-  const profileImageUrl = `https://og.codecrafters.io/api/user_profile/${username}.png`;
-  const pageTitle = `${username}'s CodeCrafters Profile`;
-  const pageDescription = `View ${username}'s profile on CodeCrafters`;
+  let pageImageUrl
+  let pageTitle
+  let pageDescription
+
+  if (username) {
+    // Generate a proper OG Image URL for the username's Profile
+    pageImageUrl = `https://og.codecrafters.io/api/user_profile/${username}`;
+    pageTitle = `${username}'s CodeCrafters Profile`;
+    pageDescription = `View ${username}'s profile on CodeCrafters`;
+  }
+
+  if (conceptSlug) {
+    // Convert the slug('network-protocols') to title('Network Protocols')
+    const conceptTitle = conceptSlug
+      .split('-')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ');
+
+    // Generate a proper OG Image URL for the concept
+    pageImageUrl = `https://og.codecrafters.io/api/concept/${conceptSlug}`;
+    pageTitle = conceptTitle
+    pageDescription = `View the ${conceptTitle} concept on CodeCrafters`;
+  }
 
   // Determine URL for reading local `/dist/_empty.html`
   const indexFileURL = new URL('./dist/_empty.html', import.meta.url);
@@ -55,10 +75,10 @@ export default async function middleware(request) {
     ['name', 'description', pageDescription], // <meta name="description" content="...">
     ['property', 'og:title', pageTitle],
     ['property', 'og:description', pageDescription],
-    ['property', 'og:image', profileImageUrl],
+    ['property', 'og:image', pageImageUrl],
     ['name', 'twitter:title', pageTitle],
     ['name', 'twitter:description', pageDescription],
-    ['name', 'twitter:image', profileImageUrl],
+    ['name', 'twitter:image', pageImageUrl],
   ].reduce((text, args) => replaceMetaTag(text, ...args), indexFileText);
 
   // Serve the result as HTML
