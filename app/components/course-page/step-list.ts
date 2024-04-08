c595de3aa2c9c0d5479008ec0db0b809b877bc97
libmartinito <lib.martinito@pm.me>
2024-04-08 23:41:33 +0800

    feat(course-page): Add toggle button with tooltip to configure extensions based on repository status.

--- app/components/course-page/step-list.ts
+++ app/components/course-page/step-list.ts
@@ -11,6 +11,7 @@ interface Signature {
   Args: {
     activeRepository: RepositoryModel;
     course: CourseModel;
+    configureExtensionsModalIsOpen: boolean;
     onConfigureExtensionsButtonClick: () => void;
     stepList: StepList;
   };
@@ -33,6 +34,14 @@ export default class StepListComponent extends Component<Signature> {
 
   get otherStepGroups() {
     return this.args.stepList.stepGroups.filter((group) => group.type !== 'BaseStagesStepGroup');
+  }
+
+  get toggleButtonTooltipCopy() {
+    if (this.args.activeRepository.get('isNew')) {
+      return "Complete repository setup to configure extensions"
+    } else {
+      return "Click here to configure extensions"
+    }
   }
 }
 
