57d9d80d085fcff6514e154a76137feda8d875dd
libmartinito <lib.martinito@pm.me>
2024-04-08 23:41:25 +0800

    feat(components): Add toggle size and tooltip functionality.

--- app/components/toggle.ts
+++ app/components/toggle.ts
@@ -6,12 +6,27 @@ interface Signature {
   Args: {
     isDisabled?: boolean;
     isOn: boolean;
+    isVisible?: boolean;
+    size?: 'small' | 'regular';
+    tooltipCopy?: string;
   };
 }
 
 export default class ToggleComponent extends Component<Signature> {
   get isOff(): boolean {
     return !this.args.isOn;
+  }
+
+  get isVisible(): boolean {
+    return this.args.isVisible ?? true;
+  }
+
+  get sizeIsRegular(): boolean {
+    return !this.args.size || this.args.size === 'regular';
+  }
+
+  get sizeIsSmall(): boolean {
+    return this.args.size === 'small';
   }
 }
 
