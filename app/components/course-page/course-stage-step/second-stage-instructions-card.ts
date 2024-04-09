54a8e9c7790d666793414f91df4b9a18cdce47bd
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-04-09 13:38:28 +0100

    refactor: Remove unnecessary services and method in SecondStageInstructionsCardComponent
    Removed unnecessary services and method in the SecondStageInstructionsCardComponent to simplify the code and improve readability.

--- app/components/course-page/course-stage-step/second-stage-instructions-card.ts
+++ app/components/course-page/course-stage-step/second-stage-instructions-card.ts
@@ -1,10 +1,6 @@
 import Component from '@glimmer/component';
-import CoursePageStateService from 'codecrafters-frontend/services/course-page-state';
-import RouterService from '@ember/routing/router-service';
-import Store from '@ember-data/store';
 import type CourseStageModel from 'codecrafters-frontend/models/course-stage';
 import type RepositoryModel from 'codecrafters-frontend/models/repository';
-import { inject as service } from '@ember/service';
 
 interface Signature {
   Element: HTMLDivElement;
@@ -15,19 +11,7 @@ interface Signature {
   };
 }
 
-export default class SecondStageInstructionsCardComponent extends Component<Signature> {
-  @service declare coursePageState: CoursePageStateService;
-  @service declare store: Store;
-  @service declare router: RouterService;
-
-  get submitChangesInstructionsMarkdown() {
-    return `\`\`\`
-git add .
-git commit -m "pass 2nd stage" # any msg
-git push origin master
-\`\`\``;
-  }
-}
+export default class SecondStageInstructionsCardComponent extends Component<Signature> {}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
