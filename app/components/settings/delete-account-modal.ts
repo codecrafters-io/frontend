ec6da69b41c554955429a5b339aafc8b7b2de9b9
libmartinito <lib.martinito@pm.me>
2024-04-10 16:34:23 +0800

    feat(settings): Add delete account modal component and template.

---
+++ app/components/settings/delete-account-modal.ts
@@ -0,0 +1,59 @@
+import AuthenticatorService from 'codecrafters-frontend/services/authenticator';
+import Component from '@glimmer/component';
+import rippleSpinnerImage from '/assets/images/icons/ripple-spinner.svg';
+import RouterService from '@ember/routing/router-service';
+import * as Sentry from '@sentry/ember';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
+interface Signature {
+  Element: HTMLDivElement;
+
+  Args: {
+    onClose?: () => void;
+  };
+}
+
+export default class DeleteAccountModalComponent extends Component<Signature> {
+  @service declare authenticator: AuthenticatorService;
+  @service declare router: RouterService;
+
+  @tracked accountDeletionErrorMessage: string | null = null;
+  @tracked isDeletingAccount = false;
+
+  rippleSpinnerImage = rippleSpinnerImage;
+
+  get currentUser() {
+    return this.authenticator.currentUser;
+  }
+
+  @action
+  async deleteAccount() {
+    if (!this.currentUser) {
+      return;
+    }
+
+    try {
+      this.isDeletingAccount = true;
+
+      await this.currentUser.destroyRecord();
+
+      this.accountDeletionErrorMessage = null;
+      this.isDeletingAccount = false;
+      this.router.transitionTo('account-deleted');
+    } catch (error) {
+      this.isDeletingAccount = false;
+
+      this.accountDeletionErrorMessage = 'Failed to create repository, please try again? Contact us at hello@codecrafters.io if this error persists.';
+
+      Sentry.captureException(error);
+    }
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'Settings::DeleteAccountModal': typeof DeleteAccountModalComponent;
+  }
+}
