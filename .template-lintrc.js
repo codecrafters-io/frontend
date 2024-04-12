e34c85291954c838bb458371f6530646abbaff1d
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-04-12 01:21:54 +0100

    refactor: Update template-lint rules and plugins

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended',
+  plugins: ['ember-template-lint-plugin-prettier'],
+  extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
+
+  rules: {
+    'require-presentational-children': 'off',
+    'no-invalid-link-text': 'off',
+  },
 };
