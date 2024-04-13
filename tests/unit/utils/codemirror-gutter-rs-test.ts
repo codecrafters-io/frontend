fa397f82232d48d3b1cbdedcbac3e1ae7666b128
Vasyl Marchuk <public@vasylmarchuk.com>
2024-04-13 10:09:30 +0300

    Add a Right-Side Gutter extension for CodeMirror

---
+++ tests/unit/utils/codemirror-gutter-rs-test.ts
@@ -0,0 +1,10 @@
+import { gutter } from 'codecrafters-frontend/utils/codemirror-gutter-rs';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | codemirror-gutter-rs', function () {
+  // TODO: Replace this with your real tests.
+  test('it works', function (assert) {
+    const result = gutter({});
+    assert.ok(result);
+  });
+});
