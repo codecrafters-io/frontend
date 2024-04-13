f17bdda70a92954db5f0a29b36e8481e0081de35
Vasyl Marchuk <public@vasylmarchuk.com>
2024-04-13 11:53:14 +0300

    WIP Show line comment counts

--- app/components/code-mirror.js
+++ app/components/code-mirror.js
@@ -5,6 +5,62 @@ import { basicSetup } from 'codemirror';
 import { EditorView } from '@codemirror/view';
 import { javascript } from '@codemirror/lang-javascript';
 import { EditorState, Compartment } from '@codemirror/state';
+import {
+  gutter as gutterRS,
+  GutterMarker as GutterMarkerRS,
+  lineNumbers as lineNumbersRS,
+  highlightActiveLineGutter as highlightActiveLineGutterRS,
+} from 'codecrafters-frontend/utils/codemirror-gutter-rs';
+
+function getRandomInt(inclusiveMin, exclusiveMax) {
+  const minCeiled = Math.ceil(inclusiveMin);
+  const maxFloored = Math.floor(exclusiveMax);
+
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+function smartLineMarker(view, line) {
+  const lineNumber = view.state.doc.lineAt(line.from).number;
+
+  if (lineNumber % 3 !== 0) {
+    return new CommentButtonGutterMarker(lineNumber);
+  }
+
+  return new CommentsCountGutterMarker(lineNumber, getRandomInt(1, 11));
+}
+
+class CommentsCountGutterMarker extends GutterMarkerRS {
+  lineNumber = 0;
+  commentsCount = 0;
+
+  constructor(lineNumber, commentsCount) {
+    super();
+    this.lineNumber = lineNumber;
+    this.commentsCount = commentsCount;
+  }
+
+  toDOM() {
+    const numbersMap = ['0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟'];
+
+    return document.createTextNode(`${numbersMap[this.commentsCount] || this.commentsCount}`);
+  }
+}
+
+class CommentButtonGutterMarker extends GutterMarkerRS {
+  lineNumber = 0;
+
+  constructor(lineNumber) {
+    super();
+    this.lineNumber = lineNumber;
+  }
+
+  toDOM() {
+    const elem = document.createElement('comment-button');
+    elem.innerText = `💬`;
+
+    return elem;
+  }
+}
 
 export default class CodeMirrorComponent extends Component {
   editableCompartment = new Compartment();
@@ -17,6 +73,13 @@ export default class CodeMirrorComponent extends Component {
     const extensions = [
       basicSetup,
       javascript(),
+      gutterRS({
+        class: 'cm-custom-gutter-rs',
+        renderEmptyElements: true,
+        lineMarker: smartLineMarker,
+      }),
+      lineNumbersRS(),
+      highlightActiveLineGutterRS(),
       this.editableCompartment.of(EditorView.editable.of(!this.args.readOnly)),
       this.readOnlyCompartment.of(EditorState.readOnly.of(this.args.readOnly)),
       EditorView.updateListener.of((update) => {
