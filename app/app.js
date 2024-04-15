3c9d19e1c9ad2370d9d35cf4f249e5f705ac85fe
libmartinito <lib.martinito@pm.me>
2024-04-15 20:46:23 +0800

    build: Remove unused import of Spotlight from app.js.

--- app/app.js
+++ app/app.js
@@ -3,7 +3,6 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'codecrafters-frontend/config/environment';
 import * as Sentry from '@sentry/ember';
-import * as Spotlight from '@spotlightjs/spotlight';
 
 const sentryDSN = 'https://478cca7283ca40209deae5160b54ee4f@o294739.ingest.sentry.io/5922961';
 
