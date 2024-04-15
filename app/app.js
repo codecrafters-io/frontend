6dfe9ba4e13464729afad2d0501ea9af8451bf76
libmartinito <lib.martinito@pm.me>
2024-04-15 17:19:57 +0800

    build: Fix import statement for Spotlight in app.js.

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'codecrafters-frontend/config/environment';
 import * as Sentry from '@sentry/ember';
-import * as Spotlight from '@spotlightjs/spotlight'
+import * as Spotlight from '@spotlightjs/spotlight';
 
 const sentryDSN = 'https://478cca7283ca40209deae5160b54ee4f@o294739.ingest.sentry.io/5922961';
 
