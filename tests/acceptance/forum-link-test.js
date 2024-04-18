4886761467e6d00288fcea280438945548021d88
libmartinito <lib.martinito@pm.me>
2024-04-18 19:16:25 +0800

    build: Fix formatting in forum-link-test.js.

--- tests/acceptance/forum-link-test.js
+++ tests/acceptance/forum-link-test.js
@@ -6,17 +6,17 @@ import { setupApplicationTest } from 'codecrafters-frontend/tests/helpers';
 import { setupWindowMock } from 'ember-window-mock/test-support';
 import { signIn } from 'codecrafters-frontend/tests/support/authentication-helpers';
 
-module('Acceptance | forum-link-test', function(hooks) {
+module('Acceptance | forum-link-test', function (hooks) {
   setupApplicationTest(hooks);
   setupWindowMock(hooks);
 
-  test('forum link in account dropdown redirects to forum', async function(assert) {
+  test('forum link in account dropdown redirects to forum', async function (assert) {
     testScenario(this.server);
     signIn(this.owner, this.server);
 
     window.open = (urlToOpen) => {
       assert.strictEqual(urlToOpen, 'https://forum.codecrafters.io/', 'should redirect to forum');
-    }
+    };
 
     await catalogPage.visit();
     await catalogPage.accountDropdown.toggle();
