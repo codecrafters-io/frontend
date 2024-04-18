e2a7fc88c479d589a5e23aba6c733fd4585ac78b
libmartinito <lib.martinito@pm.me>
2024-04-18 19:03:09 +0800

    feat: Add acceptance test for forum link redirection in account dropdown.

---
+++ tests/acceptance/forum-link-test.js
@@ -0,0 +1,25 @@
+import catalogPage from 'codecrafters-frontend/tests/pages/catalog-page';
+import testScenario from 'codecrafters-frontend/mirage/scenarios/test';
+import window from 'ember-window-mock';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'codecrafters-frontend/tests/helpers';
+import { setupWindowMock } from 'ember-window-mock/test-support';
+import { signIn } from 'codecrafters-frontend/tests/support/authentication-helpers';
+
+module('Acceptance | forum-link-test', function(hooks) {
+  setupApplicationTest(hooks);
+  setupWindowMock(hooks);
+
+  test('forum link in account dropdown redirects to forum', async function(assert) {
+    testScenario(this.server);
+    signIn(this.owner, this.server);
+
+    window.open = (urlToOpen) => {
+      assert.strictEqual(urlToOpen, 'https://forum.codecrafters.io/', 'should redirect to forum');
+    }
+
+    await catalogPage.visit();
+    await catalogPage.accountDropdown.toggle();
+    await catalogPage.accountDropdown.clickOnLink('Forum');
+  });
+});
