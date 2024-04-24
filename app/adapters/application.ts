60626dc1f635364967682dfb551af52c11f53d7a
libmartinito <lib.martinito@pm.me>
2024-04-24 22:45:15 +0800

    feat: Add posthog session id to request headers

--- app/adapters/application.ts
+++ app/adapters/application.ts
@@ -1,5 +1,6 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import { inject as service } from '@ember/service';
+import * as Sentry from '@sentry/ember';
 import config from 'codecrafters-frontend/config/environment';
 import type SessionTokenStorageService from 'codecrafters-frontend/services/session-token-storage';
 import type VersionTrackerService from 'codecrafters-frontend/services/version-tracker';
@@ -18,6 +19,14 @@ export default class ApplicationAdapter extends JSONAPIAdapter {
     }
 
     headers['x-codecrafters-client-version'] = this.versionTracker.currentVersion;
+
+    try {
+      // @ts-ignore
+      // posthog is initialized in a script tag in index.html
+      headers['x-posthog-session-id'] = window.posthog.get_session_id();
+    } catch (error) {
+      Sentry.captureException(error);
+    }
 
     return headers;
   }
