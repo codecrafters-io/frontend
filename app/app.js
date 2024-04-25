aeb9f5d65224163bfb2024c24d4e61aa746758d5
libmartinito <lib.martinito@pm.me>
2024-04-26 00:37:16 +0800

    refactor: Try initializing posthog instance in index.html instead of app.js

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'codecrafters-frontend/config/environment';
-import posthog from 'posthog-js';
 import * as Sentry from '@sentry/ember';
 
 const sentryDSN = 'https://478cca7283ca40209deae5160b54ee4f@o294739.ingest.sentry.io/5922961';
@@ -15,10 +14,6 @@ if (config.environment === 'development' || config.environment === 'production')
     release: config.x.version,
     tracesSampleRate: 0.01,
   });
-}
-
-if (config.environment === 'production') {
-  posthog.init('phc_jCl1mm3XbnvyIUr4h54oORqWEqj37gxhZIOebREBwxb', { api_host: 'https://app.posthog.com' });
 }
 
 export default class App extends Application {
