f22f42a82ac5aca66d8afa69e02c01ef1bdf4021
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-04-27 00:57:46 +0100

    fix highlighting

--- app/components/syntax-highlighted-code.ts
+++ app/components/syntax-highlighted-code.ts
@@ -1,9 +1,9 @@
+import * as shiki from 'shiki';
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
-import config from 'codecrafters-frontend/config/environment';
 import getOrCreateCachedHighlighterPromise from 'codecrafters-frontend/utils/highlighter-cache';
-import { htmlSafe } from '@ember/template';
 import { tracked } from '@glimmer/tracking';
-import type { SafeString } from '@ember/template/-private/handlebars';
+import { task } from 'ember-concurrency';
 
 export type Signature = {
   Element: HTMLDivElement;
@@ -18,11 +18,48 @@ export type Signature = {
 };
 
 export default class SyntaxHighlightedCodeComponent extends Component<Signature> {
-  @tracked asyncHighlightedHTML?: SafeString;
+  @tracked asyncHighlightedCode: string | null = null;
+  @tracked asyncHighlightedHTML: string | null = null;
 
   constructor(owner: unknown, args: Signature['Args']) {
     super(owner, args);
 
+    this.highlightCode.perform();
+
+    // if (config.environment === 'test' && error.message.match(/WebAssembly.instantiate/)) {
+    //   console.log('ignoring WebAssembly error only present in tests');
+    // } else {
+    //   throw error;
+    // }
+  }
+
+  get highlightedHtml() {
+    if (this.asyncHighlightedCode === this.args.code) {
+      return this.asyncHighlightedHTML!;
+    } else {
+      return this.temporaryHTML;
+    }
+  }
+
+  get temporaryHTML() {
+    const linesHTML = this.trimmedCode
+      .split('\n')
+      .map((line) => `<div class="line">${line}&nbsp;</div>`) // &nbsp; is used to ensure that empty lines are still rendered
+      .join('');
+
+    return `<pre><code>${linesHTML}</code></pre>`;
+  }
+
+  get trimmedCode() {
+    return this.args.code.trim();
+  }
+
+  @action
+  handleDidUpdateCode() {
+    this.highlightCode.perform();
+  }
+
+  highlightCode = task({ keepLatest: true }, async (): Promise<void> => {
     const highlighterPromise = getOrCreateCachedHighlighterPromise(`${this.args.theme}-${this.args.language}`, {
       theme: this.args.theme,
       // @ts-expect-error - shiki types are not up to date
@@ -33,38 +70,13 @@ export default class SyntaxHighlightedCodeComponent extends Component<Signature>
       .split(',')
       .flatMap((lineOrBlock) => [{ line: parseInt(lineOrBlock), classes: ['highlighted'] }]);
 
-    highlighterPromise
-      .then((highlighter) => {
-        this.asyncHighlightedHTML = htmlSafe(highlighter.codeToHtml(this.trimmedCode, { lang: this.args.language, lineOptions: lineOptions }));
-
-        // Uncomment to test temporaryHTML rendering
-        // this.asyncHighlightedHTML = this.temporaryHTML;
-      })
-      .catch((error) => {
-        if (config.environment === 'test' && error.message.match(/WebAssembly.instantiate/)) {
-          console.log('ignoring WebAssembly error only present in tests');
-        } else {
-          throw error;
-        }
+    highlighterPromise.then((highlighter) => {
+      highlighter.loadLanguage(this.args.language as shiki.Lang).then(() => {
+        this.asyncHighlightedHTML = highlighter.codeToHtml(this.trimmedCode, { lang: this.args.language, lineOptions: lineOptions });
+        this.asyncHighlightedCode = this.args.code;
       });
-  }
-
-  get highlightedHtml() {
-    return this.asyncHighlightedHTML || this.temporaryHTML;
-  }
-
-  get temporaryHTML() {
-    const linesHTML = this.trimmedCode
-      .split('\n')
-      .map((line) => `<div class="line">${line}&nbsp;</div>`) // &nbsp; is used to ensure that empty lines are still rendered
-      .join('');
-
-    return htmlSafe(`<pre><code>${linesHTML}</code></pre>`);
-  }
-
-  get trimmedCode() {
-    return this.args.code.trim();
-  }
+    });
+  });
 }
 
 declare module '@glint/environment-ember-loose/registry' {
