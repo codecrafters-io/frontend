00545ba77ec97a9a0e1cbf8a1dd9bd71a0e2bf26
libmartinito <lib.martinito@pm.me>
2024-04-27 14:44:07 +0800

    refactor: Use config instead of location.href

--- app/instance-initializers/posthog.js
+++ app/instance-initializers/posthog.js
@@ -1,7 +1,8 @@
+import config from 'codecrafters/config/environment';
 import posthog from 'posthog-js';
 
 export function initialize() {
-  if (window.location.href.includes('codecrafters.io')) {
+  if (config.environment === 'production') {
     posthog.init('phc_jCl1mm3XbnvyIUr4h54oORqWEqj37gxhZIOebREBwxb', { api_host: 'https://app.posthog.com' });
   }
 }
