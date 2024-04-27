27a7808c202a1f6b5a8fa4e8dc050e9fef3844b8
libmartinito <lib.martinito@pm.me>
2024-04-28 00:42:53 +0800

    refactor: Convert individual payment method update request model to typescript

--- app/models/individual_payment_method_update_request.ts
+++ app/models/individual_payment_method_update_request.ts
@@ -2,5 +2,5 @@ import Model from '@ember-data/model';
 import { attr } from '@ember-data/model';
 
 export default class IndividualPaymentMethodUpdateRequestModel extends Model {
-  @attr('string') url;
+  @attr('string') declare url: string;
 }
