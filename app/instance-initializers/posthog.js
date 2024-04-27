6a37893415a311caf2f2747cd46598805476f657
libmartinito <lib.martinito@pm.me>
2024-04-27 15:06:46 +0800

    refactor: Use a different approach to conditionally loading posthog init

--- app/instance-initializers/posthog.js
+++ app/instance-initializers/posthog.js
@@ -1,8 +1,7 @@
-import config from 'codecrafters/config/environment';
 import posthog from 'posthog-js';
 
-export function initialize() {
-  if (config.environment === 'production') {
+export function initialize(applicationInstance) {
+  if (application.resolveRegistration('config:environment').environment === 'production') {
     posthog.init('phc_jCl1mm3XbnvyIUr4h54oORqWEqj37gxhZIOebREBwxb', { api_host: 'https://app.posthog.com' });
   }
 }
