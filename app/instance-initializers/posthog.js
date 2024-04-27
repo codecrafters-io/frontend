86da22a7157ea3debae60c008bc121dde78b0679
libmartinito <lib.martinito@pm.me>
2024-04-27 15:27:52 +0800

    refactor: Try importing config using the relative path

--- app/instance-initializers/posthog.js
+++ app/instance-initializers/posthog.js
@@ -1,8 +1,7 @@
+import config from '../config/environment';
 import posthog from 'posthog-js';
 
 export function initialize(applicationInstance) {
-  let config = applicationInstance.lookup('config:environment');
-
   if (config.environment === 'production') {
     posthog.init('phc_jCl1mm3XbnvyIUr4h54oORqWEqj37gxhZIOebREBwxb', { api_host: 'https://app.posthog.com' });
   }
