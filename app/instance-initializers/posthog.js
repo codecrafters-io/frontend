8f6900116f2240be0bca728a623a6eaf129289cf
libmartinito <lib.martinito@pm.me>
2024-04-27 11:35:24 +0800

    fix: Lint fix

--- app/instance-initializers/posthog.js
+++ app/instance-initializers/posthog.js
@@ -1,8 +1,8 @@
 import posthog from 'posthog-js';
 
-export function initialize(applicationInstance) {
+export function initialize() {
   if (window.location.href.includes('codecrafters.io')) {
-    posthog.init('phc_jCl1mm3XbnvyIUr4h54oORqWEqj37gxhZIOebREBwxb',{api_host:'https://app.posthog.com'})
+    posthog.init('phc_jCl1mm3XbnvyIUr4h54oORqWEqj37gxhZIOebREBwxb', { api_host: 'https://app.posthog.com' });
   }
 }
 
