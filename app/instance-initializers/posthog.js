fe9fc4a14a22cff3a3a153bc52fcefcefa5fc49d
libmartinito <lib.martinito@pm.me>
2024-04-27 11:31:10 +0800

    feat: Use the posthog-js package instead of using a script tag in index.html

---
+++ app/instance-initializers/posthog.js
@@ -0,0 +1,11 @@
+import posthog from 'posthog-js';
+
+export function initialize(applicationInstance) {
+  if (window.location.href.includes('codecrafters.io')) {
+    posthog.init('phc_jCl1mm3XbnvyIUr4h54oORqWEqj37gxhZIOebREBwxb',{api_host:'https://app.posthog.com'})
+  }
+}
+
+export default {
+  initialize,
+};
