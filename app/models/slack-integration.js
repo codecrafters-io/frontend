df3c668586b02891026a90e8195453940ab31ff8
libmartinito <lib.martinito@pm.me>
2024-04-28 00:44:51 +0800

    refactor: Convert slack integration model to typescript

--- app/models/slack-integration.js
+++
@@ -1,7 +0,0 @@
-import { attr, belongsTo } from '@ember-data/model';
-import Model from '@ember-data/model';
-
-export default class SlackIntegration extends Model {
-  @attr('string') slackChannelName;
-  @belongsTo('team', { async: false, inverse: 'slackIntegrations' }) team;
-}
