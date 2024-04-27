da5d7fef80820389c90136f7abf13ab77e38586d
libmartinito <lib.martinito@pm.me>
2024-04-27 15:46:56 +0800

    refactor: Try using config.default

--- app/instance-initializers/posthog.js
+++ app/instance-initializers/posthog.js
@@ -1,8 +1,8 @@
 import config from '../config/environment';
 import posthog from 'posthog-js';
 
-export function initialize() {
-  if (config.environment === 'production') {
+export function initialize(applicationInstance) {
+  if (config.default.environment === 'production') {
     posthog.init('phc_jCl1mm3XbnvyIUr4h54oORqWEqj37gxhZIOebREBwxb', { api_host: 'https://app.posthog.com' });
   }
 }
