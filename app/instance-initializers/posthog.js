3bd1fb1eebb2b7986a6e015c59c8452661644dbc
libmartinito <lib.martinito@pm.me>
2024-04-27 15:09:54 +0800

    fix: Lint fix

--- app/instance-initializers/posthog.js
+++ app/instance-initializers/posthog.js
@@ -1,7 +1,7 @@
 import posthog from 'posthog-js';
 
 export function initialize(applicationInstance) {
-  if (application.resolveRegistration('config:environment').environment === 'production') {
+  if (applicationInstance.resolveRegistration('config:environment').environment === 'production') {
     posthog.init('phc_jCl1mm3XbnvyIUr4h54oORqWEqj37gxhZIOebREBwxb', { api_host: 'https://app.posthog.com' });
   }
 }
