b104b3b174f4f6f3c81a0e3c6da6fc54383f4fd5
libmartinito <lib.martinito@pm.me>
2024-04-28 23:35:37 +0800

    feat: Update date-from-now helper to show update in real time

--- app/helpers/date-from-now.ts
+++ app/helpers/date-from-now.ts
@@ -1,21 +1,41 @@
-import { helper } from '@ember/component/helper';
 import { formatDistanceStrictWithOptions } from 'date-fns/fp';
+import Helper from '@ember/component/helper';
 
-const dateFromNow = helper(function dateFromNow(
-  [date]: [(Date | null | undefined)?],
-  { currentDate = new Date() }: { currentDate?: Date | undefined } = {},
-): string {
-  if (date === undefined || date === null) {
-    return '';
-  }
-
-  return formatDistanceStrictWithOptions({ addSuffix: true }, currentDate, date);
-});
-
-declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry {
-    'date-from-now': typeof dateFromNow;
+interface DateFromNowSignature {
+  Args: {
+    Positional: Array<Date | null | undefined>;
   }
 }
 
-export default dateFromNow;
+export default class DateFromNow extends Helper<DateFromNowSignature> {
+  timer: number | null = null;
+
+  clearTimer() {
+    if (this.timer !== null) {
+      window.clearTimeout(this.timer);
+      this.timer = null;
+    }
+  }
+
+  compute(positional: [Date]) {
+    let [date] = positional;
+    this.scheduleRecompute();
+    return formatDistanceStrictWithOptions({ addSuffix: true }, new Date(), date);
+  }
+
+  scheduleRecompute() {
+    this.clearTimer();
+    this.timer = window.setTimeout(() => this.recompute(), 1000);
+  }
+
+  willDestroy() {
+    super.willDestroy();
+    this.clearTimer();
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'date-from-now': typeof DateFromNow;
+  }
+}
