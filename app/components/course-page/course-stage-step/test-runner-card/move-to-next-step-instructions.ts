bc9c5762d51bd05dfbcd2e4072d4d634579231c6
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-04-30 18:06:02 +0100

    update git commands

--- app/components/course-page/course-stage-step/test-runner-card/move-to-next-step-instructions.ts
+++ app/components/course-page/course-stage-step/test-runner-card/move-to-next-step-instructions.ts
@@ -1,4 +1,7 @@
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import type { CopyableTerminalCommandVariant } from 'codecrafters-frontend/components/copyable-terminal-command-with-variants';
 import type CourseStageStep from 'codecrafters-frontend/utils/course-page-step-list/course-stage-step';
 
 interface Signature {
@@ -9,7 +12,46 @@ interface Signature {
   };
 }
 
-export default class MoveToNextStepInstructionsComponent extends Component<Signature> {}
+export default class MoveToNextStepInstructionsComponent extends Component<Signature> {
+  @tracked selectedCommandVariant: CopyableTerminalCommandVariant;
+
+  constructor(owner: unknown, args: Signature['Args']) {
+    super(owner, args);
+
+    this.selectedCommandVariant = this.commandVariants[0]!;
+  }
+
+  get commandVariants(): CopyableTerminalCommandVariant[] {
+    const cliVariant = {
+      label: 'codecrafters cli',
+      commands: ['codecrafters submit # Visit https://codecrafters.io/cli to install'],
+    };
+
+    const gitVariant = {
+      label: 'git',
+      commands: ['git add .', 'git commit --allow-empty -m "pass stage" # any message', 'git push origin master'],
+    };
+
+    if (this.recommendedClientType === 'cli') {
+      return [cliVariant, gitVariant];
+    } else {
+      return [gitVariant];
+    }
+  }
+
+  get recommendedClientType() {
+    if (this.args.currentStep.courseStage.isFirst) {
+      return 'git';
+    } else {
+      return 'cli';
+    }
+  }
+
+  @action
+  handleVariantSelect(variant: CopyableTerminalCommandVariant) {
+    this.selectedCommandVariant = variant;
+  }
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
