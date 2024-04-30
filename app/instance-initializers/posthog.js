3cbfe1eeb9cc26e245977382850d0fbced92d851
libmartinito <lib.martinito@pm.me>
2024-04-30 20:36:00 +0800

    refactor: Use && instead of nesting if statements

--- app/instance-initializers/posthog.js
+++ app/instance-initializers/posthog.js
@@ -2,10 +2,8 @@ import config from 'codecrafters-frontend/config/environment';
 import posthog from 'posthog-js';
 
 export function initialize() {
-  if (typeof FastBoot === 'undefined') {
-    if (config.environment === 'production') {
-      posthog.init('phc_jCl1mm3XbnvyIUr4h54oORqWEqj37gxhZIOebREBwxb', { api_host: 'https://app.posthog.com' });
-    }
+  if (typeof FastBoot === 'undefined' && config.environment === 'production') {
+    posthog.init('phc_jCl1mm3XbnvyIUr4h54oORqWEqj37gxhZIOebREBwxb', { api_host: 'https://app.posthog.com' });
   }
 }
 
