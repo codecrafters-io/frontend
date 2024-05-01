4c148c1bfbef5f05130a5ef0fe9a903365603041
libmartinito <lib.martinito@pm.me>
2024-05-01 20:42:20 +0800

    refactor: Remove unused promotion code property in individual checkout session model

--- app/models/individual-checkout-session.ts
+++ app/models/individual-checkout-session.ts
@@ -7,7 +7,6 @@ export default class IndividualCheckoutSessionModel extends Model {
   @attr('boolean') declare earlyBirdDiscountEnabled: boolean;
   @attr('boolean') declare extraInvoiceDetailsRequested: boolean;
   @attr('string') declare pricingFrequency: string;
-  @attr('string') declare promotionCode: string;
   @attr('boolean') declare referralDiscountEnabled: boolean;
   @attr('string') declare successUrl: string;
   @attr('string') declare cancelUrl: string;
