034fd3df6fc0fd8860dc7cd3848fb603c4b18b35
libmartinito <lib.martinito@pm.me>
2024-05-01 20:29:34 +0800

    refactor: Update status type in course leaderboard entry to be more specific

--- app/models/course-leaderboard-entry.ts
+++ app/models/course-leaderboard-entry.ts
@@ -7,7 +7,7 @@ import type UserModel from 'codecrafters-frontend/models/user';
 
 export default class CourseLeaderboardEntry extends Model {
   @attr('date') declare lastAttemptAt: Date;
-  @attr('string') declare status: string;
+  @attr('string') declare status: 'evaluating' | 'completed' | 'idle';
 
   @belongsTo('course-stage', { async: false, inverse: null }) declare currentCourseStage: CourseStageModel;
   @belongsTo('language', { async: false, inverse: null }) declare language: LanguageModel;
