14901ea801937ec33e4bf9d7bf904e5f0c49a2ad
libmartinito <lib.martinito@pm.me>
2024-05-01 20:40:14 +0800

    refactor: Update last sync status type in github repository sync configuration to be more specific

--- app/models/github-repository-sync-configuration.ts
+++ app/models/github-repository-sync-configuration.ts
@@ -5,7 +5,7 @@ import type RepositoryModel from 'codecrafters-frontend/models/repository';
 export default class GithubRepositorySyncConfiguration extends Model {
   @attr('string') declare githubRepositoryId: string;
   @attr('string') declare githubRepositoryName: string;
-  @attr('string') declare lastSyncStatus: string;
+  @attr('string') declare lastSyncStatus: 'success' | 'failure';
   @attr('date') declare lastSyncedAt: Date;
 
   @belongsTo('repository', { async: false, inverse: 'githubRepositorySyncConfigurations' }) declare repository: RepositoryModel;
