1d99687cca8966a3e9375c66dec8d1a2801b8450
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-05-21 15:52:58 +0100

    Add time service initialization in instance initializer

--- app/services/time.ts
+++ app/services/time.ts
@@ -4,22 +4,16 @@ import Service, { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class TimeService extends Service {
-  @tracked currentTime: Date;
-  @tracked intervalId: number;
+  @tracked declare currentTime: Date;
+  @tracked declare intervalId: number;
 
   @service declare date: DateService;
   @service declare localStorage: LocalStorageService;
 
-  constructor() {
-    super();
-    // using another service from a service breaks tests?
-    console.log('will this be logged', this.localStorage);
+  setupTimer() {
     this.currentTime = new Date();
-    this.intervalId = this.updateTime();
-  }
 
-  updateTime() {
-    return setInterval(() => {
+    this.intervalId = setInterval(() => {
       this.currentTime = new Date(this.date.now());
     }, 1000);
   }
