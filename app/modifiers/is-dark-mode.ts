be0471fe1941522e8bc99b9d2478e6a320a9acd2
Vasyl Marchuk <public@vasylmarchuk.com>
2024-05-26 17:14:00 +0300

    Refactored and improved `is-dark-mode` modifier

--- app/modifiers/is-dark-mode.ts
+++ app/modifiers/is-dark-mode.ts
@@ -1,56 +1,139 @@
-// Invokes a callback when the dark mode setting changes (and on initial load).
-// Usage: <div {{is-dark-mode this.handleDarkModeUpdated}}></div>
 import { registerDestructor } from '@ember/destroyable';
-import { action } from '@ember/object';
-import { task } from 'ember-concurrency';
+import { service } from '@ember/service';
+import { task, timeout } from 'ember-concurrency';
 import Modifier from 'ember-modifier';
+import type DarkModeService from 'codecrafters-frontend/services/dark-mode';
 
+/**
+ * Enable or disable the DOM Mutation Observer
+ */
+const DOM_MUTATION_OBSERVER_ENABLED: boolean = false; // TODO: Figure out an efficient way to enable the observer while avoiding too many calls
+
+/**
+ * Enable or disable subscribing to `DarkModeService.isEnabled` changes
+ */
+const DARK_MODE_UPDATE_LISTENER_ENABLED: boolean = true;
+
+/**
+ * Method to use for delaying `triggerCallback` task execution:
+ * - `restartable` cancels all queued calls before they have a chance to execute, executes only the latest call: causes much less unnecessary task executions
+ * - `keepLatest` always executes first & last queued calls, cancelling the ones in-between: might be more reliable but always triggers rendering twice
+ */
+const CALLBACK_TASK_TIMEOUT_METHOD: 'keepLatest' | 'restartable' = 'restartable';
+
+/**
+ * Timeout used for `restartable` method
+ */
+const CALLBACK_TASK_TIMEOUT_PRE: number = 10;
+
+/**
+ * Timeout used for `keepLatest` method when `DOM_MUTATION_OBSERVER_ENABLED` is true
+ */
+const CALLBACK_TASK_TIMEOUT_POST: number = 100;
+
+/**
+ * Name of element's attribute for storing the currently reported state of Dark Mode,
+ * to avoid invoking callbacks again with same value and to prevent double-renders
+ */
+const CACHED_STATE_ATTRIBUTE_NAME = 'data-reported-dark-mode';
+
+/**
+ * Modifier Args Signature
+ */
 type Signature = {
   Args: {
     Positional: [callback: (isDarkMode: boolean) => void];
   };
 };
 
-function cleanup(instance: IsDarkModeModifier) {
-  if (instance.observer) {
-    instance.observer.disconnect();
-    instance.observer = undefined;
-  }
-}
-
+/**
+ * Invokes a callback on initial load and when Dark Mode environment changes,
+ * passing it `isDarkMode: boolean` as argument.
+ * Calculated based on element's DOM parents with `.dark` class.
+ * Relies on DarkModeService and/or DOM Mutation Obsersers for monitoring changes.
+ * Usage:
+ * ```
+ * <div {{is-dark-mode this.handleDarkModeUpdated}} />
+ * ```
+ */
 export default class IsDarkModeModifier extends Modifier<Signature> {
-  callback?: (isDarkMode: boolean) => void;
-  element?: HTMLElement;
-  observer?: MutationObserver;
+  @service declare darkMode: DarkModeService;
 
-  @action
-  eventHandler(_mutationsList: MutationRecord[], _observer: MutationObserver) {
-    this.eventHandlerTask.perform();
-  }
+  /**
+   * Callback to invoke on initial load and when Dark Mode environment changes
+   * @private
+   */
+  #callback?: (isDarkMode: boolean) => void;
 
-  eventHandlerTask = task({ keepLatest: true }, async () => {
-    const isDarkMode = !!this.element!.closest('.dark');
+  /**
+   * Elemnent which the modifier was used on
+   * @private
+   */
+  #element?: HTMLElement;
 
-    if (this.callback) {
-      this.callback(isDarkMode);
+  /**
+   * Task for actually scanning the DOM and invoking the callback
+   */
+  triggerCallback = task({ [CALLBACK_TASK_TIMEOUT_METHOD]: true }, async () => {
+    // For "restartable" to have immediate effect for sequential calls
+    if (CALLBACK_TASK_TIMEOUT_METHOD === 'restartable') {
+      await timeout(CALLBACK_TASK_TIMEOUT_PRE);
     }
 
-    // When we support updates, we should ensures we aren't performing this check too often
-    // await timeout(1000);
+    // Skip immediately if no callback is passed, to avoid an unnecessary DOM scan
+    if (!this.#callback) {
+      return;
+    }
+
+    // Scan the DOM for element parents with `.dark` class
+    const isDarkMode = !!this.#element!.closest('.dark');
+
+    // If callback was called earlier with same value - don't call it again to avoid double-renders
+    if (this.#element?.getAttribute(CACHED_STATE_ATTRIBUTE_NAME) === `${isDarkMode}`) {
+      return;
+    }
+
+    // Remember which value is passed to the callback
+    this.#element?.setAttribute(CACHED_STATE_ATTRIBUTE_NAME, `${isDarkMode}`);
+    this.#callback(isDarkMode);
+
+    // Ensure we don't run this task too often when DOM Observer is enabled
+    if (CALLBACK_TASK_TIMEOUT_METHOD === 'keepLatest' && DOM_MUTATION_OBSERVER_ENABLED) {
+      await timeout(CALLBACK_TASK_TIMEOUT_POST);
+    }
   });
 
+  /**
+   * Modifier constructor, called when modifier is rendered.
+   * @param element element to which the modifier is attached
+   * @param callback callback to invoke on initial load and when Dark Mode environment changes
+   */
   modify(element: HTMLElement, [callback]: [(isDarkMode: boolean) => void]) {
-    this.callback = callback;
-    this.element = element;
+    this.#element = element;
+    this.#callback = callback;
 
-    // Disabling this until we're able to figure out an efficient way to do it - maybe use a DarkModeService with a callback?
-    // We don't have any cases yet where dark mode is toggled after the initial load, so this is fine for now.
-    // this.observer = new MutationObserver(this.eventHandler);
-    // this.observer.observe(document.body, { attributes: true, childList: true, subtree: true, attributeFilter: ['class'] });
+    if (DOM_MUTATION_OBSERVER_ENABLED) {
+      const domMutationObserver = new MutationObserver((_mutationsList: MutationRecord[], _observer: MutationObserver) => {
+        this.triggerCallback.perform();
+      });
+      domMutationObserver.observe(document.body, { attributes: true, childList: true, subtree: true, attributeFilter: ['class'] });
+      registerDestructor(this, () => {
+        domMutationObserver.disconnect();
+      });
+    }
 
-    this.eventHandlerTask.perform(); // Perform once on initial load
+    if (DARK_MODE_UPDATE_LISTENER_ENABLED) {
+      const darkModeUpdateListener = () => {
+        this.triggerCallback.perform();
+      };
 
-    registerDestructor(this, cleanup);
+      this.darkMode.registerUpdateListener(darkModeUpdateListener);
+      registerDestructor(this, () => {
+        this.darkMode.unregisterUpdateListener(darkModeUpdateListener);
+      });
+    }
+
+    this.triggerCallback.perform(); // Perform once on initial load
   }
 }
 
