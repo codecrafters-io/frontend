d13875b46e181185dcefa0f52962d71cab912c98
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-05-27 16:09:49 +0100

    Update PostHog initialization in app/instance-initializers/posthog.js
    The commit message could be: "Disable autocapture in PostHog initialization"

--- app/instance-initializers/posthog.js
+++ app/instance-initializers/posthog.js
@@ -5,6 +5,7 @@ export function initialize() {
   if (typeof FastBoot === 'undefined' && config.environment === 'production') {
     posthog.init('phc_jCl1mm3XbnvyIUr4h54oORqWEqj37gxhZIOebREBwxb', {
       api_host: 'https://app.posthog.com',
+      autocapture: false, // We have our own events
       maskAllInputs: false,
       maskInputOptions: {
         password: true,
