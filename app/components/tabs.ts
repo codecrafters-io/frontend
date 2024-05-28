5a6445a66414bf55ce779bbdbb3b41f38245f01b
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-05-28 21:04:40 +0100

    Refactor evaluations section and tabs in code example evaluator page

--- app/components/tabs.ts
+++ app/components/tabs.ts
@@ -1,16 +1,23 @@
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+
+export type Tab = {
+  slug: string;
+  title: string;
+  icon?: string;
+};
 
 export type Signature = {
   Element: HTMLDivElement;
 
   Args: {
-    tabs: {
-      slug: string;
-      title: string;
-      icon?: string;
-    }[];
+    tabs: Tab[];
     size?: 'regular' | 'small';
+
+    // These can be set if the component needs to be "controlled"
+    activeTabSlug?: string;
+    onTabChange?: (tab: Tab) => void;
   };
 
   Blocks: {
@@ -19,12 +26,19 @@ export type Signature = {
 };
 
 export default class TabsComponent extends Component<Signature> {
-  @tracked activeTabSlug;
+  @tracked uncontrolledActiveTabSlug = this.args.tabs[0]!.slug;
 
-  constructor(owner: unknown, args: Signature['Args']) {
-    super(owner, args);
+  get activeTabSlug() {
+    return this.args.activeTabSlug || this.uncontrolledActiveTabSlug;
+  }
 
-    this.activeTabSlug = args.tabs[0]!.slug;
+  @action
+  handleTabChange(tab: Tab) {
+    this.uncontrolledActiveTabSlug = tab.slug;
+
+    if (this.args.onTabChange) {
+      this.args.onTabChange(tab);
+    }
   }
 }
 
