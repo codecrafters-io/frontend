d940432e876047411bd39e7dab7a109ad58ec922
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-05-28 16:49:03 +0100

    Refactor community solutions list and code examples templates

--- app/components/course-page/course-stage-step/community-solutions-list.ts
+++ app/components/course-page/course-stage-step/community-solutions-list.ts
@@ -5,13 +5,15 @@ import { action } from '@ember/object';
 import { service } from '@ember/service';
 import type CommunityCourseStageSolutionModel from 'codecrafters-frontend/models/community-course-stage-solution';
 import type AuthenticatorService from 'codecrafters-frontend/services/authenticator';
+import type RepositoryModel from 'codecrafters-frontend/models/repository';
+import type CoursePageStateService from 'codecrafters-frontend/services/course-page-state';
 
 type Signature = {
   Element: HTMLDivElement;
 
   Args: {
     solutions: CommunityCourseStageSolutionModel[];
-    repository: string;
+    repository: RepositoryModel;
   };
 };
 
@@ -19,6 +21,7 @@ export default class CommunitySolutionsListComponent extends Component<Signature
   rippleSpinnerImage = rippleSpinnerImage;
 
   @service declare authenticator: AuthenticatorService;
+  @service declare coursePageState: CoursePageStateService;
 
   @tracked configureGithubIntegrationModalIsOpen = false;
 
@@ -27,5 +30,11 @@ export default class CommunitySolutionsListComponent extends Component<Signature
     if (this.authenticator.isAuthenticated) {
       solution.createView({ position_in_list: solutionIndex + 1 });
     }
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'CoursePage::CourseStageStep::CommunitySolutionsList': typeof CommunitySolutionsListComponent;
   }
 }
