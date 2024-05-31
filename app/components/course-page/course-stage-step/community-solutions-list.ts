bce8c014325657e448da37b1fed2738c40e93b45
libmartinito <lib.martinito@pm.me>
2024-05-31 17:09:38 +0800

    feat: Add stage incomplete modal when viewing code examples

--- app/components/course-page/course-stage-step/community-solutions-list.ts
+++ app/components/course-page/course-stage-step/community-solutions-list.ts
@@ -1,10 +1,11 @@
-import rippleSpinnerImage from '/assets/images/icons/ripple-spinner.svg';
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
+import rippleSpinnerImage from '/assets/images/icons/ripple-spinner.svg';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
-import type CommunityCourseStageSolutionModel from 'codecrafters-frontend/models/community-course-stage-solution';
+import { tracked } from '@glimmer/tracking';
 import type AuthenticatorService from 'codecrafters-frontend/services/authenticator';
+import type CommunityCourseStageSolutionModel from 'codecrafters-frontend/models/community-course-stage-solution';
+import type CoursePageStateService from 'codecrafters-frontend/services/course-page-state';
 
 type Signature = {
   Element: HTMLDivElement;
@@ -19,13 +20,25 @@ export default class CommunitySolutionsListComponent extends Component<Signature
   rippleSpinnerImage = rippleSpinnerImage;
 
   @service declare authenticator: AuthenticatorService;
+  @service declare coursePageState: CoursePageStateService;
 
   @tracked configureGithubIntegrationModalIsOpen = false;
+  @tracked stageIncompleteModalWasDismissed = false;
 
   @action
   handleSolutionExpand(solution: CommunityCourseStageSolutionModel, solutionIndex: number) {
     if (this.authenticator.isAuthenticated) {
       solution.createView({ position_in_list: solutionIndex + 1 });
     }
+  }
+
+  @action
+  handleStageIncompleteModalDismissed() {
+    this.stageIncompleteModalWasDismissed = true;
+  }
+
+  get shouldShowStageIncompleteModal() {
+    console.log(this.coursePageState.currentStep.globalPosition);
+    return !this.stageIncompleteModalWasDismissed && this.coursePageState.currentStep.globalPosition > 4 && this.args.solutions.length > 0;
   }
 }
