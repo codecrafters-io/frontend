87b2c726112ef17bd1b315af8439a26cba1c2df0
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-06-01 10:57:42 +0100

    Update course order in view-courses test
    The order of the courses 'Build your own Redis' and 'Build your own Git' has been swapped in the view-courses test. This commit fixes the
    order to match the expected order.

--- tests/acceptance/view-courses-test.js
+++ tests/acceptance/view-courses-test.js
@@ -22,8 +22,8 @@ module('Acceptance | view-courses', function (hooks) {
 
     await percySnapshot('Catalog Page');
 
-    assert.strictEqual(catalogPage.courseCards[0].name, 'Build your own Git');
-    assert.strictEqual(catalogPage.courseCards[1].name, 'Build your own Redis');
+    assert.strictEqual(catalogPage.courseCards[0].name, 'Build your own Redis');
+    assert.strictEqual(catalogPage.courseCards[1].name, 'Build your own Git');
     assert.strictEqual(catalogPage.courseCards[2].name, 'Build your own grep');
     assert.strictEqual(catalogPage.courseCards[3].name, 'Build your own Docker');
     assert.strictEqual(catalogPage.courseCards[4].name, 'Build your own SQLite');
@@ -177,8 +177,8 @@ module('Acceptance | view-courses', function (hooks) {
     await catalogPage.visit();
     assert.strictEqual(catalogPage.courseCards.length, 5, 'expected 5 course cards to be present');
 
-    assert.strictEqual(catalogPage.courseCards[0].name, 'Build your own Git');
-    assert.strictEqual(catalogPage.courseCards[1].name, 'Build your own Redis');
+    assert.strictEqual(catalogPage.courseCards[0].name, 'Build your own Redis');
+    assert.strictEqual(catalogPage.courseCards[1].name, 'Build your own Git');
     assert.strictEqual(catalogPage.courseCards[2].name, 'Build your own grep');
     assert.strictEqual(catalogPage.courseCards[3].name, 'Build your own Docker');
     assert.strictEqual(catalogPage.courseCards[4].name, 'Build your own SQLite');
