da447d33372de32bdd970e23641e2b5e5f9827a8
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-06-03 23:41:36 +0100

    Refactor FAQ components and add TypeScript support
    The commit message could be: "Refactor FAQ components and add TypeScript support"

--- app/components/faq-item.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
-export default class FaqItemComponent extends Component {
-  @action
-  toggleItem() {
-    this.args.onToggle(this.args.faq);
-  }
-}
--- app/components/faq-list.ts
+++ app/components/faq-list.ts
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
-type Faq = {
+export type Faq = {
   query: string;
   answer: string;
 };
