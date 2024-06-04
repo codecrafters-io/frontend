dc3859027827155d2a8ba7ac972218e4fe25d4f8
Vasyl Marchuk <public@vasylmarchuk.com>
2024-06-04 12:59:43 +0300

    Add full Dark Mode support to the Demo route

--- app/routes/demo.ts
+++ app/routes/demo.ts
@@ -1,3 +1,8 @@
 import Route from '@ember/routing/route';
+import RouteInfoMetadata from 'codecrafters-frontend/utils/route-info-metadata';
 
-export default class DemoRoute extends Route {}
+export default class DemoRoute extends Route {
+  buildRouteInfoMetadata(): RouteInfoMetadata {
+    return new RouteInfoMetadata({ supportsDarkMode: true });
+  }
+}
