0c66bbc35194f44f92fc34e55fc83c3257b2d731
Rafey Ahmad <rafeyahmad@protonmail.com>
2024-07-12 18:14:59 +0530

    Fix tests + make separate component for Hero section

---
+++ app/components/affiliate-link-page/hero-section.ts
@@ -0,0 +1,12 @@
+import heroImage from '/assets/images/affiliate-program-features/hero-image.png';
+import Component from '@glimmer/component';
+
+export default class HeroSectionComponent extends Component {
+  heroImage = heroImage;
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'AffiliateLinkPage::HeroSection': typeof HeroSectionComponent;
+  }
+}
