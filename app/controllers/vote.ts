e3d33a500fc5327a035931a3aa5026338939463b
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-07-28 18:16:07 +0100

    convert vote controller

---
+++ app/controllers/vote.ts
@@ -0,0 +1,6 @@
+import Controller from '@ember/controller';
+import type { ModelType } from 'codecrafters-frontend/routes/vote';
+
+export default class VoteController extends Controller {
+  declare model: ModelType;
+}
