813dd916a6d077c2512cc560e0924264366e18ab
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-07-28 11:22:15 +0100

    Refactor BasicDetailsSectionComponent to FormSectionComponent in ConceptAdmin module

--- app/components/concept-admin/form-section.ts
+++ app/components/concept-admin/form-section.ts
@@ -14,10 +14,10 @@ interface Signature {
   };
 }
 
-export default class BasicDetailsSectionComponent extends Component<Signature> {}
+export default class FormSectionComponent extends Component<Signature> {}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
-    'ConceptAdmin::FormSection': typeof BasicDetailsSectionComponent;
+    'ConceptAdmin::FormSection': typeof FormSectionComponent;
   }
 }
