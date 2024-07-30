d86d585ada253ce03c35ddec226c54cc6e5aed01
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-07-30 15:18:02 +0100

    leaderboard updates

---
+++ app/components/leaderboard-entry.ts
@@ -0,0 +1,42 @@
+import Component from '@glimmer/component';
+import type UserModel from 'codecrafters-frontend/models/user';
+import { htmlSafe } from '@ember/template';
+
+interface Signature {
+  Element: HTMLAnchorElement;
+
+  Args: {
+    progressDenominator: number;
+    progressNumerator: number;
+    route: string;
+    routeModel: string;
+    status: string;
+    isForCurrentUser?: boolean;
+    isCollapsed?: boolean;
+    user: UserModel;
+  };
+
+  Blocks: {
+    afterUsername: [];
+  };
+}
+
+export default class LeaderboardEntryComponent extends Component<Signature> {
+  get isSkeleton(): boolean {
+    return !this.args.user;
+  }
+
+  get progressBarWidthStyle() {
+    return htmlSafe(`width: ${this.progressPercentage}%;`);
+  }
+
+  get progressPercentage(): number {
+    return 100 * (this.args.progressNumerator / this.args.progressDenominator);
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    LeaderboardEntry: typeof LeaderboardEntryComponent;
+  }
+}
