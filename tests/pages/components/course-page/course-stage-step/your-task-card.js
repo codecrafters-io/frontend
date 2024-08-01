e182aee96d36467bcbfe191e80aaaaf70073b636
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-08-01 14:43:14 +0100

    update styles for prism

--- tests/pages/components/course-page/course-stage-step/your-task-card.js
+++ tests/pages/components/course-page/course-stage-step/your-task-card.js
@@ -2,6 +2,8 @@ import { clickOnText, text } from 'ember-cli-page-object';
 
 export default {
   clickOnActionButton: clickOnText('[data-test-action-button]'),
+  clickOnCollapseButton: clickOnText('[data-test-collapse-button]'),
+  clickOnExpandButton: clickOnText('[data-test-expand-button]'),
   footerText: text('[data-test-footer-text]'),
   scope: '[data-test-your-task-card]',
 };
