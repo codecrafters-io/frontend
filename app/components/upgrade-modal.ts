e35090308ae505c8049c622a584718dcfc2d1484
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-08-01 18:06:37 +0100

    Add handleDidInsert action to UpgradeModalComponent

--- app/components/upgrade-modal.ts
+++ app/components/upgrade-modal.ts
@@ -1,5 +1,8 @@
 import Component from '@glimmer/component';
 import type { Signature as UpgradePromptSignature } from './course-page/course-stage-step/upgrade-prompt';
+import { action } from '@ember/object';
+import type AnalyticsEventTrackerService from 'codecrafters-frontend/services/analytics-event-tracker';
+import { service } from '@ember/service';
 
 interface Signature {
   Element: HTMLDivElement;
@@ -9,7 +12,16 @@ interface Signature {
   };
 }
 
-export default class UpgradeModalComponent extends Component<Signature> {}
+export default class UpgradeModalComponent extends Component<Signature> {
+  @service declare analyticsEventTracker: AnalyticsEventTrackerService;
+
+  @action
+  handleDidInsert() {
+    this.analyticsEventTracker.track('viewed_upgrade_prompt', {
+      feature_slug: this.args.featureSlugToHighlight,
+    });
+  }
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
