41a434994a53983b7e56c16d05470cce9ab06a2c
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-08-05 14:31:26 +0100

    feat(header): add VIP badge expiration tooltip

--- app/components/header/vip-badge.ts
+++ app/components/header/vip-badge.ts
@@ -1,10 +1,19 @@
+import { service } from '@ember/service';
 import Component from '@glimmer/component';
+import type UserModel from 'codecrafters-frontend/models/user';
+import type AuthenticatorService from 'codecrafters-frontend/services/authenticator';
 
 interface Signature {
   Element: HTMLDivElement;
 }
 
-export default class VipBadgeComponent extends Component<Signature> {}
+export default class VipBadgeComponent extends Component<Signature> {
+  @service declare authenticator: AuthenticatorService;
+
+  get currentUser() {
+    return this.authenticator.currentUser as UserModel;
+  }
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
