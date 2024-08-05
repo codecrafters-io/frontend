97d89d3ce36f2529a646b63eb1d7eb2028bba711
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-08-05 02:36:41 +0100

    Remove import of tracked from account-dropdown.ts
    This commit removes the import of tracked from the account-dropdown.ts file in the app/components/header directory.

--- app/components/header/account-dropdown.ts
+++ app/components/header/account-dropdown.ts
@@ -2,7 +2,6 @@ import Component from '@glimmer/component';
 import window from 'ember-window-mock';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 import type AuthenticatorService from 'codecrafters-frontend/services/authenticator';
 import type RouterService from '@ember/routing/router-service';
 import type Store from '@ember-data/store';
