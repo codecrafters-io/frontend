aadb07102d9ff65b73d54ac287e5a51f67072989
Vasyl Marchuk <public@vasylmarchuk.com>
2024-08-08 17:41:48 +0300

    Stub `global` in non-FastBoot context

--- app/app.js
+++ app/app.js
@@ -5,6 +5,11 @@ import config from 'codecrafters-frontend/config/environment';
 import * as Sentry from '@sentry/ember';
 import 'ember-basic-dropdown/styles';
 
+if (typeof window.global === 'undefined') {
+  window.global = window;
+  window.process = { env: {}, version: '' };
+}
+
 const sentryDSN = 'https://478cca7283ca40209deae5160b54ee4f@o294739.ingest.sentry.io/5922961';
 
 if (config.environment === 'development' || config.environment === 'production') {
