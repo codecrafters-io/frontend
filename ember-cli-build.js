9b23e73fd05eb7c07d6e6e448b00672ceb24ef68
Vasyl Marchuk <public@vasylmarchuk.com>
2024-08-08 17:51:47 +0300

    Add `canvas` to WebPack resolve fallbacks

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -112,6 +112,7 @@ module.exports = function (defaults) {
             tls: false, // require.resolve('tls-browserify'),
             vm: require.resolve('vm-browserify'),
             zlib: false, // require.resolve('browserify-zlib'),
+            canvas: false,
           },
         },
         plugins: [
