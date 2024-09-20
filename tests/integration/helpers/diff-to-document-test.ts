c416b07e877d5ef831eb92db1178b68389dd9474
Vasyl Marchuk <public@vasylmarchuk.com>
2024-09-20 12:54:08 +0300

    Better test for `diff-to-document` to also test deleted lines

--- tests/integration/helpers/diff-to-document-test.ts
+++ tests/integration/helpers/diff-to-document-test.ts
@@ -8,13 +8,13 @@ module('Integration | Helper | diff-to-document', function (hooks) {
 
   test('it converts a diff to a document object', async function (assert) {
     await render(hbs`
-      {{#let (diff-to-document ' 1234\n+2345\n 6789\n') as |doc|}}
+      {{#let (diff-to-document ' 1234\n+2345\n 6789\n-0123\n') as |doc|}}
         <pre data-test-current-document>{{doc.current}}</pre>
         <pre data-test-original-document>{{doc.original}}</pre>
       {{/let}}
     `);
 
     assert.dom('[data-test-current-document]').hasText(/^1234\n2345\n6789\n$/);
-    assert.dom('[data-test-original-document]').hasText(/^1234\n6789\n$/);
+    assert.dom('[data-test-original-document]').hasText(/^1234\n6789\n0123\n$/);
   });
 });
