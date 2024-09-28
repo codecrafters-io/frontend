1c2ffd95c6f12741aafae4d8a07ecd6d1f398531
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-09-28 04:29:48 +0100

    Add dark mode styles for expand sidebar button

--- app/components/course-page/expand-sidebar-button.ts
+++ app/components/course-page/expand-sidebar-button.ts
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 
 interface Signature {
-  Element: HTMLDivElement;
+  Element: HTMLButtonElement;
 }
 
 export default class ExpandSidebarButtonComponent extends Component<Signature> {}
