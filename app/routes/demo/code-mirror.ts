4af2754f600320cfe1dddcfbc6fce5167cb7120f
Vasyl Marchuk <public@vasylmarchuk.com>
2024-09-29 15:50:09 +0300

    De-duplicate query param definitions in `demo/code-mirror` route

--- app/routes/demo/code-mirror.ts
+++ app/routes/demo/code-mirror.ts
@@ -1,169 +1,59 @@
 import Route from '@ember/routing/route';
 
-export default class DemoCodeMirrorRoute extends Route {
-  queryParams = {
-    allowMultipleSelections: {
-      replace: true,
-    },
+const QUERY_PARAMS = [
+  'allowMultipleSelections',
+  'autocompletion',
+  'bracketMatching',
+  'closeBrackets',
+  'collapseUnchanged',
+  'crosshairCursor',
+  'document',
+  'drawSelection',
+  'dropCursor',
+  'editable',
+  'filename',
+  'foldGutter',
+  'highlightActiveLine',
+  'highlightSelectionMatches',
+  'highlightSpecialChars',
+  'highlightTrailingWhitespace',
+  'highlightWhitespace',
+  'history',
+  'indentOnInput',
+  'indentUnit',
+  'indentWithTab',
+  'language',
+  'lineNumbers',
+  'lineSeparator',
+  'lineWrapping',
+  'mergeControls',
+  'originalDocument',
+  'outline',
+  'placeholder',
+  'preserveHistory',
+  'readOnly',
+  'rectangularSelection',
+  'scrollPastEnd',
+  'selectedDocumentIndex',
+  'selectedIndentUnitIndex',
+  'selectedLineSeparatorIndex',
+  'selectedTabSizeIndex',
+  'selectedThemeIndex',
+  'syntaxHighlighting',
+  'tabSize',
+  'theme',
+];
 
-    autocompletion: {
-      replace: true,
-    },
-
-    bracketMatching: {
-      replace: true,
-    },
-
-    closeBrackets: {
-      replace: true,
-    },
-
-    collapseUnchanged: {
-      replace: true,
-    },
-
-    crosshairCursor: {
-      replace: true,
-    },
-
-    document: {
-      replace: true,
-    },
-
-    drawSelection: {
-      replace: true,
-    },
-
-    dropCursor: {
-      replace: true,
-    },
-
-    editable: {
-      replace: true,
-    },
-
-    filename: {
-      replace: true,
-    },
-
-    foldGutter: {
-      replace: true,
-    },
-
-    highlightActiveLine: {
-      replace: true,
-    },
-
-    highlightSelectionMatches: {
-      replace: true,
-    },
-
-    highlightSpecialChars: {
-      replace: true,
-    },
-
-    highlightTrailingWhitespace: {
-      replace: true,
-    },
-
-    highlightWhitespace: {
-      replace: true,
-    },
-
-    history: {
-      replace: true,
-    },
-
-    indentOnInput: {
-      replace: true,
-    },
-
-    indentUnit: {
-      replace: true,
-    },
-
-    indentWithTab: {
-      replace: true,
-    },
-
-    language: {
-      replace: true,
-    },
-
-    lineNumbers: {
-      replace: true,
-    },
-
-    lineSeparator: {
-      replace: true,
-    },
-
-    lineWrapping: {
-      replace: true,
-    },
-
-    mergeControls: {
-      replace: true,
-    },
-
-    originalDocument: {
-      replace: true,
-    },
-
-    outline: {
-      replace: true,
-    },
-
-    placeholder: {
-      replace: true,
-    },
-
-    preserveHistory: {
-      replace: true,
-    },
-
-    readOnly: {
-      replace: true,
-    },
-
-    rectangularSelection: {
-      replace: true,
-    },
-
-    scrollPastEnd: {
-      replace: true,
-    },
-
-    selectedDocumentIndex: {
-      replace: true,
-    },
-
-    selectedIndentUnitIndex: {
-      replace: true,
-    },
-
-    selectedLineSeparatorIndex: {
-      replace: true,
-    },
-
-    selectedTabSizeIndex: {
-      replace: true,
-    },
-
-    selectedThemeIndex: {
-      replace: true,
-    },
-
-    syntaxHighlighting: {
-      replace: true,
-    },
-
-    tabSize: {
-      replace: true,
-    },
-
-    theme: {
-      replace: true,
-    },
+interface QueryParamOptions {
+  [key: string]: {
+    replace: boolean;
   };
+}
+
+export default class DemoCodeMirrorRoute extends Route {
+  queryParams = QUERY_PARAMS.reduce<QueryParamOptions>((acc, param) => {
+    acc[param] = { replace: true };
+
+    return acc;
+  }, {});
 }
