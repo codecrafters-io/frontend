19845355bd946dd4c44ca24d07701b87cdac2d0d
Vasyl Marchuk <public@vasylmarchuk.com>
2024-10-06 11:10:45 +0300

    Extract `blendColors` function into an util, with unit tests

--- app/utils/code-mirror-themes.ts
+++ app/utils/code-mirror-themes.ts
@@ -1,17 +1,7 @@
 import { EditorView } from '@codemirror/view';
 import { githubDark, githubLight } from '@uiw/codemirror-theme-github';
 import tailwindColors from 'tailwindcss/colors';
-import hexRgb from 'hex-rgb';
-import rgbHex from 'rgb-hex';
-
-function blendColors(fgColor = '#ffffff', opacity = 1, bgColor = '#ffffff') {
-  const fgRgb = hexRgb(fgColor, { format: 'array' });
-  const bgRgb = hexRgb(bgColor, { format: 'array' });
-
-  const blendedRgb = fgRgb.slice(0, 3).map<number>((colFg, idx) => opacity * colFg + (1 - opacity) * (bgRgb[idx] || 255)) as [number, number, number];
-
-  return `#${rgbHex(...blendedRgb)}`;
-}
+import blendColors from 'codecrafters-frontend/utils/blend-colors';
 
 const BASE_STYLE = {
   '.cm-gutters': {
