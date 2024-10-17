71804d58c815b15da01afb87d5a93423820f5250
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-10-17 22:57:16 +0100

    Refactor current step complete modal and overlay for improved user experience.

--- app/components/course-page/current-step-complete-modal.ts
+++ app/components/course-page/current-step-complete-modal.ts
@@ -1,6 +1,7 @@
+import { service } from '@ember/service';
 import Component from '@glimmer/component';
 import type Step from 'codecrafters-frontend/utils/course-page-step-list/step';
-
+import CoursePageStateService from 'codecrafters-frontend/services/course-page-state';
 interface Signature {
   Element: HTMLDivElement;
 
@@ -10,7 +11,17 @@ interface Signature {
   };
 }
 
-export default class CurrentStepCompleteModalComponent extends Component<Signature> {}
+export default class CurrentStepCompleteModalComponent extends Component<Signature> {
+  @service declare coursePageState: CoursePageStateService;
+
+  get currentStep() {
+    return this.coursePageState.currentStep;
+  }
+
+  get nextStep() {
+    return this.coursePageState.nextStep;
+  }
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
