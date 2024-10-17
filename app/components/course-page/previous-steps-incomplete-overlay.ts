05f1e7bbf7962d9104da29f0a8ccc9b0e37b0ddc
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-10-17 21:13:29 +0100

    Refactor PreviousStepsIncompleteOverlay to use currentStep and handle modal state.

--- app/components/course-page/previous-steps-incomplete-overlay.ts
+++ app/components/course-page/previous-steps-incomplete-overlay.ts
@@ -1,13 +1,15 @@
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
 import Component from '@glimmer/component';
-import type Step from 'codecrafters-frontend/utils/course-page-step-list/step';
+import type CoursePageStateService from 'codecrafters-frontend/services/course-page-state';
+import { Step } from 'codecrafters-frontend/utils/course-page-step-list';
 
 interface Signature {
   Element: HTMLDivElement;
 
   Args: {
-    isBlurred: boolean;
-    onClose: () => void;
-    activeStep: Step;
+    currentStep: Step;
   };
 
   Blocks: {
@@ -15,7 +17,36 @@ interface Signature {
   };
 }
 
-export default class PreviousStepsIncompleteOverlayComponent extends Component<Signature> {}
+export default class PreviousStepsIncompleteOverlayComponent extends Component<Signature> {
+  @tracked modalWasDismissed = false;
+  @tracked lastSeenStepId: string | null = null;
+
+  @service declare coursePageState: CoursePageStateService;
+
+  get shouldShowModal(): boolean {
+    // This _shouldn't_ happen as long as we're always rendered in the course page, but let's be safe
+    if (!this.coursePageState.activeStep) {
+      return false;
+    }
+
+    return !this.modalWasDismissed && this.args.currentStep.status === 'locked';
+  }
+
+  @action
+  handleModalDismissed() {
+    this.modalWasDismissed = true;
+  }
+
+  @action
+  handleStepIdUpdated() {
+    if (this.args.currentStep.id === this.lastSeenStepId) {
+      return;
+    }
+
+    this.lastSeenStepId = this.args.currentStep.id;
+    this.modalWasDismissed = false;
+  }
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
