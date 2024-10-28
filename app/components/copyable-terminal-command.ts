8292b6e8e4a2103d6113f2303fdb8b4f2af955e2
Vasyl Marchuk <public@vasylmarchuk.com>
2024-10-28 12:03:57 +0200

    Modify `CopyableTerminalCommandComponent` to use `ClipboardService`

--- app/components/copyable-terminal-command.ts
+++ app/components/copyable-terminal-command.ts
@@ -1,17 +1,15 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import fade from 'ember-animated/transitions/fade';
-import type DarkModeService from 'codecrafters-frontend/services/dark-mode';
 import { service } from '@ember/service';
-import config from 'codecrafters-frontend/config/environment';
-import { task, timeout } from 'ember-concurrency';
+import type DarkModeService from 'codecrafters-frontend/services/dark-mode';
+import type ClipboardService from 'codecrafters-frontend/services/clipboard';
 
 interface Signature {
   Element: HTMLDivElement;
 
   Args: {
     commands: string[];
-    onCopyButtonClick?: () => void;
+    onCopyButtonClick?: () => void | Promise<void>;
     onVariantLabelClick?: (variant: string) => void;
     selectedVariantLabel?: string;
     variantLabels?: string[];
@@ -22,8 +20,7 @@ export default class CopyableTerminalCommandComponent extends Component<Signatur
   transition = fade;
 
   @service declare darkMode: DarkModeService;
-
-  @tracked wasCopiedRecently: boolean = false;
+  @service declare clipboard: ClipboardService;
 
   get codeForHighlighting(): string {
     return this.args.commands.join('\n');
@@ -32,18 +29,6 @@ export default class CopyableTerminalCommandComponent extends Component<Signatur
   get copyableText(): string {
     return this.args.commands.map((command) => command.replace(/# .*$/, '')).join('\n');
   }
-
-  handleCopyButtonClickTask = task({ keepLatest: true }, async (): Promise<void> => {
-    await navigator.clipboard.writeText(this.copyableText);
-    this.wasCopiedRecently = true;
-
-    if (this.args.onCopyButtonClick) {
-      this.args.onCopyButtonClick();
-    }
-
-    await timeout(config.x.copyConfirmationTimeout);
-    this.wasCopiedRecently = false;
-  });
 }
 
 declare module '@glint/environment-ember-loose/registry' {
