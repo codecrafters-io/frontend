fc7f76bd6e1a0ae148ff4f5cbf864f1ced20306f
Vasyl Marchuk <public@vasylmarchuk.com>
2024-11-08 12:20:58 +0200

    Extract `localStorage`-stubbing logic into a test helper `stubLocalStorage`

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -6,8 +6,8 @@ import { setApplication } from '@ember/test-helpers';
 import { setup as setupQunitDom } from 'qunit-dom';
 import { setup as setupQunitAssertionsExtra } from 'qunit-assertions-extra';
 import setupSinon from 'ember-sinon-qunit';
-import { stub } from 'sinon';
 import start from 'ember-exam/test-support/start';
+import stubLocalStorage from 'codecrafters-frontend/tests/support/stub-local-storage';
 // import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
@@ -18,35 +18,7 @@ setupQunitDom(QUnit.assert);
 setupQunitAssertionsExtra(QUnit.assert);
 
 QUnit.testStart(function () {
-  const localStorageCache = new Map();
-
-  stub(window.localStorage, 'getItem').callsFake(function (key) {
-    if (localStorageCache.has(key)) {
-      return localStorageCache.get(key);
-    } else {
-      return null;
-    }
-  });
-
-  stub(window.localStorage, 'setItem').callsFake(function (key, value) {
-    localStorageCache.set(key, value);
-  });
-
-  stub(window.localStorage, 'removeItem').callsFake(function (key) {
-    localStorageCache.delete(key);
-  });
-
-  stub(window.localStorage, 'clear').callsFake(function () {
-    localStorageCache.clear();
-  });
-
-  stub(window.localStorage, 'key').callsFake(function (index) {
-    return localStorageCache.key(index);
-  });
-
-  stub(window.localStorage.__proto__, 'length').get(function () {
-    return localStorageCache.size;
-  });
+  stubLocalStorage();
 });
 
 QUnit.done(async function () {
