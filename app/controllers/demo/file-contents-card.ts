29d67c6cd2545f3c9fbd96a7a6857dcd68cab82a
Vasyl Marchuk <public@vasylmarchuk.com>
2024-11-17 16:34:15 +0200

    Persist all options from `FileContentsCard` Demo in query params

--- app/controllers/demo/file-contents-card.ts
+++ app/controllers/demo/file-contents-card.ts
@@ -3,18 +3,33 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import EXAMPLE_DOCUMENTS, { ExampleDocument } from 'codecrafters-frontend/utils/code-mirror-documents';
 
+const OPTION_DEFAULTS = {
+  foldGutter: true,
+  headerTooltipText: false,
+  isCollapsed: false,
+  isCollapsible: false,
+  scrollIntoViewOnCollapse: true,
+  selectedDocumentIndex: 1,
+};
+
 export default class DemoFileContentsCardController extends Controller {
+  queryParams = ['foldGutter', 'headerTooltipText', 'isCollapsed', 'isCollapsible', 'scrollIntoViewOnCollapse', 'selectedDocumentIndex'];
+
   @tracked documents: ExampleDocument[] = EXAMPLE_DOCUMENTS;
 
-  @tracked foldGutter: boolean = true;
-  @tracked headerTooltipText: boolean = false;
-  @tracked isCollapsed: boolean = false;
-  @tracked isCollapsible: boolean = false;
-  @tracked scrollIntoViewOnCollapse: boolean = true;
-  @tracked selectedDocumentIndex: number = 1;
+  @tracked foldGutter: boolean = OPTION_DEFAULTS.foldGutter;
+  @tracked headerTooltipText: boolean = OPTION_DEFAULTS.headerTooltipText;
+  @tracked isCollapsed: boolean = OPTION_DEFAULTS.isCollapsed;
+  @tracked isCollapsible: boolean = OPTION_DEFAULTS.isCollapsible;
+  @tracked scrollIntoViewOnCollapse: boolean = OPTION_DEFAULTS.scrollIntoViewOnCollapse;
+  @tracked selectedDocumentIndex: number = OPTION_DEFAULTS.selectedDocumentIndex;
 
   get selectedDocument() {
     return this.documents[this.selectedDocumentIndex] || ExampleDocument.createEmpty();
+  }
+
+  @action resetAllOptions() {
+    Object.assign(this, OPTION_DEFAULTS);
   }
 
   @action selectedDocumentIndexDidChange(event: Event) {
