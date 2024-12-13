fed7f044c513b5f4a040998ab0e2ce8a06f2ade8
Andy Li <1450947+andy1li@users.noreply.github.com>
2024-12-13 16:36:46 +0800

    feat: Add conditional rendering for help content in first-stage tutorial steps

--- app/components/course-page/course-stage-step/first-stage-tutorial-card/navigate-to-file-step.ts
+++ app/components/course-page/course-stage-step/first-stage-tutorial-card/navigate-to-file-step.ts
@@ -1,6 +1,9 @@
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import type RepositoryModel from 'codecrafters-frontend/models/repository';
 import type CourseStageModel from 'codecrafters-frontend/models/course-stage';
+import type { CopyableTerminalCommandVariant } from 'codecrafters-frontend/components/copyable-terminal-command-with-variants';
 
 interface Signature {
   Element: HTMLDivElement;
@@ -9,16 +12,47 @@ interface Signature {
     repository: RepositoryModel;
     courseStage: CourseStageModel;
     isComplete: boolean;
+    shouldShowStage1Help: boolean;
   };
 }
 
 export default class NavigateToFileStepComponent extends Component<Signature> {
+  @tracked selectedCommandVariant: CopyableTerminalCommandVariant;
+
+  constructor(owner: unknown, args: Signature['Args']) {
+    super(owner, args);
+
+    this.selectedCommandVariant = this.commandVariants[0]!;
+  }
+
+  get commandVariants(): CopyableTerminalCommandVariant[] {
+    return [
+      {
+        label: 'Windows',
+        commands: ['start .'],
+      },
+      {
+        label: 'macOS',
+        commands: ['open .'],
+      },
+      {
+        label: 'Linux',
+        commands: ['ls'],
+      },
+    ];
+  }
+
   get filename() {
     return this.solution?.changedFiles[0]?.filename;
   }
 
   get solution() {
     return this.args.courseStage.solutions.find((solution) => solution.language === this.args.repository.language);
+  }
+
+  @action
+  handleVariantSelect(variant: CopyableTerminalCommandVariant) {
+    this.selectedCommandVariant = variant;
   }
 }
 
