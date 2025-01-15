d22395a9bd85a036b3936207b38c2d48c297aeeb
Vasyl Marchuk <public@vasylmarchuk.com>
2025-01-15 11:23:19 +0200

    Extract `toDOM` into a separate function & add `isCollapseUnchangedWidget` function

--- app/utils/code-mirror-collapse-unchanged-gutter.ts
+++ app/utils/code-mirror-collapse-unchanged-gutter.ts
@@ -1,32 +1,37 @@
-import { EditorView, gutter, GutterMarker } from '@codemirror/view';
+import { EditorView, gutter, GutterMarker, type WidgetType } from '@codemirror/view';
+
+function isCollapseUnchangedWidget(widget: WidgetType) {
+  return 'type' in widget && widget.type === 'collapsed-unchanged-code';
+}
+
+function toDOM(_view: EditorView): Node {
+  const el = document.createElement('div');
+  el.className = 'cm-collapseUnchangedGutterElement';
+  el.addEventListener('click', (_e) => {
+    const editor = el.closest('.cm-editor');
+    const gutter = el.closest('.cm-gutter');
+    const gutterElement = el.closest('.cm-gutterElement');
+    const gutterElementSiblings = gutter?.querySelectorAll('.cm-collapseUnchangedBarSibling');
+
+    if (!editor || !gutter || !gutterElement || !gutterElementSiblings) {
+      return;
+    }
+
+    // Find the index of the clicked gutter element
+    const gutterElementIndex = [...gutterElementSiblings.values()].indexOf(gutterElement);
+
+    // Find the corresponding Collapse Unchanged Bar in the content
+    const collapsedUnchangedBar = editor.querySelectorAll<HTMLElement>('.cm-content .cm-collapsedLines').item(gutterElementIndex);
+
+    collapsedUnchangedBar?.click();
+  });
+
+  return el;
+}
 
 export class CollapseUnchangedGutterMarker extends GutterMarker {
   elementClass = 'cm-collapseUnchangedBarSibling';
-
-  toDOM(_view: EditorView): Node {
-    const el = document.createElement('div');
-    el.className = 'cm-collapseUnchangedGutterElement';
-    el.addEventListener('click', (_e) => {
-      const editor = el.closest('.cm-editor');
-      const gutter = el.closest('.cm-gutter');
-      const gutterElement = el.closest('.cm-gutterElement');
-      const gutterElementSiblings = gutter?.querySelectorAll('.cm-collapseUnchangedBarSibling');
-
-      if (!editor || !gutter || !gutterElement || !gutterElementSiblings) {
-        return;
-      }
-
-      // Find the index of the clicked gutter element
-      const gutterElementIndex = [...gutterElementSiblings.values()].indexOf(gutterElement);
-
-      // Find the corresponding Collapse Unchanged Bar in the content
-      const collapsedUnchangedBar = editor.querySelectorAll<HTMLElement>('.cm-content .cm-collapsedLines').item(gutterElementIndex);
-
-      collapsedUnchangedBar?.click();
-    });
-
-    return el;
-  }
+  toDOM = toDOM;
 }
 
 export function collapseUnchangedGutter() {
@@ -35,11 +40,7 @@ export function collapseUnchangedGutter() {
       class: 'cm-collapseUnchangedGutter',
       renderEmptyElements: true,
       widgetMarker(_view, widget, _block) {
-        if ('type' in widget && widget.type === 'collapsed-unchanged-code') {
-          return new CollapseUnchangedGutterMarker();
-        }
-
-        return null;
+        return isCollapseUnchangedWidget(widget) ? new CollapseUnchangedGutterMarker() : null;
       },
     }),
   ];
