82534c3a6567471cb245d9b9983f9d45828dd0c8
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-01-16 23:04:18 +0800

    wiepr

--- app/components/course-page/course-stage-step/first-stage-tutorial-card/submit-code-step.ts
+++ app/components/course-page/course-stage-step/first-stage-tutorial-card/submit-code-step.ts
@@ -1,7 +1,9 @@
 import { action } from '@ember/object';
 import { service } from '@ember/service';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import type CoursePageStateService from 'codecrafters-frontend/services/course-page-state';
+import type FeatureFlagsService from 'codecrafters-frontend/services/feature-flags';
 
 interface Signature {
   Element: HTMLDivElement;
@@ -13,6 +15,14 @@ interface Signature {
 
 export default class SubmitCodeStepComponent extends Component<Signature> {
   @service declare coursePageState: CoursePageStateService;
+  @service declare featureFlags: FeatureFlagsService;
+
+  @tracked isCommitModalOpen = false;
+  @tracked isPushModalOpen = false;
+
+  get canSeeSplitUpGitCommandsForStage1() {
+    return this.featureFlags.canSeeSplitUpGitCommandsForStage1;
+  }
 
   @action
   handleViewLogsButtonClick() {
