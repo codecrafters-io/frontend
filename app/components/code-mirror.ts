dde0484362d87eef51a07398adb98ac3d5779fec
Vasyl Marchuk <public@vasylmarchuk.com>
2025-01-18 17:56:50 +0200

    [WIP] Add `lineComments` CodeMirror extension - external comments

--- app/components/code-mirror.ts
+++ app/components/code-mirror.ts
@@ -43,7 +43,7 @@ import { collapseRanges } from 'codecrafters-frontend/utils/code-mirror-collapse
 import { collapseRangesGutter } from 'codecrafters-frontend/utils/code-mirror-collapse-ranges-gutter';
 import { collapseUnchanged } from 'codecrafters-frontend/utils/code-mirror-collapse-unchanged';
 import { collapseUnchangedGutter } from 'codecrafters-frontend/utils/code-mirror-collapse-unchanged-gutter';
-import { lineComments } from 'codecrafters-frontend/utils/code-mirror-line-comments';
+import { lineComments, type LineCommentsCollection } from 'codecrafters-frontend/utils/code-mirror-line-comments';
 
 function generateHTMLElement(src: string): HTMLElement {
   const div = document.createElement('div');
@@ -61,7 +61,7 @@ export type LineRange = { startLine: number; endLine: number };
 
 type DocumentUpdateCallback = (newValue: string) => void;
 
-type Argument = boolean | string | number | undefined | Extension | DocumentUpdateCallback | LineRange[];
+type Argument = boolean | string | number | undefined | Extension | DocumentUpdateCallback | LineRange[] | LineCommentsCollection;
 
 type OptionHandler = (args: Signature['Args']['Named']) => Extension[] | Promise<Extension[]>;
 
@@ -89,7 +89,7 @@ const OPTION_HANDLERS: { [key: string]: OptionHandler } = {
   indentOnInput: ({ indentOnInput: enabled }) => (enabled ? [indentOnInput()] : []),
   indentUnit: ({ indentUnit: indentUnitText }) => (indentUnitText !== undefined ? [indentUnit.of(indentUnitText)] : []),
   indentWithTab: ({ indentWithTab: enabled }) => (enabled ? [keymap.of([indentWithTab])] : []),
-  lineComments: ({ lineComments: enabled }) => (enabled ? [lineComments()] : []),
+  lineCommentsOrComments: ({ comments = [], lineComments: enabled }) => (enabled ? [lineComments(comments)] : []),
   lineNumbers: ({ lineNumbers: enabled }) => (enabled ? [lineNumbers()] : []),
   foldGutter: ({ foldGutter: enabled }) =>
     enabled
@@ -194,6 +194,10 @@ export interface Signature {
        * Enable collapsing unchanged lines in the diff editor
        */
       collapseUnchanged?: boolean;
+      /**
+       * Comments to show for each line in the editor, if `lineComments` are enabled
+       */
+      comments?: LineCommentsCollection;
       /**
        * Use a crosshair cursor over the editor when ALT key is pressed
        */
