79e5beacbadb91695798de16fd12d0f36803f872
Vasyl Marchuk <public@vasylmarchuk.com>
2025-01-19 19:22:31 +0200

    [WIP] Add `lineComments` CodeMirror extension - `lineCommentsGutter` in separate module

--- app/utils/code-mirror-line-comments.ts
+++ app/utils/code-mirror-line-comments.ts
@@ -1,10 +1,8 @@
-import { BlockInfo, Decoration, EditorView, WidgetType, type DecorationSet } from '@codemirror/view';
+import { Decoration, EditorView, WidgetType, type DecorationSet } from '@codemirror/view';
 import { EditorState, Facet, Line, StateField } from '@codemirror/state';
-import {
-  gutter as gutterRS,
-  GutterMarker as GutterMarkerRS,
-  highlightActiveLineGutter as highlightActiveLineGutterRS,
-} from 'codecrafters-frontend/utils/code-mirror-gutter-rs';
+import { lineCommentsGutter } from 'codecrafters-frontend/utils/code-mirror-line-comments-gutter';
+
+export const lineDataFacet = Facet.define<LineDataCollection>();
 
 export class LineData {
   lineNumber: number;
@@ -67,8 +65,6 @@ function lineCommentsDecorations(state: EditorState) {
   return Decoration.set(decorations);
 }
 
-const lineDataFacet = Facet.define<LineDataCollection>();
-
 const lineCommentsStateField = StateField.define<DecorationSet>({
   create(state) {
     return lineCommentsDecorations(state);
@@ -82,64 +78,13 @@ const lineCommentsStateField = StateField.define<DecorationSet>({
   },
 });
 
-class CommentsCountGutterMarker extends GutterMarkerRS {
-  line: BlockInfo;
-
-  constructor(line: BlockInfo) {
-    super();
-    this.line = line;
-  }
-
-  toDOM(view: EditorView) {
-    const lineNumber = view.state.doc.lineAt(this.line.from).number;
-    const commentsCount = view.state.facet(lineDataFacet)[0]?.dataForLine(lineNumber)?.commentsCount || 0;
-    const elem = document.createElement('comments-count');
-
-    elem.innerText = `${commentsCount > 99 ? '99+' : commentsCount}`;
-
-    if (commentsCount > 99) {
-      elem.className = 'cm-over-99';
-    }
-
-    return elem;
-  }
-}
-
-class CommentButtonGutterMarker extends GutterMarkerRS {
-  line: BlockInfo;
-
-  constructor(line: BlockInfo) {
-    super();
-    this.line = line;
-  }
-
-  toDOM() {
-    const elem = document.createElement('comment-button');
-
-    elem.innerText = `💬`;
-
-    return elem;
-  }
-}
-
 export function lineComments(lineData: LineDataCollection) {
   return [
     lineDataFacet.of(lineData),
 
     lineCommentsStateField,
 
-    gutterRS({
-      class: 'cm-lineCommentsGutter',
-
-      lineMarker(view, line) {
-        const lineNumber = view.state.doc.lineAt(line.from).number;
-        const commentsCount = view.state.facet(lineDataFacet)[0]?.dataForLine(lineNumber)?.commentsCount || 0;
-
-        return new (commentsCount === 0 ? CommentButtonGutterMarker : CommentsCountGutterMarker)(line);
-      },
-    }),
-
-    highlightActiveLineGutterRS(),
+    lineCommentsGutter(),
 
     EditorView.baseTheme({
       '.cm-line': {
@@ -156,47 +101,6 @@ export function lineComments(lineData: LineDataCollection) {
 
         '& .cm-insertedLine + br': {
           display: 'none',
-        },
-      },
-
-      '.cm-gutters.cm-gutters-rs': {
-        backgroundColor: '#ffffff20', // '#ff000070', // 'transparent',
-
-        '& .cm-lineCommentsGutter': {
-          minWidth: '24px',
-          textAlign: 'center',
-
-          '& .cm-gutterElement': {
-            cursor: 'pointer',
-
-            '& comments-count': {
-              display: 'block',
-              backgroundColor: '#ffcd72c0',
-              borderRadius: '50%',
-              color: '#24292e',
-              transform: 'scale(0.75)',
-              fontWeight: '500',
-              fontSize: '12px',
-
-              '&.cm-over-99': {
-                fontSize: '9.5px',
-              },
-            },
-
-            '& comment-button': {
-              opacity: '0.15',
-            },
-
-            '&:hover': {
-              '& comment-button': {
-                opacity: '1',
-              },
-
-              '& comments-count': {
-                backgroundColor: '#ffa500',
-              },
-            },
-          },
         },
       },
     }),
