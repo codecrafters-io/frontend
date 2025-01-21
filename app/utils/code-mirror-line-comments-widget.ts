dc400b6c92dcdcde071370073b6c0b176bcc09d1
Vasyl Marchuk <public@vasylmarchuk.com>
2025-01-21 14:40:20 +0200

    [WIP] Add `lineComments` CodeMirror extension - toggling of lineCommentsWidget on/off is working

--- app/utils/code-mirror-line-comments-widget.ts
+++ app/utils/code-mirror-line-comments-widget.ts
@@ -1,55 +1,75 @@
 import { Decoration, EditorView, WidgetType, type DecorationSet } from '@codemirror/view';
-import { EditorState, Line, StateField } from '@codemirror/state';
-import { lineDataFacet } from 'codecrafters-frontend/utils/code-mirror-line-comments';
+import { Line, StateEffect, StateField } from '@codemirror/state';
+import { lineDataFacet, expandedLineNumbersFacet, expandedLineNumbersCompartment } from 'codecrafters-frontend/utils/code-mirror-line-comments';
 
 class LineCommentsWidget extends WidgetType {
   line: Line;
+  isExpanded: boolean;
 
-  constructor(line: Line) {
+  constructor(line: Line, isExpanded: boolean = false) {
     super();
     this.line = line;
+    this.isExpanded = isExpanded;
   }
 
   toDOM(view: EditorView): HTMLElement {
+    const expandedLines = view.state.facet(expandedLineNumbersFacet)[0] || [];
     const commentsCount = view.state.facet(lineDataFacet)[0]?.dataForLine(this.line.number)?.commentsCount || 0;
+    const classNames = ['cm-lineCommentsWidget'];
+
+    if (expandedLines.includes(this.line.number) || this.isExpanded) {
+      classNames.push('cm-lineCommentsWidgetExpanded');
+    }
 
     const elem = document.createElement('div');
-    elem.className = 'cm-lineCommentsWidget';
+    elem.className = classNames.join(' ');
 
     if (commentsCount) {
       elem.innerText = `💬 COMMENTS FOR LINE #${this.line.number} (COUNT: ${commentsCount})`;
+    } else {
+      elem.innerText = `💬 ADD COMMENT FOR LINE #${this.line.number}`;
     }
 
     return elem;
   }
 }
 
-function lineCommentsWidgetDecorations(state: EditorState) {
-  const decorations = [];
-
-  for (let i = 1; i <= state.doc.lines; i++) {
-    const line = state.doc.line(i);
-    decorations.push(
-      Decoration.widget({
-        widget: new LineCommentsWidget(line),
-        side: 10,
-        // inlineOrder: true,
-        // block: true,
-      }).range(line.to),
-    );
-  }
-
-  return Decoration.set(decorations);
+function lineCommentsWidgetDecoration(line: Line) {
+  return Decoration.widget({
+    widget: new LineCommentsWidget(line),
+    side: 10,
+    // inlineOrder: true,
+    // block: true,
+  }).range(line.to);
 }
 
 const lineCommentsWidgetStateField = StateField.define<DecorationSet>({
   create(state) {
-    return lineCommentsWidgetDecorations(state);
+    return Decoration.set(Array.from({ length: state.doc.lines }, (_, i) => lineCommentsWidgetDecoration(state.doc.line(i + 1))));
   },
-  update(_value, tr) {
-    return lineCommentsWidgetDecorations(tr.state);
-    // return tr.docChanged ? lineCommentsDecorations(tr.state) : value;
+
+  update(decorations, transaction) {
+    if (transaction.docChanged) {
+      return this.create(transaction.state);
+    }
+
+    // let newDecorations = decorations.map(transaction.changes);
+
+    for (const effect of transaction.effects) {
+      if (effect instanceof StateEffect && effect.value?.compartment === expandedLineNumbersCompartment) {
+        return this.create(transaction.state);
+
+        // newDecorations = newDecorations.update({ filter: (pos) => pos !== effect.value.pos }).update({
+        //   add: [lineCommentsWidgetDecoration(transaction.state.doc.lineAt(effect.value.pos))],
+        // });
+      }
+    }
+
+    // return newDecorations;
+
+    return decorations;
   },
+
   provide(field) {
     return EditorView.decorations.from(field);
   },
@@ -58,10 +78,15 @@ const lineCommentsWidgetStateField = StateField.define<DecorationSet>({
 const lineCommentsWidgetBaseTheme = EditorView.baseTheme({
   '.cm-line': {
     '& .cm-lineCommentsWidget': {
-      display: 'block',
+      display: 'none',
       backgroundColor: '#009bff40',
       paddingLeft: '1rem',
       marginRight: '-1rem',
+
+      '&.cm-lineCommentsWidgetExpanded': {
+        display: 'block',
+        backgroundColor: '#009bff80',
+      },
 
       '& + br': {
         display: 'none',
