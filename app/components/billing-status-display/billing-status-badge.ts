f414d7fefb2f5c262df17d4d749fa4e8c20ef502
Ryan Gang <ryan-gg@outlook.com>
2025-01-27 13:27:08 +0530

    feat: add BillingStatusBadge component for dynamic billing status rendering
    Create a new component to handle various billing status scenarios, including VIP badge, member badge, discount timer, free weeks, and upgrade button

---
+++ app/components/billing-status-display/billing-status-badge.ts
@@ -0,0 +1,61 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import type AuthenticatorService from 'codecrafters-frontend/services/authenticator';
+import type FeatureFlagsService from 'codecrafters-frontend/services/feature-flags';
+import type RouterService from '@ember/routing/router-service';
+import type UserModel from 'codecrafters-frontend/models/user';
+import type PromotionalDiscountModel from 'codecrafters-frontend/models/promotional-discount';
+
+interface Signature {
+  Element: HTMLDivElement;
+  Args: {};
+}
+
+export default class BillingStatusDisplayComponent extends Component<Signature> {
+  @service declare authenticator: AuthenticatorService;
+  @service declare featureFlags: FeatureFlagsService;
+  @service declare router: RouterService;
+
+  get activeDiscountForYearlyPlan(): PromotionalDiscountModel | null {
+    return this.currentUser?.activeDiscountForYearlyPlan || null;
+  }
+
+  get canSeeDiscountCountdown() {
+    return this.featureFlags.canSeeDiscountCountdown;
+  }
+
+  get currentUser(): UserModel | null {
+    return this.authenticator.currentUser;
+  }
+
+  get shouldShowFreeWeeksLeftButton(): boolean {
+    return (
+      !!this.currentUser &&
+      !this.currentUser.canAccessMembershipBenefits &&
+      this.currentUser.hasActiveFreeUsageGrants &&
+      !this.currentUser.hasActiveFreeUsageGrantsValueIsOutdated
+    );
+  }
+
+  get shouldShowMemberBadge(): boolean {
+    return !!this.currentUser && this.currentUser.hasActiveSubscription;
+  }
+
+  get shouldShowUpgradeButton(): boolean {
+    if (this.currentUser) {
+      return !this.currentUser.canAccessPaidContent && this.router.currentRouteName !== 'pay';
+    } else {
+      return true;
+    }
+  }
+
+  get shouldShowVipBadge(): boolean {
+    return !!this.currentUser && this.currentUser.isVip;
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'BillingStatusDisplay::BillingStatusBadge': typeof BillingStatusDisplayComponent;
+  }
+}
