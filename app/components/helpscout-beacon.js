db9cdf4c807a976d4f13560fce3c29e8baefd344
Ryan Gang <ryan-gg@outlook.com>
2025-02-03 15:03:38 +0530

    feat: add HelpScout Beacon component for customer support

---
+++ app/components/helpscout-beacon.js
@@ -0,0 +1,21 @@
+import Component from '@glimmer/component';
+import { loadScript } from '../utils/load-helpscout-beacon';
+import { action } from '@ember/object';
+
+export default class HelpScoutBeaconComponent extends Component {
+  @action
+  async initBeacon() {
+    try {
+      await loadScript('https://beacon-v2.helpscout.net', () => {
+        if (typeof window.Beacon === 'function') {
+          window.Beacon('init', 'bb089ae9-a4ae-4114-8f7a-b660f6310158');
+          console.log('HelpScout Beacon loaded successfully.');
+        } else {
+          console.error('HelpScout Beacon is not available on the window object.');
+        }
+      });
+    } catch (error) {
+      console.error('Error loading HelpScout Beacon:', error);
+    }
+  }
+}
