aeb38ad403002605b90fe1cb88d6a902dbf2f344
Ryan Gang <ryan-gg@outlook.com>
2025-02-04 12:05:32 +0530

    test: enhance header authentication test with upgrade button checks

--- tests/acceptance/header-test.js
+++ tests/acceptance/header-test.js
@@ -11,12 +11,16 @@ module('Acceptance | header-test', function (hooks) {
   setupApplicationTest(hooks);
   setupAnimationTest(hooks);
 
-  test('header should show sign-in button if user is not authenticated', async function (assert) {
+  test('header should show sign-in & upgrade button if user is unauthenticated', async function (assert) {
     testScenario(this.server);
 
     await catalogPage.visit();
 
     assert.true(catalogPage.header.signInButton.isVisible, 'expect sign-in button to be visible');
+    assert.true(catalogPage.header.upgradeButton.isVisible, 'expect billing status  badge to be visible');
+    await catalogPage.header.upgradeButton.click();
+
+    assert.strictEqual(currentURL(), '/pay', 'expect to be redirected to pay page');
   });
 
   test('header should show upgrade button if user does not have an active subscription', async function (assert) {
