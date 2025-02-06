0a4a45b0e69218cfa6006a92d71d4b6a675e7a41
Ryan Gang <ryan-gg@outlook.com>
2025-02-06 17:35:18 +0530

    refactor: improve concept engagement block group tracking
    Update `ConceptEngagementModel` to introduce block group tracking with:
    - New `BlockGroup` interface
    - Cached methods for calculating block groups
    - Tracked property for last revealed block group index
    - Updated methods for calculating completed blocks count

--- app/models/concept-engagement.ts
+++ app/models/concept-engagement.ts
@@ -1,6 +1,12 @@
-import ConceptModel from 'codecrafters-frontend/models/concept';
+import ConceptModel, { type Block } from 'codecrafters-frontend/models/concept';
 import UserModel from 'codecrafters-frontend/models/user';
 import Model, { attr, belongsTo } from '@ember-data/model';
+import { cached, tracked } from '@glimmer/tracking';
+
+export interface BlockGroup {
+  index: number;
+  blocks: Block[];
+}
 
 export default class ConceptEngagementModel extends Model {
   @belongsTo('concept', { async: false, inverse: 'engagements' }) declare concept: ConceptModel;
@@ -10,12 +16,47 @@ export default class ConceptEngagementModel extends Model {
   @attr('date') declare lastActivityAt: Date;
   @attr('date') declare startedAt: Date;
 
+  // This needs to be updated on button clicks
+  @tracked lastRevealedBlockGroupIndex: number | null = null;
+
+  get currentBlockGroupIndex() {
+    return this.lastRevealedBlockGroupIndex || 0;
+  }
+
   get completedBlocksCount() {
-    return Math.round((this.currentProgressPercentage / 100) * this.totalBlocksCount);
+    return this.allBlockGroups.reduce((count, blockGroup) => {
+      if (blockGroup.index < this.currentBlockGroupIndex) {
+        count += blockGroup.blocks.length;
+      }
+
+      return count;
+    }, 0);
+  }
+
+  @cached
+  get allBlocks() {
+    return this.concept.parsedBlocks;
+  }
+
+  @cached
+  get allBlockGroups(): BlockGroup[] {
+    return this.allBlocks.reduce((groups, block) => {
+      if (groups.length <= 0) {
+        groups.push({ index: 0, blocks: [] });
+      }
+
+      (groups[groups.length - 1] as BlockGroup).blocks.push(block);
+
+      if (block.isInteractable || groups.length === 0) {
+        groups.push({ index: groups.length, blocks: [] });
+      }
+
+      return groups;
+    }, [] as BlockGroup[]);
   }
 
   get totalBlocksCount() {
-    const allBlocks = this.concept.parsedBlocks;
+    const allBlocks = this.allBlockGroups;
 
     return allBlocks.length;
   }
