a3926244bf2b7a89a16fb688505fd5cba4fdab83
Ryan Gang <ryan-gg@outlook.com>
2025-02-06 19:48:38 +0530

    test: add data test attribute and page object for HelpScout Beacon

--- tests/acceptance/helpscount-beacon-test.js
+++ tests/acceptance/helpscount-beacon-test.js
@@ -3,9 +3,9 @@ import { setupApplicationTest } from 'codecrafters-frontend/tests/helpers';
 import { setupAnimationTest, animationsSettled } from 'ember-animated/test-support';
 import testScenario from 'codecrafters-frontend/mirage/scenarios/test';
 import { waitUntil, visit } from '@ember/test-helpers';
-// import { pauseTest } from '@ember/test-helpers';
 import { signIn, signInAsAdmin } from 'codecrafters-frontend/tests/support/authentication-helpers';
 import catalogPage from 'codecrafters-frontend/tests/pages/catalog-page';
+import helpscoutBeacon from 'codecrafters-frontend/tests/pages/components/helpscout-beacon';
 
 module('Acceptance | helpscout-beacon', function (hooks) {
   setupApplicationTest(hooks);
@@ -16,34 +16,18 @@ module('Acceptance | helpscout-beacon', function (hooks) {
     await visit('/catalog');
     await animationsSettled();
 
-    // await waitUntil(() => window.Beacon, { timeout: 1000 });
-
-    assert.ok(window.Beacon, 'Helpscout Beacon is initialized');
-    assert.strictEqual(typeof window.Beacon, 'function', 'Beacon function is available');
-    window.Beacon('init', 'bb089ae9-a4ae-4114-8f7a-b660f6310158');
-    window.Beacon('info');
-    console.log('window.Beacon', window.Beacon);
-    console.log(window.Beacon('info'));
-    // await pauseTest();
-});
+    await this.pauseTest();
+    assert.ok(helpscoutBeacon.isPresent, 'Helpscout Beacon is present');
+    await this.pauseTest();
+  });
 
   test("beacon doesn't render on concepts page", async function (assert) {
     testScenario(this.server);
-
-    signInAsAdmin(this.owner, this.server);
-    await catalogPage.visit();
-
-    // await pauseTest();
-    await catalogPage.header.clickOnHeaderLink('Badges');
-
+    await conceptsPage.visit();
     await animationsSettled();
 
-    // await waitUntil(() => window.Beacon, { timeout: 1000 });
-
-    assert.ok(!window.Beacon, 'Helpscout Beacon is not initialized');
-    assert.strictEqual(typeof window.Beacon, 'undefined', 'Beacon function is not available');
-    console.log('window.Beacon', window.Beacon);
-    console.log(window.Beacon('info'));
-    // await pauseTest();
+    await this.pauseTest();
+    assert.false(helpscoutBeacon.isPresent, 'Helpscout Beacon is not present');
+    await this.pauseTest();
   });
 });
