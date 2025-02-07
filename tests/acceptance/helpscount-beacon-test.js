4d42ed615fed4191e65aef4cab3f8e9fc8748c5e
Ryan Gang <ryan-gg@outlook.com>
2025-02-07 19:15:49 +0530

    test: add tests for helpscout beacon visibility

--- tests/acceptance/helpscount-beacon-test.js
+++ tests/acceptance/helpscount-beacon-test.js
@@ -6,28 +6,48 @@ import { waitUntil, visit } from '@ember/test-helpers';
 import { signIn, signInAsAdmin } from 'codecrafters-frontend/tests/support/authentication-helpers';
 import catalogPage from 'codecrafters-frontend/tests/pages/catalog-page';
 import helpscoutBeacon from 'codecrafters-frontend/tests/pages/components/helpscout-beacon';
+import conceptsPage from '../pages/concepts-page';
+import userPage from 'codecrafters-frontend/tests/pages/user-page';
+import createConceptFromFixture from 'codecrafters-frontend/mirage/utils/create-concept-from-fixture';
+import dummy from 'codecrafters-frontend/mirage/concept-fixtures/dummy';
+import networkProtocols from 'codecrafters-frontend/mirage/concept-fixtures/network-protocols';
 
-module('Acceptance | helpscout-beacon', function (hooks) {
+function createConcepts(server) {
+  createConceptFromFixture(server, networkProtocols);
+  createConceptFromFixture(server, dummy);
+}
+
+module('Acceptance | helpscout-beacon-test', function (hooks) {
   setupApplicationTest(hooks);
   setupAnimationTest(hooks);
 
   test('beacon renders on catalog page', async function (assert) {
     testScenario(this.server);
-    await visit('/catalog');
-    await animationsSettled();
+    await catalogPage.visit();
 
-    await this.pauseTest();
-    assert.ok(helpscoutBeacon.isPresent, 'Helpscout Beacon is present');
-    await this.pauseTest();
+    assert.ok(catalogPage.helpscoutBeacon.isPresent, 'Helpscout Beacon is present');
   });
 
   test("beacon doesn't render on concepts page", async function (assert) {
     testScenario(this.server);
     await conceptsPage.visit();
-    await animationsSettled();
 
-    await this.pauseTest();
-    assert.false(helpscoutBeacon.isPresent, 'Helpscout Beacon is not present');
-    await this.pauseTest();
+    assert.false(conceptsPage.helpscoutBeacon.isPresent, 'Helpscout Beacon is not present');
+  });
+  
+  test("beacon doesn't render on concepts page", async function (assert) {
+    testScenario(this.server);
+    createConcepts(this.server);
+
+    await conceptsPage.visit();
+    await conceptsPage.clickOnConceptCard('Network Protocols');
+    assert.false(conceptsPage.helpscoutBeacon.isPresent, 'Helpscout Beacon is not present');
+  });
+
+  test("beacon doesn't render on user pages", async function (assert) {
+    testScenario(this.server);
+    await userPage.visit({ username: 'rohitpaulk' });
+
+    assert.false(userPage.helpscoutBeacon.isPresent, 'Helpscout Beacon is not present');
   });
 });
