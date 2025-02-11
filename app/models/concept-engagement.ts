98207cd64061c8dca178f341e55ab43c1ddb12fa
Ryan Gang <ryan-gg@outlook.com>
2025-02-11 17:59:42 +0530

    refactor: update completedBlockGroupsCount calculation method
    Remove parseIntoBlockGroups method and replace with a more direct conversion of block progress to block group progress

--- app/models/concept-engagement.ts
+++ app/models/concept-engagement.ts
@@ -1,4 +1,4 @@
-import ConceptModel, { type Block, type BlockGroup } from 'codecrafters-frontend/models/concept';
+import ConceptModel, { type BlockGroup } from 'codecrafters-frontend/models/concept';
 import UserModel from 'codecrafters-frontend/models/user';
 import Model, { attr, belongsTo } from '@ember-data/model';
 
@@ -11,32 +11,28 @@ export default class ConceptEngagementModel extends Model {
   @attr('date') declare startedAt: Date;
 
   get completedBlockGroupsCount() {
-    const completedBlocksIndex = Math.round((this.currentProgressPercentage / 100) * this.totalBlocksCount);
+    const completedBlocksCount = Math.round((this.currentProgressPercentage / 100) * this.totalBlocksCount);
 
-    return this.parseIntoBlockGroups(this.concept.parsedBlocks.slice(0, completedBlocksIndex)).length;
+    return this.convertBlockProgressIntoBlockGroupProgress(this.concept.allBlockGroups, completedBlocksCount);
+  }
+
+  convertBlockProgressIntoBlockGroupProgress(allBlockGroups: BlockGroup[], completedBlocksCount: number) {
+    let completedBlockGroups = 0;
+    for (const blockGroup of allBlockGroups) {
+      completedBlockGroups += blockGroup.blocks.length;
+      if (completedBlocksCount <= completedBlockGroups) {
+        return blockGroup.index + 2;
+      }
+    }
+
+    return allBlockGroups.length;
   }
 
   get totalBlockGroupsCount() {
-    return this.parseIntoBlockGroups(this.concept.parsedBlocks).length;
+    return this.concept.allBlockGroups.length;
   }
 
   get totalBlocksCount() {
     return this.concept.parsedBlocks.length;
-  }
-
-  parseIntoBlockGroups(blocks: Block[]): BlockGroup[] {
-    return blocks.reduce((groups, block) => {
-      if (groups.length <= 0) {
-        groups.push({ index: 0, blocks: [] });
-      }
-
-      (groups[groups.length - 1] as BlockGroup).blocks.push(block);
-
-      if (block.isInteractable || groups.length === 0) {
-        groups.push({ index: groups.length, blocks: [] });
-      }
-
-      return groups;
-    }, [] as BlockGroup[]);
   }
 }
