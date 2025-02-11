217fadc2c02ed347f324886e92ccb25142427dc0
Ryan Gang <ryan-gg@outlook.com>
2025-02-11 12:52:05 +0530

    test: add rust content to dummy concept's fixture

--- mirage/concept-fixtures/dummy.js
+++ mirage/concept-fixtures/dummy.js
@@ -6,7 +6,7 @@ export default {
       blocks: [
         { args: { markdown: 'Hello, world!' }, type: 'markdown' },
         { args: {}, type: 'click_to_continue' },
-        { args: { markdown: 'Hello world 2!' }, type: 'markdown' },
+        { args: { markdown: '### Variable assignment with `if` expressions\n\nYou can also assign the result of an `if` expression.\n\nIn the example below, `weather` is getting assigned by the result of the `if` expression.\n\n```rust\nlet temperature = 15;\n\nlet weather = if temperature <= 10 {\n      "cold"\n} else if temperature <= 20 {\n      "cool"\n} else {\n      "warm"\n};,\n\nprintln!("The weather is {}", weather); // "The weather is cool"\n```' }, type: 'markdown' },
         { args: {}, type: 'click_to_continue' },
         { args: { concept_question_slug: 'dummy-q' }, type: 'concept_question' },
         { args: { markdown: "And that's the end!" }, type: 'markdown' },
@@ -27,7 +27,7 @@ export default {
       id: '4e032a49-34aa-4635-97b8-52b46e66db20',
       type: 'concept-questions',
       attributes: {
-        'query-markdown': "What's the question?",
+        'query-markdown': "What's the question?\n\n```rust\nlet temperature = 15;\n\nlet weather = if temperature <= 10 {\n      \"cold\"\n} else if temperature <= 20 {\n      \"cool\"\n} else {\n      \"warm\"\n};,\n\nprintln!(\"The weather is {}\", weather); // \"The weather is cool\"\n```",
         options: [
           { markdown: 'Wrong answer', is_correct: false, explanation_markdown: null },
           { markdown: 'Correct', is_correct: true, explanation_markdown: null },
