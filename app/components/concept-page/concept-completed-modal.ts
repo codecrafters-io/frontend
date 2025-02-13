39e2cf2d9c2dad549e9fdabbe7afdfd047638cd5
Ryan Gang <ryan-gg@outlook.com>
2025-02-13 12:56:28 +0530

    feat: enhance concept completed modal with dynamic access message
    Add support for displaying a dynamic access message in the concept completed modal based on the concept group title. This provides more context to users about what content they'll gain access to when signing in with GitHub.

--- app/components/concept-page/concept-completed-modal.ts
+++ app/components/concept-page/concept-completed-modal.ts
@@ -1,15 +1,25 @@
 import Component from '@glimmer/component';
 import ConceptModel from 'codecrafters-frontend/models/concept';
+import type ConceptGroupModel from 'codecrafters-frontend/models/concept-group';
 
 type Signature = {
   Args: {
     concept: ConceptModel;
+    conceptGroup: ConceptGroupModel;
   };
 
   Element: HTMLDivElement;
 };
 
-export default class ConceptCompletedModal extends Component<Signature> {}
+export default class ConceptCompletedModal extends Component<Signature> {
+  get getAccessToMessage() {
+    if (this.args.conceptGroup?.title) {
+      return `the rest of ${this.args.conceptGroup.title}`;
+    }
+
+    return 'all CodeCrafters concepts';
+  }
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
