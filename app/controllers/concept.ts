cd4aa68f3b55e6e8a62bd191a50dd59176447198
Ryan Gang <ryan-gg@outlook.com>
2025-02-19 20:57:31 +0530

    feat: implement handleEngagementCreate method with async update logic

--- app/controllers/concept.ts
+++ app/controllers/concept.ts
@@ -1,6 +1,24 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 import type { ConceptRouteModel } from 'codecrafters-frontend/routes/concept';
+import type ConceptEngagementModel from 'codecrafters-frontend/models/concept-engagement';
+import { next } from '@ember/runloop';
 
 export default class ConceptController extends Controller {
   declare model: ConceptRouteModel;
+
+  @action
+  async handleEngagementCreate(engagement: ConceptEngagementModel) {
+    console.log('Engagement created in concept controller', engagement);
+    this.model.latestConceptEngagement = engagement;
+    console.log('Force set');
+    this.set('model.latestConceptEngagement', engagement);
+
+    await new Promise((resolve) => {
+      next(() => {
+        this.set('model.latestConceptEngagement', engagement);
+        resolve(undefined);
+      });
+    });
+  }
 }
