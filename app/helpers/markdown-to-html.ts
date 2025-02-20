75343ab22c90e03a2c4234a805fee94a657bf842
Ryan Gang <ryan-gg@outlook.com>
2025-02-20 21:00:52 +0530

    chore: lint fix

--- app/helpers/markdown-to-html.ts
+++ app/helpers/markdown-to-html.ts
@@ -44,6 +44,7 @@ export default class MarkdownToHtml extends Helper<Signature> {
         node.setAttribute('target', '_blank');
         node.setAttribute('rel', 'noopener noreferrer');
       }
+
       if (node.nodeName === 'PRE') {
         node.setAttribute('tabindex', '-1');
       }
