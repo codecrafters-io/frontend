9b3b339b2be95c66158dd908bf4192fc1f733c22
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-02-21 01:56:37 -0800

    feat: implement expandable concept group section
    Add functionality to expand and collapse the concept list in the 
    PrimerConceptGroupSection component. Introduce tracked properties 
    and actions to manage the expanded state, enhancing user 
    interaction. Update the template to reflect the new behavior 
    with appropriate buttons for collapsing and showing all concepts.

--- app/components/track-page/primer-concept-group-section/index.ts
+++ app/components/track-page/primer-concept-group-section/index.ts
@@ -1,7 +1,9 @@
 import Component from '@glimmer/component';
 import ConceptGroupModel from 'codecrafters-frontend/models/concept-group';
 import LanguageModel from 'codecrafters-frontend/models/language';
-
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { next } from '@ember/runloop';
 interface Signature {
   Element: HTMLDivElement;
 
@@ -11,7 +13,23 @@ interface Signature {
   };
 }
 
-export default class TrackPagePrimerConceptGroupSectionComponent extends Component<Signature> {}
+export default class TrackPagePrimerConceptGroupSectionComponent extends Component<Signature> {
+  @tracked conceptListIsExpanded = false;
+
+  @action
+  handleCollapseButtonClick() {
+    next(() => {
+      this.conceptListIsExpanded = false;
+    });
+  }
+
+  @action
+  handleSectionClick() {
+    if (!this.conceptListIsExpanded) {
+      this.conceptListIsExpanded = true;
+    }
+  }
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
