076d2080926e7c8cccd304f3d630a2eebdd18a8c
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-02-22 00:47:52 -0800

    feat: improve HelpScout Beacon initialization logic
    Refactor the HelpScoutBeaconComponent to prevent initialization 
    during FastBoot. The changes ensure that the Beacon is only 
    initialized in a browser environment, enhancing compatibility 
    and preventing errors during server-side rendering. The code 
    is streamlined for better readability and maintainability.

--- app/components/helpscout-beacon.js
+++ app/components/helpscout-beacon.js
@@ -3,8 +3,11 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import config from 'codecrafters-frontend/config/environment';
 import * as Sentry from '@sentry/ember';
+import { inject as service } from '@ember/service';
 
 export default class HelpScoutBeaconComponent extends Component {
+  @service fastboot;
+
   @tracked beaconInitialized = false;
 
   constructor() {
@@ -22,16 +25,16 @@ export default class HelpScoutBeaconComponent extends Component {
 
   @action
   async initBeacon() {
-    try {
-      if (typeof window.Beacon === 'function') {
-        window.Beacon('init', config.x.helpscoutBeaconId);
-        this.beaconInitialized = true;
-      } else {
-        console.log('HelpScout Beacon not available after script load.');
-        Sentry.captureMessage('HelpScout Beacon not available after script load.');
-      }
-    } catch (error) {
-      console.error('Error loading HelpScout Beacon:', error);
+    if (this.fastboot.isFastBoot) {
+      return;
+    }
+
+    if (typeof window.Beacon === 'function') {
+      window.Beacon('init', config.x.helpscoutBeaconId);
+      this.beaconInitialized = true;
+    } else {
+      console.log('HelpScout Beacon not available after script load.');
+      Sentry.captureMessage('HelpScout Beacon not available after script load.');
     }
   }
 
