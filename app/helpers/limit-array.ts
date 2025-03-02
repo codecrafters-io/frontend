e9b41e5c4f90aee48100bac4c668f030be805c86
Wassaf Ali <wassafali213@gmail.com>
2025-03-03 01:05:42 +0530

    ChangesAfterReview

--- app/helpers/limit-array.ts
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export default helper(function limitArray<T>([array, limit]: [T[], number]) {
-  if (limit < 0) return [];
-
-  return [...array].reverse().slice(0, limit);
-});
