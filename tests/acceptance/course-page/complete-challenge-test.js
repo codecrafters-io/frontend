3691c73b5829906e159c8b177785106beac10140
Ryan Gang <ryan-gg@outlook.com>
2025-03-25 13:34:14 +0530

    refactor(complete-challenge-test): update default completion message to null
    Modified the acceptance test for the course page to reflect that the default completion message from the backend is now null, aligning with recent changes in the CourseModel.

--- tests/acceptance/course-page/complete-challenge-test.js
+++ tests/acceptance/course-page/complete-challenge-test.js
@@ -20,12 +20,7 @@ module('Acceptance | course-page | complete-challenge-test', function (hooks) {
     const python = this.server.schema.languages.findBy({ name: 'Python' });
     const docker = this.server.schema.courses.findBy({ slug: 'docker' });
 
-    // Default completion message from the backend
-    // TODO: Add to fixtures ?
-    docker.update({
-      completionMessageMarkdown: "Congratulations! You've completed all stages of this course. 🎉",
-    });
-
+    // Default completion message from the backend is null
     this.server.create('repository', 'withAllStagesCompleted', {
       course: docker,
       language: python,
