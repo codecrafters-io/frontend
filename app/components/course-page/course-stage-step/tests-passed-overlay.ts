e97480dd1380e8995ccb3a0dc598d8d58362aa31
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-04-04 15:47:47 -0700

    feat(course-page): add tests passed modal to course stage
    Introduce a new modal component that informs users when they have passed 
    all tests for a course stage. The modal provides a celebratory message, 
    encourages progression to the next step, and offers an option to view 
    instructions again. Additionally, integrate this modal into the current 
    step overlay within the course stage template for improved user feedback 
    and navigation.

---
+++ app/components/course-page/course-stage-step/tests-passed-overlay.ts
@@ -0,0 +1,49 @@
+import Component from '@glimmer/component';
+import type CourseStageStep from 'codecrafters-frontend/utils/course-page-step-list/course-stage-step';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import fade from 'ember-animated/transitions/fade';
+
+interface Signature {
+  Element: HTMLDivElement;
+
+  Args: {
+    currentStep: CourseStageStep;
+  };
+
+  Blocks: {
+    default: [];
+  };
+}
+
+export default class TestsPassedOverlayComponent extends Component<Signature> {
+  @tracked lastSeenStepId: string | null = null;
+  @tracked modalWasDismissed = false;
+
+  transition = fade;
+
+  get shouldShowModal(): boolean {
+    return !this.modalWasDismissed && this.args.currentStep.status === 'in_progress' && this.args.currentStep.testsStatus === 'passed';
+  }
+
+  @action
+  handleModalDismissed() {
+    this.modalWasDismissed = true;
+  }
+
+  @action
+  handleStepIdUpdated() {
+    if (this.args.currentStep.id === this.lastSeenStepId) {
+      return;
+    }
+
+    this.lastSeenStepId = this.args.currentStep.id;
+    this.modalWasDismissed = false;
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'CoursePage::CourseStageStep::TestsPassedOverlay': typeof TestsPassedOverlayComponent;
+  }
+}
