ed3a3619a0767b671cdd0a5b03f3f75e18b30f8a
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-04-04 16:49:15 -0700

    feat: add choose action step in tests passed modal
    Introduce a new component to the tests passed modal, allowing users
    to choose between actions: marking the stage as complete or 
    refactoring code. Enhance accessibility with data-test attributes 
    for better integration with tests. Update related test files to 
    accommodate the new structure, ensuring consistent user interactions.

--- app/components/course-page/course-stage-step/tests-passed-modal/index.ts
+++ app/components/course-page/course-stage-step/tests-passed-modal/index.ts
@@ -1,4 +1,6 @@
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import type CourseStageStep from 'codecrafters-frontend/utils/course-page-step-list/course-stage-step';
 
 interface Signature {
@@ -10,7 +12,14 @@ interface Signature {
   };
 }
 
-export default class TestsPassedModalComponent extends Component<Signature> {}
+export default class TestsPassedModalComponent extends Component<Signature> {
+  @tracked action: 'choose_action' | 'refactor_code' | 'mark_stage_as_complete' = 'choose_action';
+
+  @action
+  handleActionButtonClick(action: 'refactor_code' | 'mark_stage_as_complete' | 'choose_action') {
+    this.action = action;
+  }
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
