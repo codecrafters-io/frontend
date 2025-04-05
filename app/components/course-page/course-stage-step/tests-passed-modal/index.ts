00a31df49f013156a24acee87c8e2414a3005744
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-04-04 18:39:21 -0700

    feat: enhance tests passed modal with loading state
    Update the action button to display a loading spinner when processing.  
    Add an isProcessing flag for better UX. Modify the ChooseActionStep to  
    pass down the new flag. Refactor the command variant component to accept  
    additional attributes. These changes improve user feedback during  
    asynchronous actions and enhance component reusability.

--- app/components/course-page/course-stage-step/tests-passed-modal/index.ts
+++ app/components/course-page/course-stage-step/tests-passed-modal/index.ts
@@ -1,7 +1,11 @@
-import { action } from '@ember/object';
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import type CourseStageStep from 'codecrafters-frontend/utils/course-page-step-list/course-stage-step';
+import { action } from '@ember/object';
+import { task } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
+import type AuthenticatorService from 'codecrafters-frontend/services/authenticator';
+import type Store from '@ember-data/store';
+import { service } from '@ember/service';
 
 interface Signature {
   Element: HTMLDivElement;
@@ -13,12 +17,37 @@ interface Signature {
 }
 
 export default class TestsPassedModalComponent extends Component<Signature> {
-  @tracked action: 'choose_action' | 'refactor_code' | 'mark_stage_as_complete' = 'choose_action';
+  @service declare authenticator: AuthenticatorService;
+  @service declare store: Store;
+
+  @tracked action: 'choose_action' | 'refactor_code' | 'mark_stage_as_complete' | 'submit_code' = 'choose_action';
 
   @action
   handleActionButtonClick(action: 'refactor_code' | 'mark_stage_as_complete' | 'choose_action') {
-    this.action = action;
+    if (action === 'mark_stage_as_complete') {
+      if (this.args.currentStep.repository.lastSubmissionCanBeUsedForStageCompletion) {
+        this.action = 'choose_action';
+        this.createCourseStageCompletionTask.perform();
+      } else {
+        this.action = 'submit_code';
+      }
+    } else {
+      this.action = action;
+    }
   }
+
+  createCourseStageCompletionTask = task(async () => {
+    await this.store
+      .createRecord('course-stage-completion', {
+        courseStage: this.args.currentStep.courseStage,
+        repository: this.args.currentStep.repository,
+        submission: this.args.currentStep.repository.lastSubmission,
+      })
+      .save();
+
+    await this.args.currentStep.repository.refreshStateFromServer();
+    await this.authenticator.syncCurrentUser();
+  });
 }
 
 declare module '@glint/environment-ember-loose/registry' {
