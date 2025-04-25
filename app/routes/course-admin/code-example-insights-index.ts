2e559d46120a66206b08479e8d1a00bf8cee6d38
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-04-24 19:26:00 -0700

    feat: add statistic component and update routing for insights

---
+++ app/routes/course-admin/code-example-insights-index.ts
@@ -0,0 +1,26 @@
+import { inject as service } from '@ember/service';
+import BaseRoute from 'codecrafters-frontend/utils/base-route';
+import Store from '@ember-data/store';
+import type CourseModel from 'codecrafters-frontend/models/course';
+
+export type ModelType = {
+  course: CourseModel;
+};
+
+export default class CodeExampleInsightsIndexRoute extends BaseRoute {
+  @service declare store: Store;
+
+  async model(): Promise<ModelType> {
+    // @ts-ignore
+    const course = this.modelFor('course-admin').course as CourseModel;
+
+    // (await this.store.query('course-stage-participation-analysis', {
+    //   course_id: course.id,
+    //   include: 'stage',
+    // })) as unknown as CourseStageParticipationAnalysisModel[];
+
+    return {
+      course: course,
+    };
+  }
+}
