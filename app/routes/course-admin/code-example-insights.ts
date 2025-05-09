8ce64547009163484390bd83ecc44537311935e8
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-05-09 16:00:14 -0700

    feat(insights): add language and solutions to code examples route

--- app/routes/course-admin/code-example-insights.ts
+++ app/routes/course-admin/code-example-insights.ts
@@ -3,10 +3,12 @@ import BaseRoute from 'codecrafters-frontend/utils/base-route';
 import Store from '@ember-data/store';
 import type CourseStageModel from 'codecrafters-frontend/models/course-stage';
 import type LanguageModel from 'codecrafters-frontend/models/language';
+import type CommunityCourseStageSolutionModel from 'codecrafters-frontend/models/community-course-stage-solution';
 
 export type ModelType = {
   courseStage: CourseStageModel;
   language: LanguageModel;
+  solutions: CommunityCourseStageSolutionModel[];
 };
 
 export default class CodeExampleInsightsRoute extends BaseRoute {
@@ -16,6 +18,7 @@ export default class CodeExampleInsightsRoute extends BaseRoute {
     // @ts-ignore
     const course = this.modelFor('course-admin').course as CourseModel;
     const courseStage = course.stages.findBy('slug', params.stage_slug);
+    const language = courseStage.course.betaOrLiveLanguages.findBy('slug', 'go');
 
     // (await this.store.query('course-stage-participation-analysis', {
     //   course_id: course.id,
@@ -24,7 +27,12 @@ export default class CodeExampleInsightsRoute extends BaseRoute {
 
     return {
       courseStage: courseStage,
-      language: courseStage.course.betaOrLiveLanguages.findBy('slug', 'go'),
+      language: language,
+      solutions: (await this.store.query('community-course-stage-solution', {
+        course_stage_id: courseStage.id,
+        language_id: language.id,
+        include: 'user',
+      })) as unknown as CommunityCourseStageSolutionModel[],
     };
   }
 }
