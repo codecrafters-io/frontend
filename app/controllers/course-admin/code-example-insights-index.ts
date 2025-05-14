f9c2cea4faeee2a3d0e9bc59bf984dcb6f44d01a
Ryan Gang <ryan-gg@outlook.com>
2025-05-14 22:15:51 +0530

    refactor: declare model type in CodeExampleInsightsIndexController for improved type safety

--- app/controllers/course-admin/code-example-insights-index.ts
+++ app/controllers/course-admin/code-example-insights-index.ts
@@ -4,10 +4,13 @@ import { inject as service } from '@ember/service';
 import type RouterService from '@ember/routing/router-service';
 import type LanguageModel from 'codecrafters-frontend/models/language';
 import Store from '@ember-data/store';
+import type { ModelType } from 'codecrafters-frontend/routes/course-admin/code-example-insights-index';
 
 export default class CodeExampleInsightsIndexController extends Controller {
   @service declare router: RouterService;
   @service declare store: Store;
+
+  declare model: ModelType;
 
   @action
   onLanguageChange(language: LanguageModel | null) {
