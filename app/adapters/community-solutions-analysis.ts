68596395d39d0f001157e9115296e01e6f74936e
Ryan Gang <ryan-gg@outlook.com>
2025-05-16 16:12:33 +0530

    refactor: remove CommunitySolutionsAnalysisAdapter as it is no longer needed

--- app/adapters/community-solutions-analysis.ts
+++
@@ -1,7 +0,0 @@
-import ApplicationAdapter from './application';
-
-export default class CommunitySolutionsAnalysisAdapter extends ApplicationAdapter {
-  pathForType() {
-    return 'code-examples';
-  }
-}
