8d615ed5557b331d1696a842205aefef923660e8
Ryan <ryan-gg@outlook.com>
2025-05-21 00:53:44 +0530

    tests: uncomment and organize imports in code example insights index page tests

--- tests/acceptance/course-admin/code-example-insights-index-test.js
+++ tests/acceptance/course-admin/code-example-insights-index-test.js
@@ -1,55 +1,45 @@
-// import { module, test } from 'qunit';
-// import { setupApplicationTest } from 'codecrafters-frontend/tests/helpers';
-// import codeExampleInsightsIndexPage from 'codecrafters-frontend/tests/pages/course-admin/code-example-insights-index-page';
-// import createCommunityCourseStageSolution from 'codecrafters-frontend/mirage/utils/create-community-course-stage-solution';
-// import testScenario from 'codecrafters-frontend/mirage/scenarios/test';
-// import { signInAsStaff } from 'codecrafters-frontend/tests/support/authentication-helpers';
-// import createLanguages from 'codecrafters-frontend/mirage/utils/create-languages';
-// import createCourseFromData from 'codecrafters-frontend/mirage/utils/create-course-from-data';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'codecrafters-frontend/tests/helpers';
+import codeExampleInsightsIndexPage from 'codecrafters-frontend/tests/pages/course-admin/code-example-insights-index-page';
+import createCommunityCourseStageSolution from 'codecrafters-frontend/mirage/utils/create-community-course-stage-solution';
+import testScenario from 'codecrafters-frontend/mirage/scenarios/test';
+import { signInAsStaff } from 'codecrafters-frontend/tests/support/authentication-helpers';
+import createLanguages from 'codecrafters-frontend/mirage/utils/create-languages';
+import createCourseFromData from 'codecrafters-frontend/mirage/utils/create-course-from-data';
 
-// // 1. Visiting /courses/shell/admin/code-examples should redirect to ?language_slug=c for the first language in the list
-// // 2. Language dropdown should work
-// // 3. Clicking on a stage should redirect to the code example insights page for that stage
-// module('Acceptance | course-admin | code-example-insights-index', function (hooks) {
-//   setupApplicationTest(hooks);
+// 1. Visiting /courses/shell/admin/code-examples should redirect to ?language_slug=c for the first language in the list
+// 2. Language dropdown should work
+// 3. Clicking on a stage should redirect to the code example insights page for that stage
+module('Acceptance | course-admin | code-example-insights-index', function (hooks) {
+  setupApplicationTest(hooks);
 
-//   hooks.beforeEach(function () {
-//     testScenario(this.server);
-//     signInAsStaff(this.owner, this.server);
+  hooks.beforeEach(function () {
+    testScenario(this.server);
+    signInAsStaff(this.owner, this.server);
 
-//     this.currentUser = this.server.schema.users.first();
-//     this.language = this.server.schema.languages.findBy({ name: 'Python' });
-//     this.course = this.server.schema.courses.findBy({ slug: 'redis' });
-//     this.courseStage = this.course.stages.models.sortBy('position')[0];
+    this.currentUser = this.server.schema.users.first();
+    this.language = this.server.schema.languages.findBy({ name: 'Python' });
+    this.course = this.server.schema.courses.findBy({ slug: 'redis' });
+    this.courseStage = this.course.stages.models.sortBy('position')[0];
 
-//     this.solution1 = createCommunityCourseStageSolution(this.server, this.course, 2, this.language);
-//     this.solution2 = createCommunityCourseStageSolution(this.server, this.course, 2, this.language);
+    this.solution1 = createCommunityCourseStageSolution(this.server, this.course, 2, this.language);
+    this.solution2 = createCommunityCourseStageSolution(this.server, this.course, 2, this.language);
 
-//     this.server.create('community-solution-analysis', {
-//       communitySolution: this.solution2,
-//       evaluator: this.evaluator,
-//       result: 'fail',
-//     });
-//   });
+    this.server.create('community-solution-analysis', {
+      communitySolution: this.solution2,
+      evaluator: this.evaluator,
+      result: 'fail',
+    });
+  });
 
-//   test('shows empty state when no stages', async function (assert) {
-//     testScenario(this.server);
-//     signInAsStaff(this.owner, this.server);
+  test('shows empty state when no stages', async function (assert) {
+    testScenario(this.server);
+    signInAsStaff(this.owner, this.server);
 
-//     // Create a course with one language but no stages
-//     createLanguages(this.server);
-//     const language = this.server.schema.languages.findBy({ slug: 'python' });
-//     const course = createCourseFromData(this.server, {
-//       slug: 'test-course',
-//       title: 'Test Course',
-//       stages: [],
-//       extensions: [],
-//       betaOrLiveLanguages: [language],
-//     });
-
-//     await codeExampleInsightsIndexPage.visit({ course_slug: course.slug, language_slug: language.slug });
-//     assert.ok(codeExampleInsightsIndexPage.noStagesFoundMessageIsVisible, 'Shows no stages found message');
-//   });
+    await codeExampleInsightsIndexPage.visit({ course_slug: this.course.slug });
+    await this.pauseTest();
+    assert.ok(codeExampleInsightsIndexPage.noStagesFoundMessageIsVisible, 'Shows no stages found message');
+  });
 
 //   test('renders stages and extensions for selected language', async function (assert) {
 //     testScenario(this.server);
@@ -81,4 +71,4 @@
 //     assert.strictEqual(codeExampleInsightsIndexPage.extensionHeaders.length, 1, 'Shows extension header');
 //     assert.ok(codeExampleInsightsIndexPage.extensionHeaders[0].text.includes('Extension 1'), 'Extension header text is correct');
 //   });
-// });
+});
--- tests/pages/course-admin/code-example-insights-index-page.js
+++ tests/pages/course-admin/code-example-insights-index-page.js
@@ -1,14 +1,14 @@
-// import { create, visitable, text, collection, isVisible, clickable } from 'ember-cli-page-object';
-// import LanguageDropdown from './components/language-dropdown';
+import { create, visitable, text, collection, isVisible, clickable } from 'ember-cli-page-object';
+import LanguageDropdown from 'codecrafters-frontend/tests/pages/components/course-admin/language-dropdown';
 
-// export default create({
-//   visit: visitable('/course-admin/code-example-insights-index'),
+export default create({
+  visit: visitable('/courses/:course_slug/admin/code-examples'),
 
-//   languageDropdown: LanguageDropdown,
+  languageDropdown: LanguageDropdown,
 
-//   stageListItems: collection('[data-test-code-example-insights-index-list-item]', {
-//     text: text(),
-//   }),
+  stageListItems: collection('[data-test-code-example-insights-index-list-item]', {
+    text: text(),
+  }),
 
-//   scope: '[data-test-code-example-insights-index-page]',
-// });
+  scope: '[data-test-code-example-insights-index-page]',
+});
