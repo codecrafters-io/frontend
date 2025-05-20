51bea43e303a39da25526179de6b280aff5e18e4
Ryan <ryan-gg@outlook.com>
2025-05-21 01:33:04 +0530

    test: add community solutions analysis creation in insights index page test

--- tests/acceptance/course-admin/code-example-insights-index-test.js
+++ tests/acceptance/course-admin/code-example-insights-index-test.js
@@ -6,6 +6,7 @@ import testScenario from 'codecrafters-frontend/mirage/scenarios/test';
 import { signInAsStaff } from 'codecrafters-frontend/tests/support/authentication-helpers';
 import createLanguages from 'codecrafters-frontend/mirage/utils/create-languages';
 import createCourseFromData from 'codecrafters-frontend/mirage/utils/create-course-from-data';
+import createCommunitySolutionsAnalysis from 'codecrafters-frontend/mirage/utils/create-community-solutions-analysis';
 
 // 1. Visiting /courses/shell/admin/code-examples should redirect to ?language_slug=c for the first language in the list
 // 2. Language dropdown should work
@@ -13,62 +14,42 @@ import createCourseFromData from 'codecrafters-frontend/mirage/utils/create-cour
 module('Acceptance | course-admin | code-example-insights-index', function (hooks) {
   setupApplicationTest(hooks);
 
-  hooks.beforeEach(function () {
+  test('opening insights index page with no language slug redirects to language slug "c"', async function (assert) {
     testScenario(this.server);
     signInAsStaff(this.owner, this.server);
 
     this.currentUser = this.server.schema.users.first();
-    this.language = this.server.schema.languages.findBy({ name: 'Python' });
+    this.language_1 = this.server.schema.languages.findBy({ name: 'C' });
+    this.language_2 = this.server.schema.languages.findBy({ name: 'Python' });
     this.course = this.server.schema.courses.findBy({ slug: 'redis' });
-    this.courseStage = this.course.stages.models.sortBy('position')[0];
+    this.courseStage_1 = this.course.stages.models.sortBy('position')[0];
+    this.courseStage_2 = this.course.stages.models.sortBy('position')[1];
 
-    this.solution1 = createCommunityCourseStageSolution(this.server, this.course, 2, this.language);
-    this.solution2 = createCommunityCourseStageSolution(this.server, this.course, 2, this.language);
-
-    this.server.create('community-solution-analysis', {
-      communitySolution: this.solution2,
-      evaluator: this.evaluator,
-      result: 'fail',
-    });
-  });
-
-  test('shows empty state when no stages', async function (assert) {
-    testScenario(this.server);
-    signInAsStaff(this.owner, this.server);
+    this.analysis = createCommunitySolutionsAnalysis(this.server, this.courseStage_1, this.language_1)
+    this.analysis = createCommunitySolutionsAnalysis(this.server, this.courseStage_2, this.language_2)
 
     await codeExampleInsightsIndexPage.visit({ course_slug: this.course.slug });
-    await this.pauseTest();
-    assert.ok(codeExampleInsightsIndexPage.noStagesFoundMessageIsVisible, 'Shows no stages found message');
+    assert.strictEqual(codeExampleInsightsIndexPage.stageListItems.length, 55); 
+    
+    let itemsText = codeExampleInsightsIndexPage.stageListItems[0].text;
+    assert.strictEqual(
+      itemsText,
+      `${this.courseStage_1.name} 1 solutions 10 lines 0 upvotes 0 downvotes`,
+      'Stage list item text is correct'
+    );
+
+    assert.strictEqual(codeExampleInsightsIndexPage.languageDropdown.currentLanguageName, 'C');
+
+    await codeExampleInsightsIndexPage.languageDropdown.click();
+    await codeExampleInsightsIndexPage.languageDropdown.clickOnLanguageLink('Python');
+    assert.strictEqual(codeExampleInsightsIndexPage.languageDropdown.currentLanguageName, 'Python');
+    assert.strictEqual(codeExampleInsightsIndexPage.stageListItems.length, 55);
+
+    let itemsText_2 = codeExampleInsightsIndexPage.stageListItems[1].text;
+    assert.strictEqual(
+      itemsText_2,
+      `${this.courseStage_2.name} 1 solutions 10 lines 0 upvotes 0 downvotes`,
+      'Stage list item text is correct'
+    );
   });
-
-//   test('renders stages and extensions for selected language', async function (assert) {
-//     testScenario(this.server);
-//     signInAsStaff(this.owner, this.server);
-
-//     // Create language and course with stages/extensions
-//     createLanguages(this.server);
-//     const language = this.server.schema.languages.findBy({ slug: 'python' });
-//     const course = createCourseFromData(this.server, {
-//       slug: 'test-course',
-//       title: 'Test Course',
-//       stages: [
-//         { name: 'Stage 1', slug: 'stage-1' },
-//         { name: 'Stage 2', slug: 'stage-2' },
-//       ],
-//       extensions: [
-//         {
-//           name: 'Extension 1',
-//           slug: 'extension-1',
-//           stages: [{ name: 'Ext Stage 1', slug: 'ext-stage-1' }],
-//         },
-//       ],
-//       betaOrLiveLanguages: [language],
-//     });
-
-//     await codeExampleInsightsIndexPage.visit({ course_slug: course.slug, language_slug: language.slug });
-
-//     assert.strictEqual(codeExampleInsightsIndexPage.stageListItems.length, 2, 'Shows base stages');
-//     assert.strictEqual(codeExampleInsightsIndexPage.extensionHeaders.length, 1, 'Shows extension header');
-//     assert.ok(codeExampleInsightsIndexPage.extensionHeaders[0].text.includes('Extension 1'), 'Extension header text is correct');
-//   });
 });
