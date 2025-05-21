09b4b668933f1bf2f5655622d4986164497625f2
Ryan <ryan-gg@outlook.com>
2025-05-21 11:49:34 +0530

    refactor: remove unused HTTP methods from community solutions analyses handler

--- mirage/handlers/community-solutions-analyses.js
+++ mirage/handlers/community-solutions-analyses.js
@@ -1,6 +1,3 @@
 export default function (server) {
   server.get('/community-solutions-analyses');
-  server.post('/community-solutions-analyses');
-  server.patch('/community-solutions-analyses/:id');
-  server.delete('/community-solutions-analyses/:id');
 }
