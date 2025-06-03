c4a83ded1fa2c19636202470d7a475f58967fe46
vedantnene <nene@codecrafters.io>
2025-06-04 03:52:40 +0530

    lint issues #2

--- app/components/faq-list.ts
+++ app/components/faq-list.ts
@@ -42,7 +42,7 @@ export default class FaqListComponent extends Component<Signature> {
       query: 'Is there a free trial?',
       answer: `<p>We always have at least one challenge available for free. You can easily find them highlighted in <a href="https://app.codecrafters.io/catalog" target="_blank" class="underline" rel="noopener noreferrer">our catalog</a>.</p>
     
-    <p>Additionally, you can explore the detailed breakdowns of all our challenges without signing up. You can also view the task descriptions for every challenge without hitting a paywall.</p>`
+    <p>Additionally, you can explore the detailed breakdowns of all our challenges without signing up. You can also view the task descriptions for every challenge without hitting a paywall.</p>`,
     },
     {
       query: 'Is there a recurring payment?',
@@ -54,14 +54,14 @@ export default class FaqListComponent extends Component<Signature> {
         "We don't offer refunds. You can freely explore our curriculum without a paywall, and also test drive a couple of stages per course, without providing any card details.",
     },
     {
-      query: 'This is great but it\'s too expensive',
+      query: "This is great but it's too expensive",
       answer: `<p>We understand the cost consideration today but investing in your technical skills is one of the highest ROI activities you can do. It has a direct impact on your earning potential.</p>
     
     <p>We’ve spoken with hundreds of our paid users about their experience before CodeCrafters. They’d spend weeks window-shopping for project ideas, trying to piece together resources, getting lost in “tutorial hell”, and realizing too late that their plans missed key details. Instead of wasting weeks or sometimes months’ worth of time, our users prefer to start right away with CodeCrafters, pushing code, refining their skills, and following our proven, battle-tested guidance.</p>
     
     <p>If you’re early in your career or still a student working with limited budgets, focusing on foundational skills might be more appropriate before investing in CodeCrafters, which is designed for those who already have a job and are ready to push their limits.</p>
     
-    <p>Many companies allow employees to expense CodeCrafters as part of their learning and development budget, yours might too. <a href="https://codecrafters.io/expense" target="_blank" class="underline" rel="noopener noreferrer">Read more about that here</a>.</p>`
+    <p>Many companies allow employees to expense CodeCrafters as part of their learning and development budget, yours might too. <a href="https://codecrafters.io/expense" target="_blank" class="underline" rel="noopener noreferrer">Read more about that here</a>.</p>`,
     },
   ];
 
