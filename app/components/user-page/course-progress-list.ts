7c1e15605c0af9927f33bfab51cb4f2812ddd9be
Arpan Pandey <arpanpandey@gmail.com>
2025-06-17 21:10:38 +0530

    Don't show alpha courses on user profile

--- app/components/user-page/course-progress-list.ts
+++ app/components/user-page/course-progress-list.ts
@@ -16,7 +16,10 @@ export default class CourseProgressListComponent extends Component<Signature> {
     const participationsGroupedByCourse: CourseParticipationModel[][] = Object.values(
       groupBy(
         this.args.user.courseParticipations.filter(
-          (participation) => !participation.course.releaseStatusIsDeprecated && !participation.course.visibilityIsPrivate,
+          (participation) => 
+            !participation.course.releaseStatusIsDeprecated && 
+            !participation.course.visibilityIsPrivate &&
+            !participation.course.releaseStatusIsAlpha
         ),
         'course',
       ),
