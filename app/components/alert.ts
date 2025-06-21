dd2069748896f4eb72cb9d1144a53aeeb5c3c1c8
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-06-20 19:17:28 -0700

    style: update alert component colors for better contrast

--- app/components/alert.ts
+++ app/components/alert.ts
@@ -15,9 +15,9 @@ interface Signature {
 export default class AlertComponent extends Component<Signature> {
   get containerColorClasses(): string {
     return {
-      green: 'bg-green-100/40 dark:bg-green-900/20 border-green-500/60 dark:border-green-500/40',
-      blue: 'bg-blue-100/40 dark:bg-blue-900/20 border-blue-500/60 dark:border-blue-500/40',
-      red: 'bg-red-100/40 dark:bg-red-900/20 border-red-500/60 dark:border-red-500/40',
+      green: 'bg-green-100/20 dark:bg-green-900/20 border-green-500/60 dark:border-green-500/40',
+      blue: 'bg-blue-100/20 dark:bg-blue-900/20 border-blue-500/60 dark:border-blue-500/40',
+      red: 'bg-red-100/20 dark:bg-red-900/20 border-red-500/60 dark:border-red-500/40',
     }[this.args.color];
   }
 }
