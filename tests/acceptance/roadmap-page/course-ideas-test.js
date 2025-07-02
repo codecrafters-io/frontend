ffc2cf0441ee24cd8a6beaa457053a01b01b1a49
Arpan Pandey <arpanpandey.aps@gmail.com>
2025-07-02 10:22:44 +0530

    Fixed the ordering changing rapidly + added tests

--- tests/acceptance/roadmap-page/course-ideas-test.js
+++ tests/acceptance/roadmap-page/course-ideas-test.js
@@ -6,6 +6,7 @@ import { assertTooltipContent } from 'ember-tooltips/test-support';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'codecrafters-frontend/tests/helpers';
 import { signIn } from 'codecrafters-frontend/tests/support/authentication-helpers';
+import FakeDateService from 'codecrafters-frontend/tests/support/fake-date-service';
 
 module('Acceptance | roadmap-page | course-ideas', function (hooks) {
   setupApplicationTest(hooks);
@@ -77,14 +78,14 @@ module('Acceptance | roadmap-page | course-ideas', function (hooks) {
 
     createCourseIdeas(this.server);
 
+    const httpCourseIdea = this.server.schema.courseIdeas.findBy({ name: 'Build your own HTTP Server' });
+    httpCourseIdea.update('developmentStatus', 'in_progress');
     const regexCourseIdea = this.server.schema.courseIdeas.findBy({ name: 'Build your own Regex Parser' });
-    regexCourseIdea.update('developmentStatus', 'in_progress');
-    const sqliteCourseIdea = this.server.schema.courseIdeas.findBy({ name: 'Build your own SQLite' });
-    sqliteCourseIdea.update('developmentStatus', 'released');
+    regexCourseIdea.update('developmentStatus', 'released');
 
     await roadmapPage.visit();
 
-    let courseIdeaCard = roadmapPage.findCourseIdeaCard('Build your own Regex Parser');
+    let courseIdeaCard = roadmapPage.findCourseIdeaCard('Build your own HTTP Server');
     await courseIdeaCard.hoverOnDevelopmentStatusPill();
 
     assertTooltipContent(assert, {
@@ -100,11 +101,107 @@ module('Acceptance | roadmap-page | course-ideas', function (hooks) {
 
     await courseIdeaCard.clickOnVoteButton();
 
-    courseIdeaCard = roadmapPage.findCourseIdeaCard('Build your own SQLite');
+    courseIdeaCard = roadmapPage.findCourseIdeaCard('Build your own Regex Parser');
     await courseIdeaCard.hoverOnDevelopmentStatusPill();
 
     assertTooltipContent(assert, {
       contentString: 'This challenge is now available! Visit the catalog to try it out.',
     });
+  });
+
+  test('it is sorted algorithmically for logged in user', async function (assert) {
+    testScenario(this.server);
+    signIn(this.owner, this.server);
+
+    // Set up fake date service for consistent sorting behavior
+    this.owner.unregister('service:date');
+    this.owner.register('service:date', FakeDateService);
+
+    const dateService = this.owner.lookup('service:date');
+    const fixedDate = new Date('2024-06-15T00:00:00.000Z').getTime();
+    dateService.setNow(fixedDate);
+
+    createCourseIdeas(this.server);
+
+    const regex = this.server.schema.courseIdeas.findBy({ name: 'Build your own Regex Parser' });
+    regex.update('votesCount', 100);
+
+    const http = this.server.schema.courseIdeas.findBy({ name: 'Build your own HTTP Server' });
+    http.update('votesCount', 15);
+
+    const shell = this.server.schema.courseIdeas.findBy({ name: 'Build your own Shell' });
+    shell.update('votesCount', 10);
+
+    const bittorrent = this.server.schema.courseIdeas.findBy({ name: 'Build your own BitTorrent client' });
+    bittorrent.update('votesCount', 5);
+
+    const react = this.server.schema.courseIdeas.findBy({ name: 'Build your own React' });
+    react.update('votesCount', 12);
+
+    await roadmapPage.visit();
+
+    const courseIdeaCards = roadmapPage.courseIdeaCards;
+    assert.strictEqual(courseIdeaCards.length, 5, 'should have 5 course idea cards (SQLite is archived)');
+
+    const cardOrder = courseIdeaCards.map((card) => card.name);
+
+    // Note: The exact order depends on the algorithmic sorting, but we can verify that
+    // in-progress ideas (HTTP Server) come first, followed by not-started ideas
+    const httpIndex = cardOrder.indexOf('Build your own HTTP Server');
+    const shellIndex = cardOrder.indexOf('Build your own Shell');
+    const bittorrentIndex = cardOrder.indexOf('Build your own BitTorrent client');
+    const reactIndex = cardOrder.indexOf('Build your own React');
+    const regexIndex = cardOrder.indexOf('Build your own Regex Parser');
+
+    // In-progress should come first (highest priority)
+    assert.strictEqual(httpIndex, 0, 'in-progress idea should be first');
+
+    // Not-started ideas should come after in-progress
+    assert.true(shellIndex > httpIndex, 'not-started ideas should come after in-progress');
+    assert.true(bittorrentIndex > httpIndex, 'not-started ideas should come after in-progress');
+    assert.true(reactIndex > httpIndex, 'not-started ideas should come after in-progress');
+
+    // Released ideas should come last (lowest priority)
+    assert.true(regexIndex > shellIndex, 'released ideas should come after not-started');
+    assert.true(regexIndex > bittorrentIndex, 'released ideas should come after not-started');
+    assert.true(regexIndex > reactIndex, 'released ideas should come after not-started');
+  });
+
+  test('it is sorted by votes count for anonymous user', async function (assert) {
+    testScenario(this.server);
+    createCourseIdeas(this.server);
+
+    const regex = this.server.schema.courseIdeas.findBy({ name: 'Build your own Regex Parser' });
+    regex.update('votesCount', 100);
+
+    const http = this.server.schema.courseIdeas.findBy({ name: 'Build your own HTTP Server' });
+    http.update('votesCount', 15);
+
+    const shell = this.server.schema.courseIdeas.findBy({ name: 'Build your own Shell' });
+    shell.update('votesCount', 10);
+
+    const bittorrent = this.server.schema.courseIdeas.findBy({ name: 'Build your own BitTorrent client' });
+    bittorrent.update('votesCount', 5);
+
+    const react = this.server.schema.courseIdeas.findBy({ name: 'Build your own React' });
+    react.update('votesCount', 12);
+
+    await roadmapPage.visit();
+
+    const courseIdeaCards = roadmapPage.courseIdeaCards;
+    assert.strictEqual(courseIdeaCards.length, 5, 'should have 5 course idea cards (SQLite is archived)');
+
+    const cardOrder = courseIdeaCards.map((card) => card.name);
+
+    // For anonymous users, ideas are sorted by development status priority first, then by vote count
+    // in_progress (3) > not_started (2) > released (1)
+    const expectedOrder = [
+      'Build your own HTTP Server',
+      'Build your own React',
+      'Build your own Shell',
+      'Build your own BitTorrent client',
+      'Build your own Regex Parser',
+    ];
+    assert.deepEqual(cardOrder, expectedOrder, 'cards should be sorted by development status priority then vote count');
   });
 });
