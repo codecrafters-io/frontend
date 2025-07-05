ea6ed5c1d128c0ccb2ca55ba6de28fdaa21a7c0f
Vasyl Marchuk <public@vasylmarchuk.com>
2025-07-05 11:17:50 +0300

    Replace `hex-rgb` and `rgb-hex` packages with `culori`

--- app/utils/blend-colors.ts
+++ app/utils/blend-colors.ts
@@ -1,11 +1,26 @@
-import hexRgb from 'hex-rgb';
-import rgbHex from 'rgb-hex';
+import type { Rgb, Color } from 'culori';
+import { converter, formatHex, blend } from 'culori';
 
-export default function blendColors(fgColor = '#ffffff', opacity = 1, bgColor = '#ffffff') {
-  const fgRgb = hexRgb(fgColor, { format: 'array' });
-  const bgRgb = hexRgb(bgColor, { format: 'array' });
+const COLOR_WHITE_RGB: Rgb = { alpha: 1, r: 1, g: 1, b: 1, mode: 'rgb' };
 
-  const blendedRgb = fgRgb.slice(0, 3).map<number>((colFg, idx) => opacity * colFg + (1 - opacity) * (bgRgb[idx] || 255)) as [number, number, number];
+export default function blendColors(
+  fgColor: Color | string = COLOR_WHITE_RGB,
+  opacity: number = 1,
+  bgColor: Color | string = COLOR_WHITE_RGB,
+): string {
+  const fgRgb = converter('rgb')(fgColor) || COLOR_WHITE_RGB;
+  const bgRgb = converter('rgb')(bgColor) || COLOR_WHITE_RGB;
 
-  return `#${rgbHex(...blendedRgb)}`;
+  const fgRgbTransparent: Rgb = {
+    mode: 'rgb',
+    alpha: opacity,
+    r: fgRgb?.r,
+    g: fgRgb?.g,
+    b: fgRgb?.b,
+  };
+
+  const blendedRgb = blend([bgRgb, fgRgbTransparent]);
+  const blendedHex = formatHex(blendedRgb);
+
+  return blendedHex;
 }
