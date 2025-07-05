46d02bdeb5839197776928364fbdd9c6053f089c
Vasyl Marchuk <public@vasylmarchuk.com>
2025-07-05 19:58:01 +0300

    Properly include `ember-animated` and `typeform` styles in `app.js`

--- app/app.js
+++ app/app.js
@@ -5,6 +5,8 @@ import config from 'codecrafters-frontend/config/environment';
 import * as Sentry from '@sentry/ember';
 import 'ember-basic-dropdown/styles';
 import 'codecrafters-frontend/app.css';
+import 'ember-animated/index';
+import '@typeform/embed/build/css/popup.css';
 
 const sentryDSN = 'https://478cca7283ca40209deae5160b54ee4f@o294739.ingest.sentry.io/5922961';
 
