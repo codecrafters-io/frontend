aa1a0cc636e3e85ec33b274d3ef69755976ed789
Arpan Pandey <arpanpandey.aps@gmail.com>
2025-07-07 15:53:00 +0530

    Apparently this format was off

--- mirage/data/course-extension-ideas.js
+++ mirage/data/course-extension-ideas.js
@@ -11,7 +11,7 @@ export default [
     slug: 'redis-geospatial-commands',
     course_slug: 'redis',
     name: 'Geospatial commands',
-    development_status: 'not-started',
+    development_status: 'not_started',
     description_md:
       "[HTTP](https://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol) is the protocol that powers the web. In this challenge, you'll build a HTTP/1.1 server that is capable of serving multiple clients. \n\nAlong the way you'll learn about TCP servers, [HTTP request syntax](https://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html), HTTP/1.1's [request pipelining](https://en.wikipedia.org/wiki/HTTP_pipelining) and more.",
   },
@@ -19,7 +19,7 @@ export default [
     slug: 'sqlite-insert',
     course_slug: 'sqlite',
     name: 'Insert rows',
-    development_status: 'not-started',
+    development_status: 'not_started',
     description_md:
       "[Shells](https://en.wikipedia.org/wiki/Unix_shell) are a command-line interface to your operating system. In this challenge, you'll build your own version of `bash` that is capable of executing basic shell commands. \n\nAlong the way, you'll learn about [pipes](https://en.wikipedia.org/wiki/Pipeline_(Unix)), [redirection](https://en.wikipedia.org/wiki/Redirection_(computing)), [fork+exec](https://en.wikipedia.org/wiki/Fork%E2%80%93exec) and more.",
   },
@@ -27,7 +27,7 @@ export default [
     slug: 'extension-idea-1',
     course_slug: 'dummy',
     name: 'Extension idea 1',
-    development_status: 'not-started',
+    development_status: 'not_started',
     description_md: 'This is the first extension idea for the dummy course',
   },
 ];
