4d27fbad9f41eeeba652e66a32f5316a516f2e7b
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-07-24 16:59:37 -0700

    feat: add discounted pricing and amortization to plan card

--- app/components/pay-page/choose-membership-plan-modal/plan-card.ts
+++ app/components/pay-page/choose-membership-plan-modal/plan-card.ts
@@ -1,15 +1,30 @@
 import Component from '@glimmer/component';
 
 interface Signature {
-  Element: HTMLAnchorElement;
+  Element: HTMLDivElement;
 
   Args: {
     actualPriceInDollars: number;
+    discountedPriceInDollars?: number;
     isSelected: boolean;
+    validityInMonths?: number; // Not set for lifetime plans
+    title: string;
   };
 }
 
-export default class PlanCard extends Component<Signature> {}
+export default class PlanCard extends Component<Signature> {
+  get amortizedMonthlyPriceInDollars(): number | null {
+    if (this.args.validityInMonths) {
+      return Math.round(this.effectivePriceInDollars / this.args.validityInMonths);
+    }
+
+    return null;
+  }
+
+  get effectivePriceInDollars(): number {
+    return this.args.discountedPriceInDollars || this.args.actualPriceInDollars;
+  }
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
