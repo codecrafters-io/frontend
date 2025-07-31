d3867dffdcda0d660839d202df5bc4807eb79996
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-07-31 01:56:54 -0700

    feat: add modal handling for campus program application

--- app/models/institution.ts
+++ app/models/institution.ts
@@ -3,6 +3,10 @@ import Model, { attr } from '@ember-data/model';
 export default class InstitutionModel extends Model {
   @attr('string') declare logoUrl: string;
   @attr('string') declare longName: string;
+
+  // @ts-expect-error empty '' not supported
+  @attr('') declare officialEmailAddressSuffixes: string[];
+
   @attr('string') declare shortName: string;
   @attr('string') declare slug: string;
 }
