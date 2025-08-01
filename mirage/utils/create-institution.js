2a40da6c4bab1192bd768a313444b9d2a69fb35b
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-08-01 13:47:35 -0700

    feat: enhance createInstitution to support dynamic slugs

--- mirage/utils/create-institution.js
+++ mirage/utils/create-institution.js
@@ -1,12 +1,24 @@
-export default function createInstitution(server, slug) {
-  if (slug !== 'nus') {
-    throw new Error('Not implemented');
-  }
+import { capitalize } from '@ember/string';
 
-  return server.create('institution', {
-    slug: 'nus',
-    shortName: 'NUS',
-    logoUrl: 'https://codecrafters.io/images/app_institution_logos/nus.svg',
-    officialEmailAddressSuffixes: ['@u.nus.edu', '@nus.edu.sg'],
-  });
+export default function createInstitution(server, slug) {
+  // TODO: Add stanford too to get one more screenshot
+  const knownSlugsToParams = {
+    nus: {
+      slug: 'nus',
+      shortName: 'NUS',
+      logoUrl: 'https://codecrafters.io/images/app_institution_logos/nus.svg',
+      officialEmailAddressSuffixes: ['@u.nus.edu', '@nus.edu.sg'],
+    },
+  };
+
+  if (knownSlugsToParams[slug]) {
+    server.create('institution', knownSlugsToParams[slug]);
+  } else {
+    server.create('institution', {
+      slug,
+      shortName: capitalize(slug),
+      logoUrl: `https://codecrafters.io/images/app_institution_logos/nus.svg`, // Use a dummy logo since we don't have one
+      officialEmailAddressSuffixes: [`@${slug}.edu`],
+    });
+  }
 }
