8c946f73c90a11a6d269d4589693f514798b10be
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-08-01 19:33:48 -0700

    refactor: simplify view creation by using resource relation

--- app/models/view.ts
+++ app/models/view.ts
@@ -3,7 +3,6 @@ import UserModel from 'codecrafters-frontend/models/user';
 
 export default class ViewModel extends Model {
   @belongsTo('user', { async: false, inverse: null }) declare viewer?: UserModel;
-  @attr('string') declare resourceId: string;
-  @attr('string') declare resourceType: string;
+  @belongsTo('viewable', { async: false, inverse: null, polymorphic: true }) declare resource: unknown;
   @attr({}) declare metadata: Record<string, unknown>;
 }
