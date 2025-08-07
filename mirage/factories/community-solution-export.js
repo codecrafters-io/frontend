f4640d9433a1b79a35924685445d17f401822e9c
Arpan Pandey <arpanpandey.aps@gmail.com>
2025-08-07 21:42:32 +0530

    Uhh

--- mirage/factories/community-solution-export.js
+++ mirage/factories/community-solution-export.js
@@ -3,7 +3,7 @@ import { Factory } from 'miragejs';
 export default Factory.extend({
   status: 'provisioned',
   lastAccessedAt: () => new Date(),
-  humanId: () => `export-${Math.random().toString(36).substring(2, 11)}`,
+  humanId: () => `brainy-toad-${Math.floor(Math.random() * 999999)}`,
   expired: false,
   githubRepositoryUrl: 'https://github.com/test-user/test-repo',
 });
