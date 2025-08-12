b8d38e0a8dd8f7c682666b9a27fbaba99189fd66
Arpan Pandey <arpanpandey.aps@gmail.com>
2025-08-12 14:33:19 +0530

    Linting

--- mirage/handlers/community-solution-exports.js
+++ mirage/handlers/community-solution-exports.js
@@ -1,6 +1,6 @@
 export default function (server) {
   server.post('/community-solution-exports');
-  
+
   server.post('/community-solution-exports/:id/mark-as-accessed', function (schema, request) {
     return schema.communitySolutionExports.find(request.params.id);
   });
