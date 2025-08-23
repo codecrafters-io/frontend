2d5562c1c72bf49a39d159dfde017b58844f637c
Arpan Pandey <arpanpandey.aps@gmail.com>
2025-08-23 10:29:24 +0530

    Fix expiration date display in membership section and update institution relation.

--- app/models/institution-membership-grant.ts
+++ app/models/institution-membership-grant.ts
@@ -3,13 +3,9 @@ import type InstitutionModel from 'codecrafters-frontend/models/institution';
 import type UserModel from 'codecrafters-frontend/models/user';
 
 export default class InstitutionMembershipGrantModel extends Model {
-  @belongsTo('institution', { async: false, inverse: null }) declare institution: InstitutionModel;
+  @belongsTo('institution', { async: true, inverse: null }) declare institution: InstitutionModel;
   @belongsTo('user', { async: true, inverse: 'institutionMembershipGrants' }) declare user: UserModel;
 
   @attr('date') declare grantedAt: Date;
   @attr('number') declare validityInDays: number;
-
-  get expiresAt() {
-    return new Date(this.grantedAt.getTime() + this.validityInDays * 24 * 60 * 60 * 1000);
-  }
 }
