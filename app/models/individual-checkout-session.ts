51f7acf403e4cb0c613881af48d646d1a04d5d75
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-09-28 16:12:33 -0700

    feat(pay-page): add pricingPlanId to checkout session model
    Add pricingPlanId attribute to IndividualCheckoutSessionModel and update
    ChooseMembershipPlanModal to include it when creating a new checkout session.
    This change is done to replace the usage of pricingFrequency with an explicit
    pricingPlanId, enabling more accurate tracking and processing of the selected
    membership plan during checkout. The pricingFrequency field remains temporarily
    for backward compatibility.

--- app/models/individual-checkout-session.ts
+++ app/models/individual-checkout-session.ts
@@ -5,6 +5,7 @@ import type RegionalDiscountModel from 'codecrafters-frontend/models/regional-di
 export default class IndividualCheckoutSessionModel extends Model {
   @attr('boolean') declare extraInvoiceDetailsRequested: boolean;
   @attr('string') declare pricingFrequency: string;
+  @attr('string') declare pricingPlanId: string;
   @attr('string') declare successUrl: string;
   @attr('string') declare cancelUrl: string;
   @attr('string') declare url: string;
