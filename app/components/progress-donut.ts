3c14ac379b1c0cc6f025771395cf42ae77547929
Arpan Pandey <arpanpandey.aps@gmail.com>
2025-10-02 20:20:57 +0100

    Refactor progress donut component to use inline styles and remove modifier.

--- app/components/progress-donut.ts
+++ app/components/progress-donut.ts
@@ -1,4 +1,6 @@
 import Component from '@glimmer/component';
+import { htmlSafe } from '@ember/template';
+import type { SafeString } from '@ember/template/-private/handlebars';
 
 interface Signature {
   Element: SVGElement;
@@ -21,6 +23,12 @@ export default class ProgressDonut extends Component<Signature> {
 
     // Apply minimum width of 10%
     return Math.max(10, (100 * (this.args.completed || 0)) / this.args.total);
+  }
+
+  get progressStyle(): SafeString {
+    const targetOffset = (62.83 * (100 - this.progressPercentage)) / 100;
+
+    return htmlSafe(`--target-offset: ${targetOffset}`);
   }
 
   get targetOffset(): number {
