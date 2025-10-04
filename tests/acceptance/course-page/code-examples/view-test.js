a61659f1eb801eb0101416259340574ea31585c2
Vasyl Marchuk <public@vasylmarchuk.com>
2025-10-04 20:33:46 +0300

    Add tests for languages dropdown shadow dissolving at end-of-scroll

--- tests/acceptance/course-page/code-examples/view-test.js
+++ tests/acceptance/course-page/code-examples/view-test.js
@@ -59,6 +59,30 @@ module('Acceptance | course-page | code-examples | view', function (hooks) {
     await codeExamplesPage.solutionCards[1].clickOnExpandButton();
 
     await codeExamplesPage.languageDropdown.toggle();
+
+    assert.true(codeExamplesPage.languageDropdown.shadowOverlay.isPresent);
+
+    await codeExamplesPage.languageDropdown.scrollToPercentage(0);
+    assert.strictEqual(codeExamplesPage.languageDropdown.shadowOverlay.opacity, 1);
+
+    await codeExamplesPage.languageDropdown.scrollToPercentage(80);
+    assert.strictEqual(codeExamplesPage.languageDropdown.shadowOverlay.opacity, 1);
+
+    await codeExamplesPage.languageDropdown.scrollToPercentage(95);
+    assert.strictEqual(codeExamplesPage.languageDropdown.shadowOverlay.opacity, 0.725);
+
+    await codeExamplesPage.languageDropdown.scrollToPercentage(97);
+    assert.strictEqual(codeExamplesPage.languageDropdown.shadowOverlay.opacity, 0.4375);
+
+    await codeExamplesPage.languageDropdown.scrollToPercentage(98);
+    assert.strictEqual(codeExamplesPage.languageDropdown.shadowOverlay.opacity, 0.2875);
+
+    await codeExamplesPage.languageDropdown.scrollToPercentage(100);
+    assert.strictEqual(codeExamplesPage.languageDropdown.shadowOverlay.opacity, 0);
+
+    await codeExamplesPage.languageDropdown.scrollToPercentage(0);
+    assert.strictEqual(codeExamplesPage.languageDropdown.shadowOverlay.opacity, 1);
+
     await codeExamplesPage.languageDropdown.clickOnLink('Python');
 
     assert.strictEqual(codeExamplesPage.solutionCards.length, 1, 'expected 1 python solution to be present');
--- tests/pages/components/language-dropdown.js
+++ tests/pages/components/language-dropdown.js
@@ -1,4 +1,5 @@
 import { clickOnText, clickable, collection, text } from 'ember-cli-page-object';
+import { timeout } from 'ember-concurrency';
 
 export default {
   currentLanguageName: text('[data-test-language-dropdown-trigger] [data-test-current-language-name]', { resetScope: true }),
@@ -12,6 +13,23 @@ export default {
   links: collection('div[role="button"]', {
     text: text(),
   }),
+
+  shadowOverlay: {
+    scope: 'shadow-overlay',
+
+    get opacity() {
+      const el = document.querySelector(this.scope);
+
+      return +el.style.opacity;
+    },
+  },
+
+  async scrollToPercentage(percentage) {
+    const el = document.querySelector(this.scope);
+    el.scrollTo({ top: (percentage / 100) * (el.scrollHeight - el.clientHeight), behavior: 'instant' });
+    el.dispatchEvent(new MouseEvent('scroll'));
+    await timeout(0); // wait till next run-loop for scroll event to propagate
+  },
 
   resetScope: true,
   scope: '[data-test-language-dropdown-content]',
