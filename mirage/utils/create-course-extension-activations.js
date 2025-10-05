e4b6772ecc286782c2d670bd2dda4786ddff5cd4
Arpan Pandey <arpanpandey.aps@gmail.com>
2025-10-05 10:38:54 +0100

    Fix: Add position to course extension activations in mirage test data

--- mirage/utils/create-course-extension-activations.js
+++ mirage/utils/create-course-extension-activations.js
@@ -1,8 +1,9 @@
 export default function createCourseExtensionActivations(server, repository) {
-  repository.course.extensions.models.forEach((extension) => {
+  repository.course.extensions.models.forEach((extension, index) => {
     server.create('course-extension-activation', {
       extension,
       repository,
+      position: index + 1, // 1-based position
     });
   });
 }
