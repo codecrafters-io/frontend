601531576de965c5a6547c7ae61d5ea43bbe1efb
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-10-24 21:07:15 -0700

    feat(course-page): enhance workflow tutorial visuals and file tree
    Replace static images with dynamic content in setup step modals for a
    more engaging user experience. Step 1 now displays a file tree preview
    based on the repository's first stage solution, improving clarity on
    editable files. Step 2 uses an animated background to better illustrate
    the git push workflow. These changes aid users in understanding the
    course setup process more effectively.

--- app/components/course-page/setup-step-complete-modal/workflow-tutorial-step1-screen.ts
+++ app/components/course-page/setup-step-complete-modal/workflow-tutorial-step1-screen.ts
@@ -1,4 +1,5 @@
 import Component from '@glimmer/component';
+import type { FileTreeFolderItem, FileTreeItem } from 'codecrafters-frontend/components/file-tree-preview/types';
 import type SetupStep from 'codecrafters-frontend/utils/course-page-step-list/setup-step';
 
 interface Signature {
@@ -10,7 +11,36 @@ interface Signature {
   };
 }
 
-export default class WorkflowTutorialStep1ScreenComponent extends Component<Signature> {}
+export default class WorkflowTutorialStep1ScreenComponent extends Component<Signature> {
+  get editableFilePathInFirstStageSolution(): string {
+    if (!this.args.step.repository.firstStageSolution) {
+      return 'README.md';
+    }
+
+    return this.args.step.repository.firstStageSolution.changedFiles[0]!.filename;
+  }
+
+  get rootFolderForfileTreePreview(): FileTreeFolderItem {
+    return {
+      type: 'folder',
+      name: this.args.step.repository.cloneDirectory,
+      children: [this.buildTreeForPath(this.editableFilePathInFirstStageSolution), { type: 'folder', name: 'other files', isGrayedOut: true }],
+    };
+  }
+
+  buildTreeForPath(path: string) {
+    const parts = path.split('/');
+
+    let node: FileTreeItem = { type: 'file', name: parts[parts.length - 1]! };
+
+    // Walk backwards through the path to build up the nested tree
+    for (let i = parts.length - 2; i >= 0; i--) {
+      node = { type: 'folder', name: parts[i]!, children: [node] };
+    }
+
+    return node;
+  }
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
