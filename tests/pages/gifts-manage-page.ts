95d1b1c3672ba8cbcc5bb0af5f55f8e06ade10f6
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-12-05 11:38:51 +0530

    test(gifts-manage-page): add page object properties for gift message
    Add clickable actions for edit, save, and cancel buttons on the gift
    management page. Add a text property to access the editable gift message
    text. These changes improve test interaction capabilities with the gift
    message UI elements.

--- tests/pages/gifts-manage-page.ts
+++ tests/pages/gifts-manage-page.ts
@@ -1,6 +1,15 @@
-import { visitable } from 'ember-cli-page-object';
+import { clickable, text, visitable } from 'ember-cli-page-object';
 import createPage from 'codecrafters-frontend/tests/support/create-page';
 
 export default createPage({
   visit: visitable('/gifts/manage/:management_token'),
+
+  giftMessageContainer: {
+    scope: '[data-test-gift-message-container]',
+    messageText: text('[data-test-gift-message-editable]'),
+  },
+
+  clickOnEditButton: clickable('[data-test-edit-button]'),
+  clickOnSaveButton: clickable('[data-test-save-button]'),
+  clickOnCancelButton: clickable('[data-test-cancel-button]'),
 });
