28116d34fe001f986fd19223dc6a62e42bc178e1
Andy Li <1450947+andy1li@users.noreply.github.com>
2026-01-12 15:02:51 +0900

    [percy] Enhance CLI connection step with OS detection and command variants support

--- app/components/course-page/setup-step/repository-setup-card/test-cli-connection-step.ts
+++ app/components/course-page/setup-step/repository-setup-card/test-cli-connection-step.ts
@@ -1,5 +1,10 @@
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import type Owner from '@ember/owner';
+import type { CopyableTerminalCommandVariant } from 'codecrafters-frontend/components/copyable-terminal-command-with-variants';
 import type RepositoryModel from 'codecrafters-frontend/models/repository';
+import detectOperatingSystem from 'codecrafters-frontend/utils/detect-operating-system';
 
 export interface Signature {
   Element: HTMLDivElement;
@@ -10,7 +15,38 @@ export interface Signature {
   };
 }
 
-export default class TestCliConnectionStep extends Component<Signature> {}
+export default class TestCliConnectionStep extends Component<Signature> {
+  @tracked commandVariants: CopyableTerminalCommandVariant[] = [];
+  @tracked selectedCommandVariant: CopyableTerminalCommandVariant | null = null;
+
+  constructor(owner: Owner, args: Signature['Args']) {
+    super(owner, args);
+    this.loadCommandVariants();
+  }
+
+  @action
+  handleVariantSelect(variant: CopyableTerminalCommandVariant) {
+    this.selectedCommandVariant = variant;
+  }
+
+  async loadCommandVariants() {
+    const powershellVariant: CopyableTerminalCommandVariant = {
+      label: 'PowerShell',
+      commands: ['irm https://codecrafters.io/install.ps1 | iex', 'codecrafters ping'],
+    };
+
+    const posixVariant: CopyableTerminalCommandVariant = {
+      label: 'Shell',
+      commands: ['curl https://codecrafters.io/install.sh | sh', 'codecrafters ping'],
+    };
+
+    if ((await detectOperatingSystem()) === 'Windows') {
+      this.commandVariants = [powershellVariant, posixVariant];
+    } else {
+      this.commandVariants = [posixVariant, powershellVariant];
+    }
+  }
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
