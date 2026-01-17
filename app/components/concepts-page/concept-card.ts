95767a790122d64e400080a4d55503d19fa50737
Andy Li <1450947+andy1li@users.noreply.github.com>
2026-01-18 05:12:17 +0900

    fix(concept-card): update link target behavior for concept card

--- app/components/concepts-page/concept-card.ts
+++ app/components/concepts-page/concept-card.ts
@@ -2,6 +2,7 @@ import AuthenticatorService from 'codecrafters-frontend/services/authenticator';
 import Component from '@glimmer/component';
 import ConceptModel from 'codecrafters-frontend/models/concept';
 import { service } from '@ember/service';
+import config from 'codecrafters-frontend/config/environment';
 import fieldComparator from 'codecrafters-frontend/utils/field-comparator';
 
 interface Signature {
@@ -20,6 +21,11 @@ export default class ConceptCard extends Component<Signature> {
       .filter((engagement) => engagement.concept.slug === this.args.concept.slug)
       .sort(fieldComparator('startedAt'))
       .reverse()[0];
+  }
+
+  get linkTarget() {
+    // Don't open in new tab during tests, as it breaks test navigation
+    return config.environment === 'test' ? undefined : '_blank';
   }
 }
 
