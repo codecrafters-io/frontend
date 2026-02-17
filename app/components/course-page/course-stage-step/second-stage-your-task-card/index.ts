9af3b1fffcb4de588ab427dbae8ea025022e1623
Paul Kuruvilla <rohitpaulk@gmail.com>
2026-02-17 12:50:43 -0800

    refactor(course-stage-step): replace ImplementSolutionStep with HowToPassStepList
    Remove the ImplementSolutionStep component and template in favor of a new,
    more comprehensive HowToPassStepList component that lists steps to complete the
    stage. This change improves clarity by explicitly showing the "implement solution"
    and "run tests" steps along with their completion status.
    
    The new component consolidates step logic, handles completion detection for each
    step, and includes an action to expand test logs, enhancing the user experience
    and maintainability.

--- app/components/course-page/course-stage-step/second-stage-your-task-card/index.ts
+++ app/components/course-page/course-stage-step/second-stage-your-task-card/index.ts
@@ -1,6 +1,4 @@
 import Component from '@glimmer/component';
-import RepositoryModel from 'codecrafters-frontend/models/repository';
-import type { StepDefinition } from 'codecrafters-frontend/components/step-list';
 import type CourseStageStep from 'codecrafters-frontend/utils/course-page-step-list/course-stage-step';
 
 interface Signature {
@@ -11,59 +9,9 @@ interface Signature {
   };
 }
 
-class BaseStep {
-  isComplete: boolean;
-  repository: RepositoryModel;
-
-  constructor(repository: RepositoryModel, isComplete: boolean) {
-    this.isComplete = isComplete;
-    this.repository = repository;
-  }
-}
-
-class ImplementSolutionStep extends BaseStep implements StepDefinition {
-  id = 'implement-solution';
-
-  get titleMarkdown() {
-    return 'Write code to pass this stage';
-  }
-}
-
-class RunTestsStep extends BaseStep implements StepDefinition {
-  id = 'run-tests';
-
-  get titleMarkdown() {
-    return 'Run tests remotely';
-  }
-}
-
 export default class SecondStageYourTaskCard extends Component<Signature> {
-  get implementSolutionStepIsComplete() {
-    return (
-      this.runTestsStepIsComplete ||
-      (this.args.currentStep.repository.lastSubmission?.courseStage === this.args.currentStep.courseStage &&
-        !this.args.currentStep.repository.lastSubmission?.clientTypeIsSystem) // Run tests (in progress)
-    );
-  }
-
   get instructionsMarkdown() {
     return this.args.currentStep.courseStage.buildInstructionsMarkdownFor(this.args.currentStep.repository);
-  }
-
-  get runTestsStepIsComplete() {
-    return (
-      this.args.currentStep.repository.stageIsComplete(this.args.currentStep.courseStage) ||
-      (this.args.currentStep.repository.lastSubmissionHasSuccessStatus &&
-        this.args.currentStep.repository.lastSubmission.courseStage === this.args.currentStep.courseStage &&
-        !this.args.currentStep.repository.lastSubmission?.clientTypeIsSystem)
-    );
-  }
-
-  get steps() {
-    return [
-      new ImplementSolutionStep(this.args.currentStep.repository, this.implementSolutionStepIsComplete),
-      new RunTestsStep(this.args.currentStep.repository, this.runTestsStepIsComplete),
-    ];
   }
 }
 
---
+++ app/components/course-page/course-stage-step/your-task-card/how-to-pass-step-list.ts
@@ -0,0 +1,86 @@
+import Component from '@glimmer/component';
+import type CoursePageStateService from 'codecrafters-frontend/services/course-page-state';
+import type CourseStageModel from 'codecrafters-frontend/models/course-stage';
+import type CourseStageSolutionModel from 'codecrafters-frontend/models/course-stage-solution';
+import type RepositoryModel from 'codecrafters-frontend/models/repository';
+import type { StepDefinition } from 'codecrafters-frontend/components/step-list';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+
+interface Signature {
+  Element: HTMLDivElement;
+
+  Args: {
+    repository: RepositoryModel;
+    courseStage: CourseStageModel;
+  };
+}
+
+class BaseStep {
+  isComplete: boolean;
+  repository: RepositoryModel;
+
+  constructor(repository: RepositoryModel, isComplete: boolean) {
+    this.isComplete = isComplete;
+    this.repository = repository;
+  }
+}
+
+class ImplementSolutionStep extends BaseStep implements StepDefinition {
+  id = 'implement-solution';
+
+  get titleMarkdown() {
+    return 'Write code to pass this stage';
+  }
+}
+
+class RunTestsStep extends BaseStep implements StepDefinition {
+  id = 'run-tests';
+
+  get titleMarkdown() {
+    return 'Run tests remotely';
+  }
+}
+
+export default class HowToPassStepList extends Component<Signature> {
+  @service declare coursePageState: CoursePageStateService;
+
+  get implementSolutionStepIsComplete() {
+    return (
+      this.runTestsStepIsComplete ||
+      (this.args.repository.lastSubmission?.courseStage === this.args.courseStage &&
+        !this.args.repository.lastSubmission?.clientTypeIsSystem)
+    );
+  }
+
+  get runTestsStepIsComplete() {
+    return (
+      this.args.repository.stageIsComplete(this.args.courseStage) ||
+      (this.args.repository.lastSubmissionHasSuccessStatus &&
+        this.args.repository.lastSubmission.courseStage === this.args.courseStage &&
+        !this.args.repository.lastSubmission?.clientTypeIsSystem)
+    );
+  }
+
+  get solution(): CourseStageSolutionModel | null {
+    return this.args.courseStage.solutions.find((s) => s.language === this.args.repository.language) || null;
+  }
+
+  get steps() {
+    return [
+      new ImplementSolutionStep(this.args.repository, this.implementSolutionStepIsComplete),
+      new RunTestsStep(this.args.repository, this.runTestsStepIsComplete),
+    ];
+  }
+
+  @action
+  handleViewLogsButtonClick() {
+    this.coursePageState.testResultsBarIsExpanded = true;
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'CoursePage::CourseStageStep::YourTaskCard::HowToPassStepList': typeof HowToPassStepList;
+  }
+}
