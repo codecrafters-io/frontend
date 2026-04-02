49f8263a2b0803e58babee11ed893f3f7068fe5d
Sarup Banskota <sbanskota08@gmail.com>
2026-04-02 16:48:13 -0700

    feat: add PostHog analytics event when user clicks Join on live call widget

--- app/components/live-call-widget/index.ts
+++ app/components/live-call-widget/index.ts
@@ -1,9 +1,11 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
+import type AnalyticsEventTrackerService from 'codecrafters-frontend/services/analytics-event-tracker';
 import type LiveCallWidgetService from 'codecrafters-frontend/services/live-call-widget';
 
 export default class LiveCallWidgetComponent extends Component {
+  @service declare analyticsEventTracker: AnalyticsEventTrackerService;
   @service declare liveCallWidget: LiveCallWidgetService;
 
   get hostInitial(): string {
@@ -15,6 +17,11 @@ export default class LiveCallWidgetComponent extends Component {
     const meetLink = this.liveCallWidget.displayData?.meet_link;
 
     if (meetLink) {
+      this.analyticsEventTracker.track('clicked_live_call_widget_join', {
+        host_name: this.liveCallWidget.displayData?.host_name,
+        meet_link: meetLink,
+      });
+
       window.open(meetLink, '_blank');
     }
   }
